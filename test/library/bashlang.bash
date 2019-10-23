3e0ec68818925d1760ff62f2a4d866f47c15df3f
Chris Suszyński <ksuszyns@redhat.com>
2019-10-23 18:49:14 +0200

    Compact bash files to 6 files
    As community clearly asked, I'm compacting Bash files to 6 elements.
    It's now separeted but also more inline.

--- test/library/bashlang.bash
+++ test/library/bashlang.bash
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-function array.contains () {
+function array.contains {
   local e match="$1"
   shift
   for e; do [[ "$e" == "$match" ]] && return 0; done
   return 1
+}
+
+function array.join {
+  local IFS="$1"
+  shift
+  echo "$*"
 }
