483655771094143244db36f7cc4d88c6bd783ee7
Markus Thömmes <markusthoemmes@me.com>
2019-12-04 08:52:21 +0100

    Improve the developer experience.
    - Clearly spell out the requirements for the scripts.
    - Remove minikube docs to not confuse people.
    - Move `envsubst` call into the catalog.sh script so it produces a meaningful result as well.

--- hack/lib/catalogsource.bash
+++ hack/lib/catalogsource.bash
@@ -12,17 +12,8 @@ function ensure_catalogsource_installed {
 function install_catalogsource {
   logger.info "Installing CatalogSource"
 
-  # Determine if we're running locally or in CI.
-  if [ -z "$OPENSHIFT_BUILD_NAMESPACE" ]; then
-    export IMAGE_KNATIVE_SERVING_OPERATOR="${DOCKER_REPO_OVERRIDE}/knative-serving-operator"
-    export IMAGE_KNATIVE_OPENSHIFT_INGRESS="${DOCKER_REPO_OVERRIDE}/knative-openshift-ingress"
-  else
-    export IMAGE_KNATIVE_SERVING_OPERATOR="registry.svc.ci.openshift.org/${OPENSHIFT_BUILD_NAMESPACE}/stable:knative-serving-operator"
-    export IMAGE_KNATIVE_OPENSHIFT_INGRESS="registry.svc.ci.openshift.org/${OPENSHIFT_BUILD_NAMESPACE}/stable:knative-openshift-ingress"
-  fi
-
   local rootdir="$(dirname "$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")")"
-  ${rootdir}/hack/catalog.sh | envsubst | oc apply -n "$OPERATORS_NAMESPACE" -f - || return 1
+  ${rootdir}/hack/catalog.sh | oc apply -n "$OPERATORS_NAMESPACE" -f - || return 1
 
   logger.success "CatalogSource installed successfully"
 }
