117ee9acca08bc79b88fd5f10b6008a61bf3d00f
Markus Thömmes <markusthoemmes@me.com>
2020-01-28 10:56:49 +0100

    Bring back the colors for TTY environments.

--- hack/lib/ui.bash
+++ hack/lib/ui.bash
@@ -1,21 +1,73 @@
 #!/usr/bin/env bash
 
+if [ -t 1 ]; then 
+  IS_TTY=true
+else
+  IS_TTY=false
+fi
+readonly IS_TTY
+
+readonly FORCE_COLOR="${FORCE_COLOR:-false}"
+
+SHOULD_COLOR="$(if [[ "${FORCE_COLOR}" == "true" ]] || [[ "${IS_TTY}" == "true" ]]; then echo true; else echo false; fi)"
+readonly SHOULD_COLOR
+
+readonly COLOR_NC='\e[0m' # No Color
+readonly COLOR_BLUE='\e[0;34m'
+readonly COLOR_GREEN='\e[0;32m'
+readonly COLOR_LIGHT_GREEN='\e[1;32m'
+readonly COLOR_CYAN='\e[0;36m'
+readonly COLOR_LIGHT_RED='\e[1;31m'
+readonly COLOR_LIGHT_YELLOW='\e[1;33m'
+
 function logger.debug {
-  printf "%7s %s %s\n" "DEBUG" "$(date '+%H:%M:%S.%3N')" "$*" 1>&2
+  logger.__log 'DEBUG' "${COLOR_BLUE}" "$*"
 }
 
 function logger.info {
-  printf "%7s %s %s\n" "INFO" "$(date '+%H:%M:%S.%3N')" "$*" 1>&2
+  logger.__log 'INFO' "${COLOR_GREEN}" "$*"
 }
 
 function logger.success {
-  printf "%7s %s %s\n" "SUCCESS" "$(date '+%H:%M:%S.%3N')" "$*" 1>&2
+  logger.__log 'SUCCESS' "${COLOR_LIGHT_GREEN}" "$*"
 }
 
 function logger.warn {
-  printf "%7s %s %s\n" "WARNING" "$(date '+%H:%M:%S.%3N')" "$*" 1>&2
+  logger.__log 'WARNING' "${COLOR_LIGHT_YELLOW}" "$*"
 }
 
 function logger.error {
-  printf "%7s %s %s\n" "ERROR" "$(date '+%H:%M:%S.%3N')" "$*" 1>&2
+  logger.__log 'ERROR' "${COLOR_LIGHT_RED}" "$*"
 }
+
+function logger.__log {
+  local message level now color
+  level="$1"
+  color="$2"
+  message="$3"
+  now="$(date '+%H:%M:%S.%3N')"
+  
+  printf "${color}%7s ${COLOR_CYAN}%s ${color}%s${COLOR_NC}\n" "${level}" "${now}" "${message}" 1>&2
+}
+
+if [[ "${SHOULD_COLOR}" == "false" ]]; then
+  function logger.debug {
+    echo 'DEBUG' "$(date '+%H:%M:%S.%3N')" "$*"
+  }
+
+  function logger.info {
+    echo 'INFO' "$(date '+%H:%M:%S.%3N')" "$*"
+  }
+
+  function logger.success {
+    echo 'SUCCESS' "$(date '+%H:%M:%S.%3N')" "$*"
+  }
+
+  function logger.warn {
+    echo 'WARNING' "$(date '+%H:%M:%S.%3N')" "$*"
+  }
+
+  function logger.error {
+    echo 'ERROR' "$(date '+%H:%M:%S.%3N')" "$*"
+  }
+fi
