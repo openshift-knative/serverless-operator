b4959fcd18a51b23917d1eeb04c3d5b43b7569e8
Martin Gencur <mgencur@redhat.com>
2020-02-24 12:15:35 +0100

    Make sure to check version of images during upgrade
    * also extend the list of OCP version where current OpenShift release
    doesn't work

--- test/lib.bash
+++ test/lib.bash
@@ -194,8 +194,9 @@ function run_knative_serving_rolling_upgrade_tests {
 
     local cluster_version
     cluster_version=$(oc get clusterversion -o=jsonpath="{.items[0].status.history[?(@.state==\"Completed\")].version}")
-    if [[ "$cluster_version" = 4.1.* || "${HOSTNAME}" = *ocp-41* ]]; then
-      if approve_csv "$upgrade_to" ; then # Upgrade should fail on OCP 4.1
+    if [[ "$cluster_version" = 4.1.* || "${HOSTNAME}" = *ocp-41* || \
+          "$cluster_version" = 4.2.* || "${HOSTNAME}" = *ocp-42* ]]; then
+      if approve_csv "$upgrade_to" ; then # Upgrade should fail on OCP 4.1, 4.2
         return 1
       fi
       # Check we got RequirementsNotMet error
@@ -207,6 +208,10 @@ function run_knative_serving_rolling_upgrade_tests {
       # Both knativeserving CRs should be updated now
       timeout 900 '[[ ! ( $(oc get knativeserving.serving.knative.dev knative-serving -n $SERVING_NAMESPACE -o=jsonpath="{.status.version}") != $serving_version && $(oc get knativeserving.serving.knative.dev knative-serving -n $SERVING_NAMESPACE -o=jsonpath="{.status.conditions[?(@.type==\"Ready\")].status}") == True ) ]]' || return 1
       timeout 900 '[[ ! ( $(oc get knativeserving.operator.knative.dev knative-serving -n $SERVING_NAMESPACE -o=jsonpath="{.status.version}") != $serving_version && $(oc get knativeserving.operator.knative.dev knative-serving -n $SERVING_NAMESPACE -o=jsonpath="{.status.conditions[?(@.type==\"Ready\")].status}") == True ) ]]' || return 1
+      # Fail if any image still has the old version
+      if oc get pod -n $SERVING_NAMESPACE -o yaml | grep image: | uniq | grep $serving_version ; then
+        return 1
+      fi
     fi
     end_prober_test ${PROBER_PID}
   fi
