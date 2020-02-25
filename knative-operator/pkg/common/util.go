d08bd787b63061f44ac83cfe730c3611e6cf8850
Markus Thömmes <markusthoemmes@me.com>
2020-02-25 09:36:09 +0100

    [SRVKS-427] Override all configuration we set as we have authority. Apply mutations on update.
    Currently, when we update the operator, we will try to override the image setting in the respective config maps. Our `Configure` helper is too conservative though, and only writes config that was not set before.
    
    All configs that we actively set are subject to our authority though. For now, we should always override all of these settings as they are integral to our integration.

--- knative-operator/pkg/common/util.go
+++ knative-operator/pkg/common/util.go
@@ -9,20 +9,24 @@ const MutationTimestampKey = "knative-serving-openshift/mutation"
 
 var Log = logf.Log.WithName("knative").WithName("openshift")
 
-// config helper to set value for key if not already set
+// Configure is a  helper to set a value for a key, potentially overriding existing contents.
 func Configure(ks *servingv1alpha1.KnativeServing, cm, key, value string) bool {
 	if ks.Spec.Config == nil {
 		ks.Spec.Config = map[string]map[string]string{}
 	}
-	if _, found := ks.Spec.Config[cm][key]; !found {
-		if ks.Spec.Config[cm] == nil {
-			ks.Spec.Config[cm] = map[string]string{}
-		}
-		ks.Spec.Config[cm][key] = value
-		Log.Info("Configured", "map", cm, key, value)
-		return true
+
+	old, found := ks.Spec.Config[cm][key]
+	if found && value == old {
+		return false
 	}
-	return false
+
+	if ks.Spec.Config[cm] == nil {
+		ks.Spec.Config[cm] = map[string]string{}
+	}
+
+	ks.Spec.Config[cm][key] = value
+	Log.Info("Configured", "map", cm, key, value, "old value", old)
+	return true
 }
 
 // IngressNamespace returns namespace where ingress is deployed.
