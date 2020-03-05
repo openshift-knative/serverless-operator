e280b19dabdc61c1f30463a412ecb9e982a8bcaf
Jim Crossley <jim@crossleys.org>
2020-03-05 16:59:43 -0500

    Drop support for obsolete CRD in tests [SRVKS-436]

--- hack/lib/serverless.bash
+++ hack/lib/serverless.bash
@@ -26,7 +26,7 @@ function install_serverless_previous {
 
   previous_csv=$("${rootdir}/hack/catalog.sh" | grep replaces: | tail -n1 | awk '{ print $2 }')
   deploy_serverless_operator "$previous_csv"  || return $?
-  deploy_knativeserving_v1alpha1_1.3.0 || return $?
+  deploy_knativeserving_cr || return $?
 }
 
 function remove_installplan {
@@ -39,7 +39,7 @@ function remove_installplan {
 
 function install_serverless_latest {
   deploy_serverless_operator_latest || return $?
-  deploy_knativeserving_v1alpha1_1.3.0 || return $?
+  deploy_knativeserving_cr || return $?
 }
 
 function deploy_serverless_operator_latest {
@@ -100,21 +100,22 @@ function find_install_plan {
   echo ""
 }
 
-# Deploys the version of KnativeServing from 1.3.0 release. The future releases should ensure compatibility with
-# this custom resource and accept it.
-function deploy_knativeserving_v1alpha1_1.3.0 {
+function deploy_knativeserving_cr {
   logger.info 'Deploy Knative Serving'
 
   # Wait for the CRD to appear
   timeout 900 "[[ \$(oc get crd | grep -c knativeservings) -eq 0 ]]" || return 6
 
-  local rootdir
-  rootdir="$(dirname "$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")")"
+   # Install Knative Serving
+  cat <<EOF | oc apply -f - || return $?
+apiVersion: operator.knative.dev/v1alpha1
+kind: KnativeServing
+metadata:
+  name: knative-serving
+  namespace: ${SERVING_NAMESPACE}
+EOF
 
-  # Install Knative Serving
-  oc apply -n "${SERVING_NAMESPACE}" -f "${rootdir}/test/v1alpha1/resources/serving.knative.dev_v1alpha1_knativeserving_cr_1.3.0.yaml" || return $?
-
-  timeout 900 '[[ $(oc get knativeserving.serving.knative.dev knative-serving -n $SERVING_NAMESPACE -o=jsonpath="{.status.conditions[?(@.type==\"Ready\")].status}") != True ]]'  || return 7
+  timeout 900 '[[ $(oc get knativeserving knative-serving -n $SERVING_NAMESPACE -o=jsonpath="{.status.conditions[?(@.type==\"Ready\")].status}") != True ]]'  || return 7
 
   logger.success 'Serverless has been installed sucessfully.'
 }
@@ -122,13 +123,9 @@ function deploy_knativeserving_v1alpha1_1.3.0 {
 function teardown_serverless {
   logger.warn '😭  Teardown Serverless...'
 
-  if oc get knativeserving.serving.knative.dev knative-serving -n "${SERVING_NAMESPACE}" >/dev/null 2>&1; then
+  if oc get knativeserving knative-serving -n "${SERVING_NAMESPACE}" >/dev/null 2>&1; then
     logger.info 'Removing KnativeServing CR'
-    oc delete knativeserving.serving.knative.dev knative-serving -n "${SERVING_NAMESPACE}" || return $?
-  fi
-  if oc get knativeserving.operator.knative.dev knative-serving -n "${SERVING_NAMESPACE}" >/dev/null 2>&1; then
-    logger.info 'Removing KnativeServing CR'
-    oc delete knativeserving.operator.knative.dev knative-serving -n "${SERVING_NAMESPACE}" || return $?
+    oc delete knativeserving knative-serving -n "${SERVING_NAMESPACE}" || return $?
   fi
   logger.info 'Ensure no knative serving pods running'
   timeout 600 "[[ \$(oc get pods -n ${SERVING_NAMESPACE} -o jsonpath='{.items}') != '[]' ]]" || return 9
