d3a3b57a9520653507dfebb3a5ced40cbb84110c
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2020-03-13 10:22:50 +0900

    Always apply kourier in the reconcile loop
    This patch changes to apply Kourier manifest always in the reconcile loop.
    
    This is the consistent method with other deployments like serving deployments and
    ConsoleCLIDownload.

--- knative-operator/pkg/controller/knativeserving/kourier/kourier.go
+++ knative-operator/pkg/controller/knativeserving/kourier/kourier.go
@@ -33,28 +33,19 @@ func Apply(instance *servingv1alpha1.KnativeServing, api client.Client, scheme *
 	if err != nil {
 		return fmt.Errorf("failed to transform kourier manifest: %w", err)
 	}
-	if instance.Status.IsFullySupported() {
-		// TODO: verify deployed kourier is not different from kourier-latest.yaml accurately.
-		if err := checkDeployments(&manifest, instance, api); err == nil {
-			return nil
-		}
-	}
-
-	// Us reaching here means we need to do something and/or wait longer.
-	instance.Status.MarkDependencyInstalling("Kourier")
-	if err := api.Status().Update(context.TODO(), instance); err != nil {
-		return fmt.Errorf("failed to update KnativeServing status: %w", err)
-	}
-
 	log.Info("Installing Kourier Ingress")
 	if err := manifest.Apply(); err != nil {
 		return fmt.Errorf("failed to apply kourier manifest: %w", err)
 	}
 	if err := checkDeployments(&manifest, instance, api); err != nil {
-		return fmt.Errorf("failed to check kourier deployments: %w", err)
+		log.Error(err, "")
+		instance.Status.MarkDependencyInstalling("Kourier")
+		if apiErr := api.Status().Update(context.TODO(), instance); apiErr != nil {
+			return fmt.Errorf("failed to update KnativeServing status: %w", err)
+		}
+		return err
 	}
 	log.Info("Kourier is ready")
-
 	instance.Status.MarkDependenciesInstalled()
 	return api.Status().Update(context.TODO(), instance)
 }
