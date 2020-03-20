14b0ae17e1029b4eff62030f1f2a2e1841eff924
Ali Ok <aliok@redhat.com>
2020-03-20 11:08:40 +0300

    Smoke tests for Knative eventing

--- test/lib.bash
+++ test/lib.bash
@@ -113,7 +113,7 @@ function prepare_knative_serving_tests {
 }
 
 function run_knative_serving_e2e_and_conformance_tests {
-  logger.info "Running E2E and conformance tests"
+  logger.info "Running Serving E2E and conformance tests"
   (
   local knative_version=$1
 
@@ -146,7 +146,7 @@ function run_knative_serving_e2e_and_conformance_tests {
 }
 
 function run_knative_serving_rolling_upgrade_tests {
-  logger.info "Running rolling upgrade tests"
+  logger.info "Running serving rolling upgrade tests"
   (
   local knative_version=$1
 
