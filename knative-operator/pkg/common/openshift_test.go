3b3930c3fda5754082ee338cba0e66ae77b9e565
Paul Morie <pmorie@redhat.com>
2020-03-24 08:48:33 -0400

    Add default behavior for HA (#202)
    * Add HA mutation function
    
    * table tests
    
    * Add openAPI for HA to KnativeServing CRD

--- knative-operator/pkg/common/openshift_test.go
+++ knative-operator/pkg/common/openshift_test.go
@@ -17,49 +17,86 @@ func init() {
 	configv1.AddToScheme(scheme.Scheme)
 }
 
+func newKs() *servingv1alpha1.KnativeServing {
+	return &servingv1alpha1.KnativeServing{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      servingName,
+			Namespace: namespace,
+		},
+	}
+}
+
 func TestMutate(t *testing.T) {
 	const (
 		networks = "foo,bar,baz"
 		domain   = "fubar"
 		image    = "docker.io/queue:tag"
 	)
-	client := fake.NewFakeClient(mockNetworkConfig(strings.Split(networks, ",")), mockIngressConfig(domain))
-	ks := &servingv1alpha1.KnativeServing{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      servingName,
-			Namespace: namespace,
+	os.Setenv("IMAGE_queue-proxy", image)
+	type check func(*testing.T, *servingv1alpha1.KnativeServing)
+
+	cases := []struct {
+		name string
+		ks   *servingv1alpha1.KnativeServing
+		ha   check
+	}{
+		{
+			name: "HA defaulted",
+			ks:   newKs(),
+			ha:   verifyDefaultHA,
+		},
+		{
+			name: "HA not defaulted",
+			ks: func() *servingv1alpha1.KnativeServing {
+				s := newKs()
+				s.Spec.HighAvailability = &servingv1alpha1.HighAvailability{
+					Replicas: 1,
+				}
+
+				return s
+			}(),
+			ha: verifyOverriddenHA,
 		},
 	}
-	// Setup image override
-	os.Setenv("IMAGE_queue-proxy", image)
-	// Mutate for OpenShift
-	err := common.Mutate(ks, client)
-	if err != nil {
-		t.Error(err)
-	}
 
-	verifyIngress(t, ks, domain)
-	verifyImageOverride(t, ks, image)
-	verifyCerts(t, ks)
+	for i := range cases {
+		tc := cases[i]
+		ks := tc.ks
 
-	// Rerun, should be a noop
-	err = common.Mutate(ks, client)
-	if err != nil {
-		t.Error(err)
-	}
-	verifyIngress(t, ks, domain)
-	verifyImageOverride(t, ks, image)
-	verifyCerts(t, ks)
+		client := fake.NewFakeClient(mockNetworkConfig(strings.Split(networks, ",")), mockIngressConfig(domain))
+		// Setup image override
+		// Mutate for OpenShift
+		err := common.Mutate(ks, client)
+		if err != nil {
+			t.Error(err)
+		}
 
-	// Force a change and rerun
-	ks.Spec.Config["network"]["ingress.class"] = "foo"
-	err = common.Mutate(ks, client)
-	if err != nil {
-		t.Error(err)
+		verifyIngress(t, ks, domain)
+		verifyImageOverride(t, ks, image)
+		verifyCerts(t, ks)
+		tc.ha(t, ks)
+
+		// Rerun, should be a noop
+		err = common.Mutate(ks, client)
+		if err != nil {
+			t.Error(err)
+		}
+		verifyIngress(t, ks, domain)
+		verifyImageOverride(t, ks, image)
+		verifyCerts(t, ks)
+		tc.ha(t, ks)
+
+		// Force a change and rerun
+		ks.Spec.Config["network"]["ingress.class"] = "foo"
+		err = common.Mutate(ks, client)
+		if err != nil {
+			t.Error(err)
+		}
+		verifyIngress(t, ks, domain)
+		verifyImageOverride(t, ks, image)
+		verifyCerts(t, ks)
+		tc.ha(t, ks)
 	}
-	verifyIngress(t, ks, domain)
-	verifyImageOverride(t, ks, image)
-	verifyCerts(t, ks)
 }
 
 func mockNetworkConfig(networks []string) *configv1.Network {
@@ -104,5 +141,24 @@ func verifyImageOverride(t *testing.T, ks *servingv1alpha1.KnativeServing, expec
 func verifyCerts(t *testing.T, ks *servingv1alpha1.KnativeServing) {
 	if ks.Spec.ControllerCustomCerts == (servingv1alpha1.CustomCerts{}) {
 		t.Error("Missing custom certs config")
+	}
+}
+
+func verifyDefaultHA(t *testing.T, ks *servingv1alpha1.KnativeServing) {
+	verifyHA(t, ks, 2)
+}
+
+func verifyOverriddenHA(t *testing.T, ks *servingv1alpha1.KnativeServing) {
+	verifyHA(t, ks, 1)
+}
+
+func verifyHA(t *testing.T, ks *servingv1alpha1.KnativeServing, replicas int32) {
+	if ks.Spec.HighAvailability == nil {
+		t.Error("Missing HA")
+		return
+	}
+
+	if ks.Spec.HighAvailability.Replicas != replicas {
+		t.Errorf("Wrong ha replica size: expected%v, got %v", replicas, ks.Spec.HighAvailability.Replicas)
 	}
 }
