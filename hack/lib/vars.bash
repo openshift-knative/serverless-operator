9ca12806a4862aa806a6ef833895377301b5a8f2
Markus Thömmes <markusthoemmes@me.com>
2020-04-17 10:24:14 +0200

    Switch test-infra to knative.dev format too.

--- hack/lib/vars.bash
+++ hack/lib/vars.bash
@@ -3,7 +3,7 @@
 readonly BUILD_NUMBER=${BUILD_NUMBER:-$(uuidgen)}
 
 # shellcheck disable=SC1091,SC1090
-source "$(dirname "${BASH_SOURCE[0]}")/../../test/vendor/github.com/knative/test-infra/scripts/e2e-tests.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/../../test/vendor/knative.dev/test-infra/scripts/e2e-tests.sh"
 
 readonly KNATIVE_VERSION="${KNATIVE_VERSION:-v0.13.2}"
 readonly KNATIVE_SERVING_REPO="${KNATIVE_SERVING_REPO:-"https://github.com/openshift/knative-serving.git"}"
