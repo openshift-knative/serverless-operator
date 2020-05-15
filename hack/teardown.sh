6c9728214b3c17de96c795ecaa5ad3411089b03c
Ali Ok <aliok@redhat.com>
2020-05-15 11:19:03 +0300

    Enable tracing for upstream eventing tests

--- hack/teardown.sh
+++ hack/teardown.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-# This script can be used to teardown Serverless on a configured cluster. 
-#
-# This script will:
-#  
-#  * Remove Knative Serving custom resource
-#  * Uninstall Serverless Operator
-#  * Remove Catalog Source
-#  * Unregister namespaces from Service Mesh and remove them
-#
-
 # shellcheck disable=SC1091,SC1090
 source "$(dirname "${BASH_SOURCE[0]}")/lib/__sources__.bash"
 
@@ -18,7 +8,8 @@ set -Eeuo pipefail
 exitcode=0
 
 (( !exitcode )) && teardown_serverless || exitcode=2
-(( !exitcode )) && delete_catalog_source || exitcode=3
-(( !exitcode )) && delete_namespaces || exitcode=4
+(( !exitcode )) && teardown_tracing || exitcode=3
+(( !exitcode )) && delete_catalog_source || exitcode=4
+(( !exitcode )) && delete_namespaces || exitcode=5
 
 exit $exitcode
