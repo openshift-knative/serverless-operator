f1a11699821e2a70e1fc525a9c52c472809b9f51
Martin Gencur <mgencur@redhat.com>
2020-06-09 17:24:22 +0200

    Do not run must-gather on failure

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -11,34 +11,9 @@ if [ -n "$OPENSHIFT_BUILD_NAMESPACE" ]; then
 fi
 debugging.setup
 
-register_teardown || exit $?
-scale_up_workers || exit $?
-create_namespaces || exit $?
-create_htpasswd_users && add_roles || exit $?
+failed=1
+rm -f "${KUBECONFIG}" # Give must-gather signal to not run
 
-failed=0
-
-(( !failed )) && install_catalogsource || failed=3
-(( !failed )) && logger.success '🚀 Cluster prepared for testing.'
-
-# Run serverless-operator specific tests.
-(( !failed )) && serverless_operator_e2e_tests || failed=4
-
-# Run upstream knative serving & eventing operator tests
-(( !failed )) && deploy_serverless_operator_latest || failed=11
-(( !failed )) && knative_serving_operator_tests || failed=12
-(( !failed )) && knative_eventing_operator_tests || failed=14
-
-# Run upstream knative serving & eventing tests
-(( !failed )) && ensure_serverless_installed || failed=15
-
-# Run knative serving additional e2e tests
-(( !failed )) && downstream_serving_e2e_tests || failed=5
-
-(( !failed )) && upstream_knative_serving_e2e_and_conformance_tests || failed=16
-(( !failed )) && knative_eventing_tests || failed=17
-
-(( failed )) && dump_state
 (( failed )) && exit $failed
 
 success
--- test/upgrade-tests.sh
+++ test/upgrade-tests.sh
@@ -11,39 +11,8 @@ if [ -n "$OPENSHIFT_BUILD_NAMESPACE" ]; then
 fi
 debugging.setup
 
-scale_up_workers || exit $?
-create_namespaces || exit $?
-create_htpasswd_users && add_roles || exit $?
-
-failed=0
-
-(( !failed )) && install_catalogsource || failed=3
-(( !failed )) && logger.success 'Cluster prepared for testing.'
-
-(( !failed )) && install_serverless_previous || failed=5
-(( !failed )) && run_knative_serving_rolling_upgrade_tests || failed=6
-
-echo ">>> Knative Servings"
-oc get knativeserving.operator.knative.dev --all-namespaces -o yaml
-
-echo ">>> Knative Services"
-oc get ksvc --all-namespaces
-
-echo ">>> Triggering GC"
-for pod in $(oc get pod -n openshift-kube-controller-manager -l kube-controller-manager=true -o custom-columns=name:metadata.name --no-headers); do
-  echo "killing pod $pod"
-  oc rsh -n openshift-kube-controller-manager "$pod" /bin/sh -c "kill 1"
-  sleep 30
-done
-
-echo "Sleeping so GC can run"
-sleep 120
-
-echo ">>> Knative Servings"
-oc get knativeserving.operator.knative.dev --all-namespaces -o yaml
-
-echo ">>> Knative Services"
-oc get ksvc --all-namespaces
+failed=1
+rm -f "${KUBECONFIG}" # Give must-gather signal to not run
 
 (( failed )) && dump_state
 (( failed )) && exit $failed
