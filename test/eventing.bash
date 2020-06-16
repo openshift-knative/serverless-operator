d8d3fa824821f098faccec34422b39e69010235e
Matthias Wessendorf <mwessend@redhat.com>
2020-06-16 09:26:22 +0200

    Updatr operator and test invoke
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- test/eventing.bash
+++ test/eventing.bash
@@ -2,19 +2,24 @@
 
 function knative_eventing_tests {
   (
-  local exitstatus=0
   logger.info 'Running eventing tests'
 
   cd "$KNATIVE_EVENTING_HOME" || return $?
 
   image_template="registry.svc.ci.openshift.org/openshift/knative-${KNATIVE_EVENTING_VERSION}:knative-eventing-test-{{.Name}}"
 
-  go_test_e2e -timeout=90m -parallel=1 ./test/e2e \
+  go_test_e2e -timeout=90m -parallel=12 ./test/e2e -brokerclass=ChannelBasedBroker -channels=messaging.knative.dev/v1alpha1:InMemoryChannel,messaging.knative.dev/v1alpha1:Channel,messaging.knative.dev/v1beta1:InMemoryChannel \
     --kubeconfig "$KUBECONFIG" \
-    --imagetemplate "$image_template" \
-    || exitstatus=$? && true
+    --imagetemplate "$image_template" || failed=1
 
-  print_test_result ${exitstatus}
+  oc patch cm config-br-defaults -n knative-eventing -p '{"data":{"default-br-config":"clusterDefault:\n  brokerClass: MTChannelBasedBroker\n  apiVersion: v1\n  kind: ConfigMap\n  name: config-br-default-channel\n  namespace: knative-eventing\n"}}' --type=merge || failed=2
+
+  go_test_e2e -timeout=90m -parallel=12 ./test/e2e -brokerclass=MTChannelBasedBroker -channels=messaging.knative.dev/v1alpha1:InMemoryChannel,messaging.knative.dev/v1alpha1:Channel,messaging.knative.dev/v1beta1:InMemoryChannel \
+    --kubeconfig "$KUBECONFIG" \
+    --imagetemplate "$image_template" || failed=3
+
+
+  print_test_result ${failed}
 
   return $exitstatus
   )
