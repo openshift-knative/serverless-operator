b02d9411b73676c9e0d832f6479c2c45935caa4b
Matthias Wessendorf <mwessend@redhat.com>
2020-06-22 12:58:13 +0200

    Patching the config, to make sure we do like on upstream, and set first the deprecated broker on test run
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- test/eventing.bash
+++ test/eventing.bash
@@ -9,6 +9,8 @@ function knative_eventing_tests {
 
   image_template="registry.svc.ci.openshift.org/openshift/knative-${KNATIVE_EVENTING_VERSION}:knative-eventing-test-{{.Name}}"
 
+  oc patch cm config-br-defaults -n knative-eventing -p '{"data":{"default-br-config":"clusterDefault:\n  brokerClass: ChannelBasedBroker\n  apiVersion: v1\n  kind: ConfigMap\n  name: config-br-default-channel\n  namespace: knative-eventing\n"}}' --type=merge || failed=2
+
   go_test_e2e -timeout=90m -parallel=12 ./test/e2e -brokerclass=ChannelBasedBroker -channels=messaging.knative.dev/v1alpha1:InMemoryChannel,messaging.knative.dev/v1alpha1:Channel,messaging.knative.dev/v1beta1:InMemoryChannel \
     --kubeconfig "$KUBECONFIG" \
     --imagetemplate "$image_template" || failed=1
