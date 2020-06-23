6a5a873c3ca7284579452f65135c76bf46ea9367
Matthias Wessendorf <mwessend@redhat.com>
2020-06-23 07:56:27 +0200

    Patch config to use explicit broker class (#348)
    * Patching the config, to make sure we do like on upstream, and set first the deprecated broker on test run
    
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>
    
    * change numbers...
    
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>
    
    * Patching the KnativeEventing CR instead
    
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- test/eventing.bash
+++ test/eventing.bash
@@ -9,15 +9,23 @@ function knative_eventing_tests {
 
   image_template="registry.svc.ci.openshift.org/openshift/knative-${KNATIVE_EVENTING_VERSION}:knative-eventing-test-{{.Name}}"
 
+  oc patch KnativeEventing knative-eventing \
+    --namespace knative-eventing \
+    --type merge \
+    --patch '{"spec":{"defaultBrokerClass":"ChannelBasedBroker"}}' || failed=1
+
   go_test_e2e -timeout=90m -parallel=12 ./test/e2e -brokerclass=ChannelBasedBroker -channels=messaging.knative.dev/v1alpha1:InMemoryChannel,messaging.knative.dev/v1alpha1:Channel,messaging.knative.dev/v1beta1:InMemoryChannel \
     --kubeconfig "$KUBECONFIG" \
-    --imagetemplate "$image_template" || failed=1
+    --imagetemplate "$image_template" || failed=2
 
-  oc patch cm config-br-defaults -n knative-eventing -p '{"data":{"default-br-config":"clusterDefault:\n  brokerClass: MTChannelBasedBroker\n  apiVersion: v1\n  kind: ConfigMap\n  name: config-br-default-channel\n  namespace: knative-eventing\n"}}' --type=merge || failed=2
+  oc patch KnativeEventing knative-eventing \
+    --namespace knative-eventing \
+    --type merge \
+    --patch '{"spec":{"defaultBrokerClass":"MTChannelBasedBroker"}}' || failed=3
 
   go_test_e2e -timeout=90m -parallel=12 ./test/e2e -brokerclass=MTChannelBasedBroker -channels=messaging.knative.dev/v1alpha1:InMemoryChannel,messaging.knative.dev/v1alpha1:Channel,messaging.knative.dev/v1beta1:InMemoryChannel \
     --kubeconfig "$KUBECONFIG" \
-    --imagetemplate "$image_template" || failed=3
+    --imagetemplate "$image_template" || failed=4
 
 
   print_test_result ${failed}
