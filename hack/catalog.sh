afed45e2a6b9942dbbd71fbfb108b3462e97dcda
Warren Van Winckel <wvanwinc@redhat.com>
2020-06-25 23:26:31 -0700

    Update with 1.8.0-rc1 references

--- hack/catalog.sh
+++ hack/catalog.sh
@@ -30,8 +30,8 @@ elif [ -n "$DOCKER_REPO_OVERRIDE" ]; then
   export IMAGE_KNATIVE_OPERATOR="${DOCKER_REPO_OVERRIDE}/knative-operator"
   export IMAGE_KNATIVE_OPENSHIFT_INGRESS="${DOCKER_REPO_OVERRIDE}/knative-openshift-ingress"
 else
-  export IMAGE_KNATIVE_OPERATOR="registry.svc.ci.openshift.org/openshift/openshift-serverless-nightly:knative-operator"
-  export IMAGE_KNATIVE_OPENSHIFT_INGRESS="registry.svc.ci.openshift.org/openshift/openshift-serverless-nightly:knative-openshift-ingress"
+  export IMAGE_KNATIVE_OPERATOR="registry.svc.ci.openshift.org/openshift/openshift-serverless-v1.8.0-rc1:knative-operator"
+  export IMAGE_KNATIVE_OPENSHIFT_INGRESS="registry.svc.ci.openshift.org/openshift/openshift-serverless-v1.8.0-rc1:knative-openshift-ingress"
 fi
 
 CRD=$(cat $(ls $CRD_DIR/*) | grep -v -- "---" | indent apiVersion)
