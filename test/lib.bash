d14d026a622c26688e10224d5027f7119e249c91
Jim Crossley <jim@crossleys.org>
2020-07-13 10:56:05 -0400

    Trying to prevent being locked out of my dev cluster

--- test/lib.bash
+++ test/lib.bash
@@ -170,7 +170,7 @@ function create_htpasswd_users {
   logger.info 'Generate kubeconfig for each user'
   for i in $(seq 1 $num_users); do
     cp "${KUBECONFIG}" "user${i}.kubeconfig"
-    occmd="bash -c '! oc login --config=user${i}.kubeconfig --username=user${i} --password=password${i} > /dev/null'"
+    occmd="bash -c '! oc login --insecure-skip-tls-verify=true --config=user${i}.kubeconfig --username=user${i} --password=password${i} > /dev/null'"
     timeout 900 "${occmd}" || return 1
   done
 }
