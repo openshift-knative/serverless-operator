9e2cb9e9cc5487a3c8f1eb43e4b5d9b761142c3a
Martin Gencur <mgencur@redhat.com>
2020-07-28 09:20:40 +0200

    Reuse test execution for Eventing from knative-eventing-src (#346)
    * Reuse test execution for Eventing from knative-eventing-src
    
    * Export all variables rather than making them readonly

--- test/eventing.bash
+++ test/eventing.bash
@@ -2,31 +2,18 @@
 
 function knative_eventing_tests {
   (
-  local failed=0
   logger.info 'Running eventing tests'
+
+  local failed=0
+
+  TEST_IMAGE_TEMPLATE="registry.svc.ci.openshift.org/openshift/knative-${KNATIVE_EVENTING_VERSION}:knative-eventing-test-{{.Name}}"
 
   cd "$KNATIVE_EVENTING_HOME" || return $?
 
-  image_template="registry.svc.ci.openshift.org/openshift/knative-${KNATIVE_EVENTING_VERSION}:knative-eventing-test-{{.Name}}"
+  source "${KNATIVE_EVENTING_HOME}/openshift/e2e-common.sh"
 
-  oc patch KnativeEventing knative-eventing \
-    --namespace knative-eventing \
-    --type merge \
-    --patch '{"spec":{"defaultBrokerClass":"ChannelBasedBroker"}}' || failed=1
-
-  go_test_e2e -timeout=90m -parallel=12 ./test/e2e -brokerclass=ChannelBasedBroker -channels=messaging.knative.dev/v1alpha1:InMemoryChannel,messaging.knative.dev/v1alpha1:Channel,messaging.knative.dev/v1beta1:InMemoryChannel \
-    --kubeconfig "$KUBECONFIG" \
-    --imagetemplate "$image_template" || failed=2
-
-  oc patch KnativeEventing knative-eventing \
-    --namespace knative-eventing \
-    --type merge \
-    --patch '{"spec":{"defaultBrokerClass":"MTChannelBasedBroker"}}' || failed=3
-
-  go_test_e2e -timeout=90m -parallel=12 ./test/e2e -brokerclass=MTChannelBasedBroker -channels=messaging.knative.dev/v1alpha1:InMemoryChannel,messaging.knative.dev/v1alpha1:Channel,messaging.knative.dev/v1beta1:InMemoryChannel \
-    --kubeconfig "$KUBECONFIG" \
-    --imagetemplate "$image_template" || failed=4
-
+  # run_e2e_tests defined in knative-eventing
+  run_e2e_tests || failed=$?
 
   print_test_result ${failed}
 
