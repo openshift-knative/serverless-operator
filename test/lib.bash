da801490199d2bce6c28c5751ac67a90722e1998
Markus Thömmes <markusthoemmes@me.com>
2020-08-03 16:00:01 +0200

    Roll back breaking deprecation fix.

--- test/lib.bash
+++ test/lib.bash
@@ -166,7 +166,7 @@ function create_htpasswd_users {
   kubectl create secret generic htpass-secret \
     --from-file=htpasswd="$(pwd)/users.htpasswd" \
     -n openshift-config \
-    --dry-run=client -o yaml | kubectl apply -f -
+    --dry-run -o yaml | kubectl apply -f -
   oc apply -f openshift/identity/htpasswd.yaml
 
   logger.info 'Generate kubeconfig for each user'
