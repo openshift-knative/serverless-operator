963ea4e7ffefc0630abbf19862910424dc7a6090
Stavros Kontopoulos <skontopo@redhat.com>
2020-09-16 19:30:40 +0300

    disable cr metrics port (#454)

--- knative-operator/pkg/common/service_monitor.go
+++ knative-operator/pkg/common/service_monitor.go
@@ -23,7 +23,7 @@ func SetupServiceMonitor(ctx context.Context, cfg *rest.Config, api client.Clien
 	// Add to the below struct any other metrics ports you want to expose.
 	servicePorts := []v1.ServicePort{
 		{Port: metricsPort, Name: metrics.OperatorPortName, Protocol: v1.ProtocolTCP, TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: metricsPort}},
-		{Port: operatorMetricsPort, Name: metrics.CRPortName, Protocol: v1.ProtocolTCP, TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: operatorMetricsPort}},
+	//	{Port: operatorMetricsPort, Name: metrics.CRPortName, Protocol: v1.ProtocolTCP, TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: operatorMetricsPort}},
 	}
 	// Create Service object to expose the metrics port(s).
 	service, err := metrics.CreateMetricsService(ctx, cfg, servicePorts)
