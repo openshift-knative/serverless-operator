25b99a952b7572cff0a91a75ceb0718cd602dd96
Ali Ok <aliok@redhat.com>
2020-09-24 14:19:33 +0300

    Disable KnativeKafka controller for now

--- knative-operator/pkg/controller/add_knativekafka.go
+++ knative-operator/pkg/controller/add_knativekafka.go
@@ -1,10 +1,11 @@
 package controller
 
-import (
-	"github.com/openshift-knative/serverless-operator/knative-operator/pkg/controller/knativekafka"
-)
+//import (
+//	"github.com/openshift-knative/serverless-operator/knative-operator/pkg/controller/knativekafka"
+//)
 
 func init() {
 	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
-	AddToManagerFuncs = append(AddToManagerFuncs, knativekafka.Add)
+	// TODO: temp change to disable KnativeKafka reconciliation
+	// AddToManagerFuncs = append(AddToManagerFuncs, knativekafka.Add)
 }
