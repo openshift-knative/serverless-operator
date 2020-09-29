0b37e765b22c438111015e944c43877c06f1b2a4
Ali Ok <aliok@redhat.com>
2020-09-29 16:25:59 +0300

    Reuse `ConditionType`s from upstream operator

--- knative-operator/pkg/apis/operator/v1alpha1/knativekafka_lifecycle.go
+++ knative-operator/pkg/apis/operator/v1alpha1/knativekafka_lifecycle.go
@@ -1,24 +1,13 @@
 package v1alpha1
 
 import "knative.dev/pkg/apis"
-
-const (
-	// DependenciesInstalled is a Condition indicating that potential dependencies have
-	// been installed correctly.
-	DependenciesInstalled apis.ConditionType = "DependenciesInstalled"
-	// InstallSucceeded is a Condition indiciating that the installation of the component
-	// itself has been successful.
-	InstallSucceeded apis.ConditionType = "InstallSucceeded"
-	// DeploymentsAvailable is a Condition indicating whether or not the Deployments of
-	// the respective component have come up successfully.
-	DeploymentsAvailable apis.ConditionType = "DeploymentsAvailable"
-)
+import knativeoperatorv1alpha1 "knative.dev/operator/pkg/apis/operator/v1alpha1"
 
 var (
 	kafkaCondSet = apis.NewLivingConditionSet(
-		DependenciesInstalled,
-		DeploymentsAvailable,
-		InstallSucceeded,
+		knativeoperatorv1alpha1.DependenciesInstalled,
+		knativeoperatorv1alpha1.DeploymentsAvailable,
+		knativeoperatorv1alpha1.InstallSucceeded,
 	)
 )
 
@@ -31,12 +20,12 @@ func (is *KnativeKafkaStatus) IsReady() bool {
 // given message.
 func (is *KnativeKafkaStatus) MarkDependencyInstalling(msg string) {
 	kafkaCondSet.Manage(is).MarkFalse(
-		DependenciesInstalled,
+		knativeoperatorv1alpha1.DependenciesInstalled,
 		"Installing",
 		"Dependency installing: %s", msg)
 }
 
 // MarkDependenciesInstalled marks the DependenciesInstalled status as true.
 func (is *KnativeKafkaStatus) MarkDependenciesInstalled() {
-	kafkaCondSet.Manage(is).MarkTrue(DependenciesInstalled)
+	kafkaCondSet.Manage(is).MarkTrue(knativeoperatorv1alpha1.DependenciesInstalled)
 }
