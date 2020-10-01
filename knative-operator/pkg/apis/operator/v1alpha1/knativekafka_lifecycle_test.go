4c57438b9bd2d0c9609662e375f839425003c182
Ali Ok <aliok@redhat.com>
2020-10-01 14:34:46 +0300

    Update KnativeKafka lifecycle unit test

--- knative-operator/pkg/apis/operator/v1alpha1/knativekafka_lifecycle_test.go
+++ knative-operator/pkg/apis/operator/v1alpha1/knativekafka_lifecycle_test.go
@@ -11,12 +11,27 @@ func TestKnativeKafkaHappyPath(t *testing.T) {
 	ks := &KnativeKafkaStatus{}
 	ks.InitializeConditions()
 
+	apistest.CheckConditionOngoing(ks, knativeoperatorv1alpha1.DeploymentsAvailable, t)
 	apistest.CheckConditionOngoing(ks, knativeoperatorv1alpha1.InstallSucceeded, t)
 
 	// Install succeeds.
 	ks.MarkInstallSucceeded()
+	// Dependencies are assumed successful too.
+	apistest.CheckConditionOngoing(ks, knativeoperatorv1alpha1.DeploymentsAvailable, t)
 	apistest.CheckConditionSucceeded(ks, knativeoperatorv1alpha1.InstallSucceeded, t)
 
+	// Deployments are not available at first.
+	ks.MarkDeploymentsNotReady()
+	apistest.CheckConditionFailed(ks, knativeoperatorv1alpha1.DeploymentsAvailable, t)
+	apistest.CheckConditionSucceeded(ks, knativeoperatorv1alpha1.InstallSucceeded, t)
+	if ready := ks.IsReady(); ready {
+		t.Errorf("ks.IsReady() = %v, want false", ready)
+	}
+
+	// Deployments become ready and we're good.
+	ks.MarkDeploymentsAvailable()
+	apistest.CheckConditionSucceeded(ks, knativeoperatorv1alpha1.DeploymentsAvailable, t)
+	apistest.CheckConditionSucceeded(ks, knativeoperatorv1alpha1.InstallSucceeded, t)
 	if ready := ks.IsReady(); !ready {
 		t.Errorf("ks.IsReady() = %v, want true", ready)
 	}
@@ -26,17 +41,25 @@ func TestKnativeKafkaErrorPath(t *testing.T) {
 	ks := &KnativeKafkaStatus{}
 	ks.InitializeConditions()
 
+	apistest.CheckConditionOngoing(ks, knativeoperatorv1alpha1.DeploymentsAvailable, t)
 	apistest.CheckConditionOngoing(ks, knativeoperatorv1alpha1.InstallSucceeded, t)
 
 	// Install fails.
 	ks.MarkInstallFailed("test")
+	apistest.CheckConditionOngoing(ks, knativeoperatorv1alpha1.DeploymentsAvailable, t)
 	apistest.CheckConditionFailed(ks, knativeoperatorv1alpha1.InstallSucceeded, t)
+
+	// Install now succeeds.
+	ks.MarkInstallSucceeded()
+	apistest.CheckConditionOngoing(ks, knativeoperatorv1alpha1.DeploymentsAvailable, t)
+	apistest.CheckConditionSucceeded(ks, knativeoperatorv1alpha1.InstallSucceeded, t)
 	if ready := ks.IsReady(); ready {
 		t.Errorf("ks.IsReady() = %v, want false", ready)
 	}
 
-	// Install now succeeds.
-	ks.MarkInstallSucceeded()
+	// Deployments become ready
+	ks.MarkDeploymentsAvailable()
+	apistest.CheckConditionSucceeded(ks, knativeoperatorv1alpha1.DeploymentsAvailable, t)
 	apistest.CheckConditionSucceeded(ks, knativeoperatorv1alpha1.InstallSucceeded, t)
 	if ready := ks.IsReady(); !ready {
 		t.Errorf("ks.IsReady() = %v, want true", ready)
