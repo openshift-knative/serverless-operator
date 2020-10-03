8b9a2ff897155b9671225ac9b9af961e14539350
Ali Ok <aliok@redhat.com>
2020-10-03 23:02:28 +0300

    Actually run upstream eventing-contrib e2e tests

--- test/eventing-contrib.bash
+++ test/eventing-contrib.bash
@@ -6,14 +6,20 @@ function upstream_knative_eventing_contrib_e2e {
 
   local failed=0
 
-  TEST_IMAGE_TEMPLATE="registry.svc.ci.openshift.org/openshift/knative-${KNATIVE_EVENTING_CONTRIB_VERSION}:knative-eventing-contrib-test-{{.Name}}"
+  TEST_IMAGE_TEMPLATE="registry.svc.ci.openshift.org/openshift/knative-${KNATIVE_EVENTING_CONTRIB_VERSION}:knative-eventing-test-{{.Name}}"
 
   cd "$KNATIVE_EVENTING_CONTRIB_HOME" || return $?
 
   source "${KNATIVE_EVENTING_CONTRIB_HOME}/openshift/e2e-common.sh"
 
+  failed=0
+
+  logger.info 'Installing Strimzi'
+  (( !failed )) && install_strimzi || failed=$?
+
   # run_e2e_tests defined in eventing-contrib
-  run_e2e_tests || failed=$?
+  logger.info 'Starting eventing-contrib tests'
+  (( !failed )) && run_e2e_tests || failed=$?
 
   print_test_result ${failed}
 
