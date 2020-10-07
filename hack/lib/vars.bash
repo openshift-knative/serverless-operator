41d542b3e9499a24105a5d9bb8a5f98fa8376baf
Chris Suszyński <ksuszyns@redhat.com>
2020-10-07 12:20:32 +0200

    Choosing first channel from the list instead of last

--- hack/lib/vars.bash
+++ hack/lib/vars.bash
@@ -50,8 +50,8 @@ export UPGRADE_OCP_IMAGE="${UPGRADE_OCP_IMAGE:-}"
 export INSTALL_PREVIOUS_VERSION="${INSTALL_PREVIOUS_VERSION:-"false"}"
 
 
-# Using last channel on the list, instead of default one
-export OLM_CHANNEL="${OLM_CHANNEL:-$(metadata.get 'olm.channels.list[*]' | tail -n 1)}"
+# Using first channel on the list, instead of default one
+export OLM_CHANNEL="${OLM_CHANNEL:-$(metadata.get 'olm.channels.list[*]' | head -n 1)}"
 # Change this when upgrades need switching to a different channel
 export OLM_UPGRADE_CHANNEL="${OLM_UPGRADE_CHANNEL:-"$OLM_CHANNEL"}"
 export OLM_SOURCE="${OLM_SOURCE:-"$OPERATOR"}"
