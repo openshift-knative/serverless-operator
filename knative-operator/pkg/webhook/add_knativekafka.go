e170e7eecaff65a7e081b6b41eb0b30041ecbc1a
Ali Ok <aliok@redhat.com>
2020-10-12 13:33:38 +0300

    Disable webhook for KnativeKafka

--- knative-operator/pkg/webhook/add_knativekafka.go
+++ knative-operator/pkg/webhook/add_knativekafka.go
@@ -1,9 +1,10 @@
 package webhook
 
-import (
-	kk "github.com/openshift-knative/serverless-operator/knative-operator/pkg/webhook/knativekafka"
-)
-
-func init() {
-	AddToManagerFuncs = append(AddToManagerFuncs, kk.ValidatingWebhook)
-}
+//
+//import (
+//	kk "github.com/openshift-knative/serverless-operator/knative-operator/pkg/webhook/knativekafka"
+//)
+//
+//func init() {
+//	AddToManagerFuncs = append(AddToManagerFuncs, kk.ValidatingWebhook)
+//}
