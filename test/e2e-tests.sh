8519feec11d07614c7a2bc00f2f77b75dd67fa64
Matthias Wessendorf <mwessend@redhat.com>
2020-10-15 17:06:18 +0200

    Adding test targets
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -27,6 +27,10 @@ failed=0
 (( !failed )) && downstream_serving_e2e_tests || failed=4
 (( !failed )) && downstream_eventing_e2e_tests || failed=5
 
+if [[ $TEST_KNATIVE_KAFKA == true ]]; then
+  (( !failed )) && downstream_knative_kafka_e2e_tests || failed=6
+fi
+
 (( failed )) && dump_state
 (( failed )) && exit $failed
 
