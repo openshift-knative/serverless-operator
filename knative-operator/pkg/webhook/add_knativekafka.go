c1e79b044948ab03b0fd9205b2f639ecef72b652
Ali Ok <aliok@redhat.com>
2020-10-15 13:55:32 +0300

    DO NOT MERGE - Gradual changes - 02

--- knative-operator/pkg/webhook/add_knativekafka.go
+++ knative-operator/pkg/webhook/add_knativekafka.go
@@ -1,10 +1,9 @@
 package webhook
 
-//
-//import (
-//	kk "github.com/openshift-knative/serverless-operator/knative-operator/pkg/webhook/knativekafka"
-//)
-//
-//func init() {
-//	AddToManagerFuncs = append(AddToManagerFuncs, kk.ValidatingWebhook)
-//}
+import (
+	kk "github.com/openshift-knative/serverless-operator/knative-operator/pkg/webhook/knativekafka"
+)
+
+func init() {
+	AddToManagerFuncs = append(AddToManagerFuncs, kk.ValidatingWebhook)
+}
