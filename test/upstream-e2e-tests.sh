9c2752a714ba7450d5ef99ac7983911f0c686f49
Matthias Wessendorf <mwessend@redhat.com>
2020-10-16 07:39:00 +0200

    Run Kafka, than Eventing, last serving
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- test/upstream-e2e-tests.sh
+++ test/upstream-e2e-tests.sh
@@ -39,10 +39,10 @@ if [[ $TEST_KNATIVE_E2E == true ]]; then
   SCALE_UP=6 scale_up_workers || failed=10
   (( !failed )) && ensure_serverless_installed || failed=7
   if [[ $TEST_KNATIVE_KAFKA == true ]]; then
-    (( !failed )) && upstream_knative_eventing_contrib_e2e || failed=10
+    (( !failed )) && upstream_knative_eventing_contrib_e2e || failed=8
   fi
-  (( !failed )) && upstream_knative_serving_e2e_and_conformance_tests || failed=8
   (( !failed )) && upstream_knative_eventing_e2e || failed=9
+  (( !failed )) && upstream_knative_serving_e2e_and_conformance_tests || failed=10
 fi
 
 (( failed )) && dump_state
