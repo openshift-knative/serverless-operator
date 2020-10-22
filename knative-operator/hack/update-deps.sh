e44d5c2bb754167b985dcfe5cf3022bf118ace99
Markus Thömmes <markusthoemmes@me.com>
2020-10-22 09:12:35 +0200

    Use new cleanup command from upstream

--- knative-operator/hack/update-deps.sh
+++ knative-operator/hack/update-deps.sh
@@ -13,10 +13,4 @@ go mod vendor
 # TODO: Remove this once we bump kubernetes versions.
 git apply "$ROOT_DIR/hack/manifestival.patch"
 
-rm -rf $(find vendor/ -name 'OWNERS')
-# Remove unit tests & e2e tests.
-rm -rf $(find vendor/ -path '*/pkg/*_test.go')
-rm -rf $(find vendor/ -path '*/e2e/*_test.go')
-
-# Add permission for shell scripts
-chmod +x $(find vendor -type f -name '*.sh')
+find vendor/ \( -name "OWNERS" -o -name "OWNERS_ALIASES" -o -name "BUILD" -o -name "BUILD.bazel" -o -name "*_test.go" \) -print0 | xargs -0 rm -f
