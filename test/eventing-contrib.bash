08b67a2696b3fbd9c6a5d0b296121a0e49bf30c7
Chris Suszyński <ksuszyns@redhat.com>
2020-10-23 23:54:31 +0200

    Random namespace pipefail free

--- test/eventing-contrib.bash
+++ test/eventing-contrib.bash
@@ -3,14 +3,16 @@
 function upstream_knative_eventing_contrib_e2e {
   logger.info 'Running eventing-contrib tests'
 
+  local randomns
   export TEST_IMAGE_TEMPLATE="registry.svc.ci.openshift.org/openshift/knative-${KNATIVE_EVENTING_CONTRIB_VERSION}:knative-eventing-test-{{.Name}}"
 
   cd "$KNATIVE_EVENTING_CONTRIB_HOME" || return $?
 
   # This the namespace used to install and test Knative Eventing-Contrib.
+  randomns="knative-eventing-$(LC_ALL=C dd if=/dev/urandom bs=256 count=1 2> /dev/null \
+    | tr -dc 'a-z0-9' | fold -w 10 | head -n 1)"
+  TEST_EVENTING_NAMESPACE="${TEST_EVENTING_NAMESPACE:-"${randomns}"}"
   export TEST_EVENTING_NAMESPACE
-  TEST_EVENTING_NAMESPACE="${TEST_EVENTING_NAMESPACE:-"knative-eventing-"$(cat /dev/urandom \
-    | tr -dc 'a-z0-9' | fold -w 10 | head -n 1)}"
 
   export KNATIVE_DEFAULT_NAMESPACE
   KNATIVE_DEFAULT_NAMESPACE="knative-eventing"
