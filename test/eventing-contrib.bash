6edd993721d096a5d5ef89e17af315a77d238e70
Chris Suszyński <ksuszyns@redhat.com>
2020-10-23 17:49:06 +0200

    Fix style of eventing-contrib

--- test/eventing-contrib.bash
+++ test/eventing-contrib.bash
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
 function upstream_knative_eventing_contrib_e2e {
-  (
   logger.info 'Running eventing-contrib tests'
 
-  local failed=0
-
-  TEST_IMAGE_TEMPLATE="registry.svc.ci.openshift.org/openshift/knative-${KNATIVE_EVENTING_CONTRIB_VERSION}:knative-eventing-test-{{.Name}}"
+  export TEST_IMAGE_TEMPLATE="registry.svc.ci.openshift.org/openshift/knative-${KNATIVE_EVENTING_CONTRIB_VERSION}:knative-eventing-test-{{.Name}}"
 
   cd "$KNATIVE_EVENTING_CONTRIB_HOME" || return $?
 
@@ -22,21 +19,16 @@ function upstream_knative_eventing_contrib_e2e {
   export CONFIG_TRACING_CONFIG
   CONFIG_TRACING_CONFIG="test/config/config-tracing.yaml"
 
+  logger.info 'Installing Tracing'
   install_tracing
 
+  # shellcheck disable=SC1090
   source "${KNATIVE_EVENTING_CONTRIB_HOME}/openshift/e2e-common.sh"
 
-  failed=0
-
   logger.info 'Installing Strimzi'
-  (( !failed )) && install_strimzi || failed=$?
+  install_strimzi
 
   # run_e2e_tests defined in eventing-contrib
   logger.info 'Starting eventing-contrib tests'
-  (( !failed )) && run_e2e_tests || failed=$?
-
-  print_test_result ${failed}
-
-  return $failed
-  )
+  run_e2e_tests
 }
