2fc255a7aa5612a41a6e4bf672fc7d73fb0bad17
Chris Suszyński <ksuszyns@redhat.com>
2020-10-24 01:30:26 +0200

    Readjusting to use valid command
    Fail seen at: https://prow.ci.openshift.org/view/gs/origin-ci-test/pr-logs/pull/openshift-knative_serverless-operator/613/pull-ci-openshift-knative-serverless-operator-master-4.6-upstream-e2e-aws-ocp-46/1319760687093780480#1:build-log.txt%3A1108

--- hack/lib/tracing.bash
+++ hack/lib/tracing.bash
@@ -63,7 +63,7 @@ EOF
 }
 
 function enable_eventing_tracing {
-  header_text "Configuring tracing for Eventing"
+  logger.info "Configuring tracing for Eventing"
 
   cat <<EOF | oc apply -f -
 apiVersion: v1
