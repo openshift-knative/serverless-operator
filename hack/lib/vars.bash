c959e25433bb9b7f6d2c318caf7ddea37d29ab4a
Warren Van Winckel <wvanwinc@redhat.com>
2020-11-27 17:32:07 -0800

    Bump SCALE_UP from 8 to 10

--- hack/lib/vars.bash
+++ hack/lib/vars.bash
@@ -37,7 +37,7 @@ readonly INTERACTIVE="${INTERACTIVE:-$(test -z "${GDMSESSION}"; echo $?)}"
 readonly KUBECONFIG="${KUBECONFIG:-$(realpath ~/.kube/config)}"
 readonly OPENSHIFT_CI="${OPENSHIFT_CI:-}"
 readonly OPERATOR="${OPERATOR:-serverless-operator}"
-readonly SCALE_UP="${SCALE_UP:-8}"
+readonly SCALE_UP="${SCALE_UP:-10}"
 
 readonly OLM_NAMESPACE="${OLM_NAMESPACE:-openshift-marketplace}"
 readonly OPERATORS_NAMESPACE="${OPERATORS_NAMESPACE:-openshift-operators}"
