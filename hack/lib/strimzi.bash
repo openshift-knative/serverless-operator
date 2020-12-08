1ec90fd858409d34f3a69284c729bbfdc5065e44
Martin Gencur <mgencur@redhat.com>
2020-12-08 13:45:53 +0100

    [release-1.12] Split Strimzi install function to be reusable downstream (#738)
    * Split Strimzi install function to be reusable downstream
    
    * Fix lint for strimzi.bash

--- hack/lib/strimzi.bash
+++ hack/lib/strimzi.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-function install_strimzi {
-  strimzi_version=`curl https://github.com/strimzi/strimzi-kafka-operator/releases/latest |  awk -F 'tag/' '{print $2}' | awk -F '"' '{print $1}' 2>/dev/null`
-  header "Strimzi install"
+function install_strimzi_operator {
+  strimzi_version=$(curl https://github.com/strimzi/strimzi-kafka-operator/releases/latest |  awk -F 'tag/' '{print $2}' | awk -F '"' '{print $1}' 2>/dev/null)
+  header "Installing Strimzi Kafka operator"
   oc create namespace kafka
   oc -n kafka apply --selector strimzi.io/crd-install=true -f "https://github.com/strimzi/strimzi-kafka-operator/releases/download/${strimzi_version}/strimzi-cluster-operator-${strimzi_version}.yaml"
   curl -L "https://github.com/strimzi/strimzi-kafka-operator/releases/download/${strimzi_version}/strimzi-cluster-operator-${strimzi_version}.yaml" \
@@ -11,7 +11,9 @@ function install_strimzi {
 
   # Wait for the CRD we need to actually be active
   oc wait crd --timeout=-1s kafkas.kafka.strimzi.io --for=condition=Established
+}
 
+function install_strimzi_cluster {
   header "Applying Strimzi Cluster file"
   cat <<-EOF | oc apply -f -
     apiVersion: kafka.strimzi.io/v1beta1
@@ -63,9 +65,11 @@ function install_strimzi {
         userOperator: {}
 EOF
 
-  header "Waiting for Strimzi to become ready"
+  header "Waiting for Strimzi cluster to become ready"
   oc wait kafka --all --timeout=-1s --for=condition=Ready -n kafka
+}
 
+function install_strimzi_users {
   header "Applying Strimzi TLS Admin user"
   cat <<-EOF | oc apply -f -
 apiVersion: kafka.strimzi.io/v1beta1
@@ -96,27 +100,36 @@ EOF
 
   header "Waiting for Strimzi admin users to become ready"
   oc wait kafkauser --all --timeout=-1s --for=condition=Ready -n kafka
-
 }
 
-function uninstall_strimzi {
-  strimzi_version=`curl https://github.com/strimzi/strimzi-kafka-operator/releases/latest |  awk -F 'tag/' '{print $2}' | awk -F '"' '{print $1}' 2>/dev/null`
+function install_strimzi {
+  header "Strimzi install"
+  install_strimzi_operator
+  install_strimzi_cluster
+  install_strimzi_users
+}
 
+function delete_strimzi_users {
   header "Deleting Strimzi users"
   oc -n kafka delete kafkauser.kafka.strimzi.io my-sasl-user
   oc -n kafka delete kafkauser.kafka.strimzi.io my-tls-user
 
-  header "Waiting for Kafka users to get deleted"
+  header "Waiting for Strimzi users to get deleted"
   timeout 600 "[[ \$(oc get kafkausers -n kafka -o jsonpath='{.items}') != '[]' ]]" || return 2
+}
 
-
-  header "Deleting Kafka instance"
+function delete_strimzi_cluster {
+  header "Deleting Strimzi cluster"
   oc delete kafka -n kafka my-cluster
 
-  header "Waiting for Kafka to get deleted"
+  header "Waiting for Strimzi cluster to get deleted"
   timeout 600 "[[ \$(oc get kafkas -n kafka -o jsonpath='{.items}') != '[]' ]]" || return 2
+}
 
-  header "Deleting Strimzi Cluster file"
+function delete_strimzi_operator {
+  header "Deleting Strimzi Kafka operator"
+  strimzi_version=$(curl https://github.com/strimzi/strimzi-kafka-operator/releases/latest |  awk -F 'tag/' '{print $2}' | awk -F '"' '{print $1}' 2>/dev/null)
+
   curl -L "https://github.com/strimzi/strimzi-kafka-operator/releases/download/${strimzi_version}/strimzi-cluster-operator-${strimzi_version}.yaml" \
   | sed 's/namespace: .*/namespace: kafka/' \
   | oc -n kafka delete -f -
@@ -124,4 +137,11 @@ function uninstall_strimzi {
   oc -n kafka delete --selector strimzi.io/crd-install=true -f "https://github.com/strimzi/strimzi-kafka-operator/releases/download/${strimzi_version}/strimzi-cluster-operator-${strimzi_version}.yaml"
 
   oc delete namespace kafka
+}
+
+function uninstall_strimzi {
+  header "Strimzi uninstall"
+  delete_strimzi_users
+  delete_strimzi_cluster
+  delete_strimzi_operator
 }
