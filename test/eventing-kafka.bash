3687c6526aad4f97c86a7eb97793379d42817a3f
Matthias Wessendorf <mwessend@redhat.com>
2020-12-09 14:52:20 +0100

    Updating latest yamls for Kafka and go with Eventing Kafka, instead of Eventing Contrib scheme
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- test/eventing-kafka.bash
+++ test/eventing-kafka.bash
@@ -3,16 +3,16 @@
 # For SC2164
 set -e
 
-function upstream_knative_eventing_contrib_e2e {
-  logger.info 'Running eventing-contrib tests'
+function upstream_knative_eventing_kafka_e2e {
+  logger.info 'Running eventing-kafka tests'
 
   local randomns
 
-  export TEST_IMAGE_TEMPLATE="registry.svc.ci.openshift.org/openshift/knative-${KNATIVE_EVENTING_CONTRIB_VERSION}:knative-eventing-test-{{.Name}}"
+  export TEST_IMAGE_TEMPLATE="registry.svc.ci.openshift.org/openshift/knative-${KNATIVE_EVENTING_KAFKA_VERSION}:knative-eventing-test-{{.Name}}"
 
-  cd "$KNATIVE_EVENTING_CONTRIB_HOME"
+  cd "$KNATIVE_EVENTING_KAFKA_HOME"
 
-  # This the namespace used to install and test Knative Eventing-Contrib.
+  # This the namespace used to install and test Knative Eventing-Kafka.
   randomns="knative-eventing-$(LC_ALL=C dd if=/dev/urandom bs=256 count=1 2> /dev/null \
     | tr -dc 'a-z0-9' | fold -w 10 | head -n 1)"
   TEST_EVENTING_NAMESPACE="${TEST_EVENTING_NAMESPACE:-"${randomns}"}"
@@ -29,9 +29,9 @@ function upstream_knative_eventing_contrib_e2e {
   install_tracing
 
   # shellcheck disable=SC1090
-  source "${KNATIVE_EVENTING_CONTRIB_HOME}/openshift/e2e-common.sh"
+  source "${KNATIVE_EVENTING_KAFKA_HOME}/openshift/e2e-common.sh"
 
-  # run_e2e_tests defined in eventing-contrib
-  logger.info 'Starting eventing-contrib tests'
+  # run_e2e_tests defined in eventing-kafka
+  logger.info 'Starting eventing-kafka tests'
   run_e2e_tests
 }
