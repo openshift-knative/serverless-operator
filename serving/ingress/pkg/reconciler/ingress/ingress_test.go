beb6ae402845300e51bee147a4351c0141c0dcef
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2020-12-16 19:57:34 +0900

    Add unit test

--- serving/ingress/pkg/reconciler/ingress/ingress_test.go
+++ serving/ingress/pkg/reconciler/ingress/ingress_test.go
@@ -68,6 +68,7 @@ func TestReconcile(t *testing.T) {
 			route(ingressNamespace, "foo"), // This gets deleted.
 			route(ingressNamespace, "foo2", func(r *routev1.Route) {
 				r.Labels[resources.OpenShiftIngressLabelKey] = "foo"
+				r.Labels[serving.RouteLabelKey] = "foo"
 			}), // This doesn't cause the label doesn't match.
 		},
 		WantDeletes: []clientgotesting.DeleteActionImpl{{
@@ -181,6 +182,21 @@ func TestReconcile(t *testing.T) {
 		WantEvents: []string{
 			Eventf(corev1.EventTypeNormal, "FinalizerUpdate", "Updated %q finalizers", ingName),
 		},
+	}, {
+		// The new downstream label (OpenShiftIngressLabelKey) was introduced but Routes with old labels still should be reconciled.
+		Name:                    "reconcile only old labels",
+		SkipNamespaceValidation: true,
+		Key:                     key,
+		Objects: []runtime.Object{
+			ing(ingNamespace, ingName),
+			route(ingressNamespace, routeName, func(r *routev1.Route) {
+				delete(r.Labels, resources.OpenShiftIngressLabelKey)
+				delete(r.Labels, resources.OpenShiftIngressNamespaceLabelKey)
+			}), // Test without downstream labels.
+		},
+		WantUpdates: []clientgotesting.UpdateActionImpl{{
+			Object: route(ingressNamespace, routeName),
+		}},
 	}}
 
 	table.Test(t, MakeFactory(func(ctx context.Context, listers *Listers, cmw configmap.Watcher) controller.Reconciler {
