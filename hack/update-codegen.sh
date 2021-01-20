b9670f5a2859992c5f7fa5ceefac12761c84aff1
Markus Thömmes <markusthoemmes@me.com>
2021-01-20 13:28:04 +0100

    Fix shellcheck issues

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -9,22 +9,23 @@ export GO111MODULE=on
 export GOFLAGS=-mod=
 
 if [ -z "${GOPATH:-}" ]; then
-  export GOPATH=$(go env GOPATH)
+  export GOPATH
+  GOPATH=$(go env GOPATH)
 fi
 
-REPO_ROOT=$(dirname ${BASH_SOURCE})/..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${REPO_ROOT}; ls -d -1 $(dirname ${BASH_SOURCE})/../vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+REPO_ROOT=$(dirname "${BASH_SOURCE[@]}")/..
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${REPO_ROOT}"; ls -d -1 "${REPO_ROOT}/vendor/k8s.io/code-generator" 2>/dev/null || echo ../code-generator)}
 
-KNATIVE_CODEGEN_PKG=${KNATIVE_CODEGEN_PKG:-$(cd ${REPO_ROOT}; ls -d -1 $(dirname ${BASH_SOURCE})/../vendor/knative.dev/pkg 2>/dev/null || echo ../pkg)}
+KNATIVE_CODEGEN_PKG=${KNATIVE_CODEGEN_PKG:-$(cd "${REPO_ROOT}"; ls -d -1 "${REPO_ROOT}/vendor/knative.dev/pkg" 2>/dev/null || echo ../pkg)}
 
 # Generate our own client for Openshift (otherwise injection won't work)
-${CODEGEN_PKG}/generate-groups.sh "client,informer,lister" \
+"${CODEGEN_PKG}/generate-groups.sh" "client,informer,lister" \
   github.com/openshift-knative/serverless-operator/pkg/client github.com/openshift/api \
   "route:v1 config:v1" \
-  --go-header-file ${REPO_ROOT}/hack/boilerplate/boilerplate.go.txt
+  --go-header-file "${REPO_ROOT}/hack/boilerplate/boilerplate.go.txt"
 
 # Knative Injection (for Openshift)
-${KNATIVE_CODEGEN_PKG}/hack/generate-knative.sh "injection" \
+"${KNATIVE_CODEGEN_PKG}/hack/generate-knative.sh" "injection" \
   github.com/openshift-knative/serverless-operator/pkg/client github.com/openshift/api \
   "route:v1 config:v1" \
-  --go-header-file ${REPO_ROOT}/hack/boilerplate/boilerplate.go.txt
+  --go-header-file "${REPO_ROOT}/hack/boilerplate/boilerplate.go.txt"
