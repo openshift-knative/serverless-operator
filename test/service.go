c581641736c75df869992f5df8f77dac0d8896ea
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2021-03-08 10:53:43 +0900

    [SRVKS-705] Add custom route test for DomainMapping's OpenShift Route
    This patch adds custom route test for DomainMapping's OpenShift Route.
    
    When users want to customize OpenShift with custom domain, they need
    to create a domainmapping and customize the OpenShift Route created by
    domainmapping's KIngress.

--- test/service.go
+++ test/service.go
@@ -14,6 +14,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	servingv1 "knative.dev/serving/pkg/apis/serving/v1"
+	servingv1alpha1 "knative.dev/serving/pkg/apis/serving/v1alpha1"
 )
 
 func Service(name, namespace, image string, annotations map[string]string) *servingv1.Service {
@@ -138,6 +139,22 @@ func WaitForServiceState(ctx *Context, name, namespace string, inState func(s *s
 	return lastState, nil
 }
 
+func WaitForDomainMappingState(ctx *Context, name, namespace string, inState func(dm *servingv1alpha1.DomainMapping, err error) (bool, error)) (*servingv1alpha1.DomainMapping, error) {
+	var (
+		lastState *servingv1alpha1.DomainMapping
+		err       error
+	)
+	waitErr := wait.PollImmediate(Interval, Timeout, func() (bool, error) {
+		lastState, err = ctx.Clients.Serving.ServingV1alpha1().DomainMappings(namespace).Get(context.Background(), name, metav1.GetOptions{})
+		return inState(lastState, err)
+	})
+
+	if waitErr != nil {
+		return lastState, fmt.Errorf("knative service %s is not in desired state, got: %+v: %w", name, lastState, waitErr)
+	}
+	return lastState, nil
+}
+
 func WaitForOperatorDepsDeleted(ctx *Context) error {
 	serverlessDependencies := []string{"knative-openshift-ingress",
 		"knative-serving-operator", "knative-eventing-operator", "knative-openshift"}
@@ -162,6 +179,10 @@ func WaitForOperatorDepsDeleted(ctx *Context) error {
 
 func IsServiceReady(s *servingv1.Service, err error) (bool, error) {
 	return s.IsReady() && s.Status.URL != nil && s.Status.URL.Host != "", err
+}
+
+func IsDomainMappingReady(dm *servingv1alpha1.DomainMapping, err error) (bool, error) {
+	return dm.IsReady() && dm.Status.URL != nil && dm.Status.URL.Host != "", err
 }
 
 func CreateDeployment(ctx *Context, name, namespace, image string) error {
--- test/servinge2e/helpers.go
+++ test/servinge2e/helpers.go
@@ -9,6 +9,7 @@ import (
 	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 	pkgTest "knative.dev/pkg/test"
 	servingv1 "knative.dev/serving/pkg/apis/serving/v1"
+	servingv1alpha1 "knative.dev/serving/pkg/apis/serving/v1alpha1"
 )
 
 func WaitForRouteServingText(t *testing.T, caCtx *test.Context, routeURL *url.URL, expectedText string) {
@@ -43,4 +44,24 @@ func withServiceReadyOrFail(ctx *test.Context, service *servingv1.Service) *serv
 	}
 
 	return service
+}
+
+func withDomainMappingReadyOrFail(ctx *test.Context, dm *servingv1alpha1.DomainMapping) *servingv1alpha1.DomainMapping {
+	dm, err := ctx.Clients.Serving.ServingV1alpha1().DomainMappings(dm.Namespace).Create(context.Background(), dm, meta.CreateOptions{})
+	if err != nil {
+		ctx.T.Fatalf("Error creating ksvc: %v", err)
+	}
+
+	// Let the ksvc be deleted after test
+	ctx.AddToCleanup(func() error {
+		ctx.T.Logf("Cleaning up Knative Service '%s/%s'", dm.Namespace, dm.Name)
+		return ctx.Clients.Serving.ServingV1alpha1().DomainMappings(dm.Namespace).Delete(context.Background(), dm.Name, meta.DeleteOptions{})
+	})
+
+	dm, err = test.WaitForDomainMappingState(ctx, dm.Name, dm.Namespace, test.IsDomainMappingReady)
+	if err != nil {
+		ctx.T.Fatalf("Error waiting for ksvc readiness: %v", err)
+	}
+
+	return dm
 }
