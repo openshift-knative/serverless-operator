3641e8dafb670310d86270a05eb6b826fd4d790b
Matthias Wessendorf <mwessend@redhat.com>
2021-03-26 10:15:08 +0100

    Retry 4d47b03 (#854)
    * Update knativekafka to v0.20.0, update project files and symlinks
    
    * Update v0.20.0 yaml for kafkasource, kafkachannel (no comments!)
    
    * Grabbing yaml from our release branch, not upstream
    
    * Back to 1 replica...
    
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>
    
    * Keep kafka-ch-dispatcher replicas set by controller
    
    * Revert "Back to 1 replica..."
    
    This reverts commit 39b8b2118feb2d7ea2dbc33002f2cd411af41576.
    
    Co-authored-by: Lukas Berk <lberk@redhat.com>
    Co-authored-by: Marek Schmidt <maschmid@redhat.com>

--- knative-operator/pkg/controller/knativekafka/knativekafka_controller.go
+++ knative-operator/pkg/controller/knativekafka/knativekafka_controller.go
@@ -222,6 +222,7 @@ func (r *ReconcileKnativeKafka) transform(manifest *mf.Manifest, instance *opera
 		setBootstrapServers(instance.Spec.Channel.BootstrapServers),
 		setAuthSecret(instance.Spec.Channel.AuthSecretNamespace, instance.Spec.Channel.AuthSecretName),
 		ImageTransform(common.BuildImageOverrideMapFromEnviron(os.Environ(), "KAFKA_IMAGE_"), log),
+		replicasTransform(manifest.Client),
 	)
 	if err != nil {
 		return fmt.Errorf("failed to transform manifest: %w", err)
