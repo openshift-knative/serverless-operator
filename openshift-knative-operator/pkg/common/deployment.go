161fa74def5b992846d70e72761760a42825f2f2
Markus Thömmes <markusthoemmes@me.com>
2021-04-23 12:57:23 +0200

    [SRVKS-725] Guarantee order of env vars when injecting them
    Passing the environment vars to inject as a map then iterating that map
    causes them to be added in a random order which causes unnecessary
    restarts of the controller. Passing them as a list guarantees the same
    order everytime.

--- openshift-knative-operator/pkg/common/deployment.go
+++ openshift-knative-operator/pkg/common/deployment.go
@@ -12,7 +12,7 @@ import (
 // specified deployment/container.
 // Note: We're not deleting empty environment variables and instead set them to empty
 // string. Three-way-merging of the deployment drops the update otherwise.
-func InjectEnvironmentIntoDeployment(deploymentName, containerName string, env map[string]string) mf.Transformer {
+func InjectEnvironmentIntoDeployment(deploymentName, containerName string, envs ...corev1.EnvVar) mf.Transformer {
 	return transformDeployment(deploymentName, func(deploy *appsv1.Deployment) error {
 		containers := deploy.Spec.Template.Spec.Containers
 		for i := range containers {
@@ -21,8 +21,8 @@ func InjectEnvironmentIntoDeployment(deploymentName, containerName string, env m
 				continue
 			}
 
-			for key, value := range env {
-				c.Env = upsert(c.Env, key, value)
+			for _, val := range envs {
+				c.Env = upsert(c.Env, val)
 			}
 		}
 
@@ -32,19 +32,16 @@ func InjectEnvironmentIntoDeployment(deploymentName, containerName string, env m
 
 // upsert updates the env var if the key already exists or inserts it if it didn't
 // exist.
-func upsert(orgEnv []corev1.EnvVar, key, value string) []corev1.EnvVar {
+func upsert(orgEnv []corev1.EnvVar, val corev1.EnvVar) []corev1.EnvVar {
 	// Set the value if the key is already present.
 	for i := range orgEnv {
-		if orgEnv[i].Name == key {
-			orgEnv[i].Value = value
+		if orgEnv[i].Name == val.Name {
+			orgEnv[i].Value = val.Value
 			return orgEnv
 		}
 	}
 	// If not, append a key-value pair.
-	return append(orgEnv, corev1.EnvVar{
-		Name:  key,
-		Value: value,
-	})
+	return append(orgEnv, val)
 }
 
 // transformDeployment returns a transformer that transforms a deployment with the given
