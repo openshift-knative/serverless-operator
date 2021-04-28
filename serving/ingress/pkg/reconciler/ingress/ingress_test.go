a15ae7cc143fb505e1e090473e73272d10d91206
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2021-04-28 21:02:26 +0900

    Allow openshift-ingress to reconcile ingress by istio ingressclass
    This patch allows openshift-ingress to reconcile ingress
    by istio ingressclass.
    
    /cc @markusthoemmes

--- serving/ingress/pkg/reconciler/ingress/ingress_test.go
+++ serving/ingress/pkg/reconciler/ingress/ingress_test.go
@@ -32,14 +32,14 @@ const (
 	ingNamespace = "testNs"
 	ingUID       = "8a7e9a9d-fbc6-11e9-a88e-0261aff8d6d8"
 
-	ingressNamespace = "knative-serving-ingress"
+	ingressNamespace = "some-ingress-namespace"
 
-	svcName    = "kourier-ingressgateway"
+	svcName    = "some-ingressgateway"
 	domainName = ingName + "." + ingNamespace + ".default.domainName"
 	routeName  = "route-" + ingUID + "-306330363338"
 )
 
-func TestReconcile(t *testing.T) {
+func TestKourierReconcile(t *testing.T) {
 	key := ingNamespace + "/" + ingName
 
 	table := TableTest{{
@@ -201,6 +201,169 @@ func TestReconcile(t *testing.T) {
 	}))
 }
 
+// TestIstioReconcile is same test with TestKourierReconcile but uses Istio ingress class.
+func TestIstioReconcile(t *testing.T) {
+	key := ingNamespace + "/" + ingName
+
+	table := TableTest{{
+		Name: "bad workqueue key",
+		Key:  "too/many/parts",
+	}, {
+		Name: "key not found",
+		Key:  "foo/not-found",
+	}, {
+		Name:    "steady state",
+		Key:     key,
+		Objects: []runtime.Object{ingIstio(ingNamespace, ingName), routeIstio(ingressNamespace, routeName)},
+	}, {
+		Name:                    "create route",
+		SkipNamespaceValidation: true,
+		Key:                     key,
+		Objects:                 []runtime.Object{ingIstio(ingNamespace, ingName)},
+		WantCreates:             []runtime.Object{routeIstio(ingressNamespace, routeName)},
+	}, {
+		Name:                    "remove outdated routes",
+		SkipNamespaceValidation: true,
+		Key:                     key,
+		Objects: []runtime.Object{
+			ingIstio(ingNamespace, ingName),
+			routeIstio(ingressNamespace, routeName),
+			routeIstio(ingressNamespace, "foo"), // This gets deleted.
+			routeIstio(ingressNamespace, "foo2", func(r *routev1.Route) {
+				r.Labels[resources.OpenShiftIngressLabelKey] = "foo"
+				r.Labels[serving.RouteLabelKey] = "foo"
+			}), // This doesn't cause the label doesn't match.
+		},
+		WantDeletes: []clientgotesting.DeleteActionImpl{{
+			ActionImpl: clientgotesting.ActionImpl{
+				Namespace: ingressNamespace,
+				Resource:  routev1.SchemeGroupVersion.WithResource("routes"),
+			},
+			Name: "foo",
+		}},
+	}, {
+		Name:                    "copy annotations and labels",
+		SkipNamespaceValidation: true,
+		Key:                     key,
+		Objects: []runtime.Object{
+			ingIstio(ingNamespace, ingName, func(i *v1alpha1.Ingress) {
+				i.Annotations["foo.bar/baz"] = "baz"
+				i.Labels["foo.bar/baz"] = "baz"
+			}),
+		},
+		WantCreates: []runtime.Object{
+			routeIstio(ingressNamespace, routeName, func(r *routev1.Route) {
+				r.Annotations["foo.bar/baz"] = "baz"
+				r.Labels["foo.bar/baz"] = "baz"
+			}),
+		},
+	}, {
+		Name:                    "copy annotations and labels on update too",
+		SkipNamespaceValidation: true,
+		Key:                     key,
+		Objects: []runtime.Object{
+			ingIstio(ingNamespace, ingName, func(i *v1alpha1.Ingress) {
+				i.Annotations["foo.bar/baz"] = "baz"
+				i.Labels["foo.bar/baz"] = "baz"
+			}),
+			routeIstio(ingressNamespace, routeName),
+		},
+		WantUpdates: []clientgotesting.UpdateActionImpl{{
+			Object: routeIstio(ingressNamespace, routeName, func(r *routev1.Route) {
+				r.Annotations["foo.bar/baz"] = "baz"
+				r.Labels["foo.bar/baz"] = "baz"
+			}),
+		}},
+	}, {
+		Name:                    "fix spec",
+		SkipNamespaceValidation: true,
+		Key:                     key,
+		Objects: []runtime.Object{
+			ingIstio(ingNamespace, ingName),
+			routeIstio(ingressNamespace, routeName, func(r *routev1.Route) {
+				r.Spec.To.Kind = "foo"
+			}),
+		},
+		WantUpdates: []clientgotesting.UpdateActionImpl{{
+			Object: routeIstio(ingressNamespace, routeName),
+		}},
+	}, {
+		Name:                    "create nothing",
+		SkipNamespaceValidation: true,
+		Key:                     key,
+		Objects: []runtime.Object{
+			ingIstio(ingNamespace, ingName, func(i *v1alpha1.Ingress) {
+				i.Annotations[resources.DisableRouteAnnotation] = "true"
+			}),
+		},
+	}, {
+		Name:                    "add finalizer",
+		SkipNamespaceValidation: true,
+		Key:                     key,
+		Objects: []runtime.Object{
+			ingIstio(ingNamespace, ingName, func(i *v1alpha1.Ingress) {
+				i.Finalizers = []string{}
+			}),
+			routeIstio(ingressNamespace, routeName),
+		},
+		WantPatches: []clientgotesting.PatchActionImpl{
+			{
+				Name:       ingName,
+				ActionImpl: clientgotesting.ActionImpl{Namespace: ingNamespace},
+				Patch:      []byte(`{"metadata":{"finalizers":["ocp-ingress"],"resourceVersion":""}}`),
+			},
+		},
+		WantEvents: []string{
+			Eventf(corev1.EventTypeNormal, "FinalizerUpdate", "Updated %q finalizers", ingName),
+		},
+	}, {
+		Name:                    "remove route and finalizer",
+		SkipNamespaceValidation: true,
+		Key:                     key,
+		Objects: []runtime.Object{
+			ingIstio(ingNamespace, ingName, func(i *v1alpha1.Ingress) {
+				i.DeletionTimestamp = &metav1.Time{
+					Time: time.Now(),
+				}
+			}),
+			routeIstio(ingressNamespace, routeName),
+		},
+		WantDeletes: []clientgotesting.DeleteActionImpl{{
+			ActionImpl: clientgotesting.ActionImpl{
+				Namespace: ingressNamespace,
+				Resource:  routev1.SchemeGroupVersion.WithResource("routes"),
+			},
+			Name: routeName,
+		}},
+		WantPatches: []clientgotesting.PatchActionImpl{
+			{
+				Name:       ingName,
+				ActionImpl: clientgotesting.ActionImpl{Namespace: ingNamespace},
+				Patch:      []byte(`{"metadata":{"finalizers":[],"resourceVersion":""}}`),
+			},
+		},
+		WantEvents: []string{
+			Eventf(corev1.EventTypeNormal, "FinalizerUpdate", "Updated %q finalizers", ingName),
+		},
+	}}
+
+	table.Test(t, MakeFactory(func(ctx context.Context, listers *Listers, cmw configmap.Watcher) controller.Reconciler {
+		r := &Reconciler{
+			routeClient: fakerouteclient.Get(ctx).RouteV1(),
+			routeLister: listers.GetRouteLister(),
+		}
+
+		ingr := ingressreconciler.NewReconciler(ctx, logging.FromContext(ctx), networkingclient.Get(ctx),
+			listers.GetIngressLister(), controller.GetEventRecorder(ctx), r, istioIngressClassName,
+			controller.Options{
+				SkipStatusUpdates: true,
+				FinalizerName:     "ocp-ingress",
+			})
+
+		return ingr
+	}))
+}
+
 type ingressOption func(*v1alpha1.Ingress)
 
 func ing(ns, name string, opts ...ingressOption) *v1alpha1.Ingress {
@@ -211,6 +374,35 @@ func ing(ns, name string, opts ...ingressOption) *v1alpha1.Ingress {
 			UID:         ingUID,
 			Labels:      map[string]string{serving.RouteNamespaceLabelKey: ns, serving.RouteLabelKey: name},
 			Annotations: map[string]string{networking.IngressClassAnnotationKey: kourierIngressClassName},
+			Finalizers:  []string{"ocp-ingress"},
+		},
+		Spec: v1alpha1.IngressSpec{
+			Rules: []v1alpha1.IngressRule{{
+				Hosts: []string{domainName},
+			}},
+		},
+		Status: v1alpha1.IngressStatus{
+			PublicLoadBalancer: &v1alpha1.LoadBalancerStatus{
+				Ingress: []v1alpha1.LoadBalancerIngressStatus{{
+					DomainInternal: svcName + "." + ingressNamespace + ".svc.cluster.local",
+				}},
+			},
+		},
+	}
+	for _, opt := range opts {
+		opt(i)
+	}
+	return i
+}
+
+func ingIstio(ns, name string, opts ...ingressOption) *v1alpha1.Ingress {
+	i := &v1alpha1.Ingress{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        name,
+			Namespace:   ns,
+			UID:         ingUID,
+			Labels:      map[string]string{serving.RouteNamespaceLabelKey: ns, serving.RouteLabelKey: name},
+			Annotations: map[string]string{networking.IngressClassAnnotationKey: istioIngressClassName},
 			Finalizers:  []string{"ocp-ingress"},
 		},
 		Spec: v1alpha1.IngressSpec{
@@ -249,6 +441,46 @@ func route(ns, name string, opts ...routeOption) *routev1.Route {
 			Annotations: map[string]string{
 				resources.TimeoutAnnotation:          resources.DefaultTimeout,
 				networking.IngressClassAnnotationKey: "kourier.ingress.networking.knative.dev",
+			},
+		},
+		Spec: routev1.RouteSpec{
+			Host: domainName,
+			Port: &routev1.RoutePort{
+				TargetPort: intstr.FromString(resources.KourierHTTPPort),
+			},
+			To: routev1.RouteTargetReference{
+				Kind:   "Service",
+				Name:   svcName,
+				Weight: ptr.Int32(100),
+			},
+			TLS: &routev1.TLSConfig{
+				Termination:                   routev1.TLSTerminationEdge,
+				InsecureEdgeTerminationPolicy: routev1.InsecureEdgeTerminationPolicyAllow,
+			},
+			WildcardPolicy: routev1.WildcardPolicyNone,
+		},
+	}
+	for _, opt := range opts {
+		opt(r)
+	}
+	return r
+}
+
+func routeIstio(ns, name string, opts ...routeOption) *routev1.Route {
+	r := &routev1.Route{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: ns,
+			Labels: map[string]string{
+				networking.IngressLabelKey:                  "test",
+				serving.RouteLabelKey:                       "test",
+				serving.RouteNamespaceLabelKey:              "testNs",
+				resources.OpenShiftIngressLabelKey:          "test",
+				resources.OpenShiftIngressNamespaceLabelKey: "testNs",
+			},
+			Annotations: map[string]string{
+				resources.TimeoutAnnotation:          resources.DefaultTimeout,
+				networking.IngressClassAnnotationKey: "istio.ingress.networking.knative.dev",
 			},
 		},
 		Spec: routev1.RouteSpec{
