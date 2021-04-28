36e6d3bc7f6b0b89eb9e530165853db76f55a2f9
Matthias Wessendorf <mwessend@redhat.com>
2021-04-28 14:51:54 +0200

    Adding patch for eventing pdb, like we have for serving
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- openshift-knative-operator/hack/update-manifests.sh
+++ openshift-knative-operator/hack/update-manifests.sh
@@ -56,3 +56,7 @@ download eventing "$KNATIVE_EVENTING_VERSION" "${eventing_files[@]}"
 git apply "$root/openshift-knative-operator/hack/002-openshift-eventing-role.patch"
 # For SRVKE-629 we disable HPA:
 git apply "$root/openshift-knative-operator/hack/005-disable-hpa.patch"
+
+# TODO: Remove this once upstream fixed https://github.com/knative/operator/issues/376.
+# This is the eventing counterpart of SRVKS-670.
+git apply "$root/openshift-knative-operator/hack/006-eventing-pdb.patch"
