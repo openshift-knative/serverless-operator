66ac6bbca816f3b89795e7a4f4fef1ed2134c52e
Matthias Wessendorf <mwessend@redhat.com>
2021-04-28 11:36:56 +0200

    Back to old file for ocp-kn-operator
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- openshift-knative-operator/hack/update-manifests.sh
+++ openshift-knative-operator/hack/update-manifests.sh
@@ -12,7 +12,6 @@ source "$root/hack/lib/__sources__.bash"
 # be fairly stable.
 serving_files=(serving-crds serving-core serving-hpa serving-domainmapping-crds serving-domainmapping serving-post-install-jobs)
 eventing_files=(eventing-crds eventing-core in-memory-channel mt-channel-broker eventing-sugar-controller)
-kafka_files=(channel-consolidated source)
 
 # This excludes the gateways and peerauthentication settings as we want customers to do
 # manipulate those.
@@ -21,36 +20,28 @@ istio_files=(200-clusterrole 500-mutating-webhook 500-validating-webhook config 
 function download {
   component=$1
   version=$2
-  org=$3
-  shift
   shift
   shift
 
   files=("$@")
-  local component_dir=""
-  local target_dir=""
-  if [[ $component == "eventing-kafka" ]]; then
-      component_dir="$root/knative-operator/deploy/resources/knativekafka/"
-      target_dir="${component_dir}/"
-  else
-      component_dir="$root/openshift-knative-operator/cmd/operator/kodata/knative-${component}"
-      target_dir="${component_dir}/${version:1}"
-      rm -r "$component_dir"
-      mkdir -p "$target_dir"
-  fi
+
+  component_dir="$root/openshift-knative-operator/cmd/operator/kodata/knative-${component}"
+  target_dir="${component_dir}/${version:1}"
+  rm -r "$component_dir"
+  mkdir -p "$target_dir"
 
   for (( i=0; i<${#files[@]}; i++ ));
   do
     index=$(( i+1 ))
     file="${files[$i]}.yaml"
     target_file="$target_dir/$index-$file"
-    url="https://github.com/$org/$component/releases/download/$version/$file"
+    url="https://github.com/knative/$component/releases/download/$version/$file"
 
     wget --no-check-certificate "$url" -O "$target_file"
   done
 }
 
-download serving "$KNATIVE_SERVING_VERSION" "knative" "${serving_files[@]}"
+download serving "$KNATIVE_SERVING_VERSION" "${serving_files[@]}"
 
 download_ingress net-istio "v$(metadata.get dependencies.net_istio)" "${istio_files[@]}"
 
@@ -58,13 +49,9 @@ download_ingress net-istio "v$(metadata.get dependencies.net_istio)" "${istio_fi
 # See also https://issues.redhat.com/browse/SRVKS-670.
 git apply "$root/openshift-knative-operator/hack/003-serving-pdb.patch"
 
-download eventing "$KNATIVE_EVENTING_VERSION" "knative" "${eventing_files[@]}"
+download eventing "$KNATIVE_EVENTING_VERSION" "${eventing_files[@]}"
 # Extra ClusterRole for downstream, so that users can get the CMs of knative-eventing
 # TODO: propose to upstream
 git apply "$root/openshift-knative-operator/hack/002-openshift-eventing-role.patch"
 # For SRVKE-629 we disable HPA:
 git apply "$root/openshift-knative-operator/hack/005-disable-hpa.patch"
-
-download eventing-kafka "$KNATIVE_EVENTING_KAFKA_VERSION" "knative-sandbox" "${kafka_files[@]}"
-# For Backport of v1alpha1 hacks, we change the storage versions:
-git apply "$root/openshift-knative-operator/hack/006-kafkachannel-storage-beta1.patch"
