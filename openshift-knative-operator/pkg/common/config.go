bc82c259e2433b656a937c84c51e8f130061939f
Markus Thömmes <markusthoemmes@me.com>
2021-04-29 09:20:41 +0200

    [SRKVS-732] Allow ingress.class to be overridden

--- openshift-knative-operator/pkg/common/config.go
+++ openshift-knative-operator/pkg/common/config.go
@@ -14,3 +14,21 @@ func Configure(s *v1alpha1.CommonSpec, cm, key, value string) {
 
 	s.Config[cm][key] = value
 }
+
+// ConfigureIfUnset sets a value in the given ConfigMap under the given key if it's not
+// already set.
+func ConfigureIfUnset(s *v1alpha1.CommonSpec, cm, key, value string) {
+	if s.Config == nil {
+		s.Config = make(map[string]map[string]string, 1)
+	}
+
+	if s.Config[cm] == nil {
+		s.Config[cm] = make(map[string]string, 1)
+	}
+
+	if _, ok := s.Config[cm][key]; ok {
+		// Already set, nothing to do here.
+		return
+	}
+	s.Config[cm][key] = value
+}
