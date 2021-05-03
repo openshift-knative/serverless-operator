697dea1af4d04832e94898062d840baa5b9cd7fc
Matthias Wessendorf <mwessend@redhat.com>
2021-05-03 10:45:43 +0200

    :dizzy: hook in tracing, for some conformance tests
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- test/eventing.bash
+++ test/eventing.bash
@@ -16,10 +16,15 @@ function upstream_knative_eventing_e2e {
   # shellcheck disable=SC1090
   source "${KNATIVE_EVENTING_HOME}/openshift/e2e-common.sh"
 
+  logger.info 'Installing Tracing'
+  install_tracing
+
   # run_e2e_tests defined in knative-eventing
+  logger.info 'Starting eventing e2e tests'
   run_e2e_tests
 
   # run_conformance_tests defined in knative-eventing
+  logger.info 'Starting eventing conformance tests'
   run_conformance_tests
 }
 
