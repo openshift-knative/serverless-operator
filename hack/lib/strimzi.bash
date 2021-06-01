8dcf75190a3c77738b787205378f3f97b4776ca7
Matthias Wessendorf <mwessend@redhat.com>
2021-06-01 13:11:25 +0200

    Revert "Fixing Strmzi Install by doing kubectl create (#853)"
    This reverts commit ddc493254812db9f6984804ca06fc0ccd5885c68.

--- hack/lib/strimzi.bash
+++ hack/lib/strimzi.bash
@@ -4,9 +4,10 @@ function install_strimzi_operator {
   strimzi_version=$(curl https://github.com/strimzi/strimzi-kafka-operator/releases/latest |  awk -F 'tag/' '{print $2}' | awk -F '"' '{print $1}' 2>/dev/null)
   header "Installing Strimzi Kafka operator"
   oc create namespace kafka
+  oc -n kafka apply --selector strimzi.io/crd-install=true -f "https://github.com/strimzi/strimzi-kafka-operator/releases/download/${strimzi_version}/strimzi-cluster-operator-${strimzi_version}.yaml"
   curl -L "https://github.com/strimzi/strimzi-kafka-operator/releases/download/${strimzi_version}/strimzi-cluster-operator-${strimzi_version}.yaml" \
   | sed 's/namespace: .*/namespace: kafka/' \
-  | oc -n kafka create -f -
+  | oc -n kafka apply -f -
 
   # Wait for the CRD we need to actually be active
   oc wait crd --timeout=-1s kafkas.kafka.strimzi.io --for=condition=Established
