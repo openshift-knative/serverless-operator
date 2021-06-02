d109a2ce320d42c4dabd16c3ae39323d3e79c66a
Matthias Wessendorf <mwessend@redhat.com>
2021-06-02 11:04:46 +0200

    Using Apache Kafka 2.8.0 and latest of Strimzi (#996)
    * Revert "Going with hard coded 0.22.1 version for 1.15 (#942)"
    
    This reverts commit 35815ca1864e367555ffc1f128b09b51edb2d48e.
    
    * Revert "Fixing Strmzi Install by doing kubectl create (#853)"
    
    This reverts commit ddc493254812db9f6984804ca06fc0ccd5885c68.
    
    * :lipstick: Using Apache 2.8.0
    
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- hack/lib/strimzi.bash
+++ hack/lib/strimzi.bash
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 function install_strimzi_operator {
-#  strimzi_version=$(curl https://github.com/strimzi/strimzi-kafka-operator/releases/latest |  awk -F 'tag/' '{print $2}' | awk -F '"' '{print $1}' 2>/dev/null)
-  strimzi_version=0.22.1
+  strimzi_version=$(curl https://github.com/strimzi/strimzi-kafka-operator/releases/latest |  awk -F 'tag/' '{print $2}' | awk -F '"' '{print $1}' 2>/dev/null)
   header "Installing Strimzi Kafka operator"
   oc create namespace kafka
+  oc -n kafka apply --selector strimzi.io/crd-install=true -f "https://github.com/strimzi/strimzi-kafka-operator/releases/download/${strimzi_version}/strimzi-cluster-operator-${strimzi_version}.yaml"
   curl -L "https://github.com/strimzi/strimzi-kafka-operator/releases/download/${strimzi_version}/strimzi-cluster-operator-${strimzi_version}.yaml" \
   | sed 's/namespace: .*/namespace: kafka/' \
-  | oc -n kafka create -f -
+  | oc -n kafka apply -f -
 
   # Wait for the CRD we need to actually be active
   oc wait crd --timeout=-1s kafkas.kafka.strimzi.io --for=condition=Established
@@ -23,7 +23,7 @@ function install_strimzi_cluster {
       namespace: kafka
     spec:
       kafka:
-        version: 2.7.0
+        version: 2.8.0
         replicas: 3
         listeners:
           - name: plain
@@ -46,8 +46,8 @@ function install_strimzi_cluster {
           offsets.topic.replication.factor: 3
           transaction.state.log.replication.factor: 3
           transaction.state.log.min.isr: 2
-          inter.broker.protocol.version: "2.7"
-          log.message.format.version: "2.7"
+          inter.broker.protocol.version: "2.8"
+          log.message.format.version: "2.8"
           auto.create.topics.enable: "false"
         storage:
           type: jbod
