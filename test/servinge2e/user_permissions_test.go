3546481168e4f01e6c78c42eb39aaed56edd1c9f
Markus Thömmes <markusthoemmes@me.com>
2021-06-07 19:35:50 +0200

    [SRVKS-258] Test that even project admins can't create internal resources (#1001)
    * [SRVKS-258] Test that even project admins can't create internal resources
    
    * Drop unused context

--- test/servinge2e/user_permissions_test.go
+++ test/servinge2e/user_permissions_test.go
@@ -2,11 +2,18 @@ package servinge2e
 
 import (
 	"context"
-	"strings"
 	"testing"
 
 	"github.com/openshift-knative/serverless-operator/test"
+	corev1 "k8s.io/api/core/v1"
+	apierrs "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/kubernetes/scheme"
+	networkingv1alpha1 "knative.dev/networking/pkg/apis/networking/v1alpha1"
+	autoscalingv1alpha1 "knative.dev/serving/pkg/apis/autoscaling/v1alpha1"
+	servingv1 "knative.dev/serving/pkg/apis/serving/v1"
 )
 
 const (
@@ -20,118 +27,131 @@ const (
 	helloworldText        = "Hello World!"
 )
 
-func TestUserPermissions(t *testing.T) {
+type allowedOperations struct {
+	get    bool
+	list   bool
+	create bool
+	delete bool
+}
 
-	caCtx := test.SetupClusterAdmin(t)
+func init() {
+	servingv1.AddToScheme(scheme.Scheme)
+	networkingv1alpha1.AddToScheme(scheme.Scheme)
+	autoscalingv1alpha1.AddToScheme(scheme.Scheme)
+}
+
+func TestUserPermissions(t *testing.T) {
 	paCtx := test.SetupProjectAdmin(t)
 	editCtx := test.SetupEdit(t)
 	viewCtx := test.SetupView(t)
-	test.CleanupOnInterrupt(t, func() { test.CleanupAll(t, caCtx, paCtx, editCtx, viewCtx) })
-	defer test.CleanupAll(t, caCtx, paCtx, editCtx, viewCtx)
+	test.CleanupOnInterrupt(t, func() { test.CleanupAll(t, paCtx, editCtx, viewCtx) })
+	defer test.CleanupAll(t, paCtx, editCtx, viewCtx)
 
-	if _, err := test.WithServiceReady(caCtx, helloworldService, testNamespace, image); err != nil {
-		t.Fatal("Knative Service not ready", err)
+	serviceGVR := servingv1.SchemeGroupVersion.WithResource("services")
+	ingressGVR := networkingv1alpha1.SchemeGroupVersion.WithResource("ingresses")
+	paGVR := autoscalingv1alpha1.SchemeGroupVersion.WithResource("podautoscalers")
+
+	service := &servingv1.Service{
+		Spec: servingv1.ServiceSpec{
+			ConfigurationSpec: servingv1.ConfigurationSpec{
+				Template: servingv1.RevisionTemplateSpec{
+					Spec: servingv1.RevisionSpec{
+						PodSpec: corev1.PodSpec{
+							Containers: []corev1.Container{{
+								Image: "some-image",
+							}},
+						},
+					},
+				},
+			},
+		},
+	}
+	ingress := &networkingv1alpha1.Ingress{}
+	pa := &autoscalingv1alpha1.PodAutoscaler{}
+	objects := map[schema.GroupVersionResource]*unstructured.Unstructured{
+		serviceGVR: {},
+		ingressGVR: {},
+		paGVR:      {},
+	}
+	if err := scheme.Scheme.Convert(service, objects[serviceGVR], nil); err != nil {
+		t.Fatalf("Failed to convert Service: %v", err)
+	}
+	if err := scheme.Scheme.Convert(ingress, objects[ingressGVR], nil); err != nil {
+		t.Fatalf("Failed to convert Ingress: %v", err)
+	}
+	if err := scheme.Scheme.Convert(pa, objects[paGVR], nil); err != nil {
+		t.Fatalf("Failed to convert PodAutoscaler: %v", err)
+	}
+
+	allowAll := allowedOperations{
+		get:    true,
+		list:   true,
+		create: true,
+		delete: true,
+	}
+	allowViewOnly := allowedOperations{
+		get:  true,
+		list: true,
 	}
 
 	tests := []struct {
 		name        string
 		userContext *test.Context
-		operation   func(context *test.Context) error
-		wantErrStr  string
+		allowed     map[schema.GroupVersionResource]allowedOperations
 	}{{
-		name: "user with view role can get",
-		operation: func(c *test.Context) error {
-			_, err := c.Clients.Serving.ServingV1().Services(testNamespace).Get(context.Background(), helloworldService, metav1.GetOptions{})
-			return err
-		},
-		userContext: viewCtx,
-	}, {
-		name: "user with view role can list",
-		operation: func(c *test.Context) error {
-			_, err := c.Clients.Serving.ServingV1().Services(testNamespace).List(context.Background(), metav1.ListOptions{})
-			return err
-		},
-		userContext: viewCtx,
-	}, {
-		name: "user with view role cannot create",
-		operation: func(c *test.Context) error {
-			_, err := test.CreateService(c, "userview-service", testNamespace, image)
-			return err
-		},
-		userContext: viewCtx,
-		wantErrStr:  "is forbidden",
-	}, {
-		name: "user with view role cannot delete",
-		operation: func(c *test.Context) error {
-			return c.Clients.Serving.ServingV1().Services(testNamespace).Delete(context.Background(), helloworldService, metav1.DeleteOptions{})
-		},
-		userContext: viewCtx,
-		wantErrStr:  "is forbidden",
-	}, {
-		name: "user with project admin role can get",
-		operation: func(c *test.Context) error {
-			_, err := c.Clients.Serving.ServingV1().Services(testNamespace).Get(context.Background(), helloworldService, metav1.GetOptions{})
-			return err
-		},
+		name:        "project admin user",
 		userContext: paCtx,
-	}, {
-		name: "user with project admin role can list",
-		operation: func(c *test.Context) error {
-			_, err := c.Clients.Serving.ServingV1().Services(testNamespace).List(context.Background(), metav1.ListOptions{})
-			return err
+		allowed: map[schema.GroupVersionResource]allowedOperations{
+			serviceGVR: allowAll,
+			ingressGVR: allowViewOnly,
+			paGVR:      allowViewOnly,
 		},
-		userContext: paCtx,
 	}, {
-		name: "user with project admin role can create",
-		operation: func(c *test.Context) error {
-			_, err := test.CreateService(c, "projectadmin-service", testNamespace, image)
-			return err
-		},
-		userContext: paCtx,
-	}, {
-		name: "user with project admin role can delete",
-		operation: func(c *test.Context) error {
-			return c.Clients.Serving.ServingV1().Services(testNamespace).Delete(context.Background(), "projectadmin-service", metav1.DeleteOptions{})
-		},
-		userContext: paCtx,
-	}, {
-		name: "user with edit role can get",
-		operation: func(c *test.Context) error {
-			_, err := c.Clients.Serving.ServingV1().Services(testNamespace).Get(context.Background(), helloworldService, metav1.GetOptions{})
-			return err
-		},
+		name:        "edit user",
 		userContext: editCtx,
-	}, {
-		name: "user with edit role can list",
-		operation: func(c *test.Context) error {
-			_, err := c.Clients.Serving.ServingV1().Services(testNamespace).List(context.Background(), metav1.ListOptions{})
-			return err
+		allowed: map[schema.GroupVersionResource]allowedOperations{
+			serviceGVR: allowAll,
+			ingressGVR: allowViewOnly,
+			paGVR:      allowViewOnly,
 		},
-		userContext: editCtx,
 	}, {
-		name: "user with edit role can create",
-		operation: func(c *test.Context) error {
-			_, err := test.CreateService(c, "useredit-service", testNamespace, image)
-			return err
+		name:        "view user",
+		userContext: viewCtx,
+		allowed: map[schema.GroupVersionResource]allowedOperations{
+			serviceGVR: allowViewOnly,
+			ingressGVR: allowViewOnly,
+			paGVR:      allowViewOnly,
 		},
-		userContext: editCtx,
-	}, {
-		name: "user with edit role can delete",
-		operation: func(c *test.Context) error {
-			return c.Clients.Serving.ServingV1().Services(testNamespace).Delete(context.Background(), "useredit-service", metav1.DeleteOptions{})
-
-		},
-		userContext: editCtx,
 	}}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			err := test.operation(test.userContext)
-			if (err != nil) != (test.wantErrStr != "") {
-				t.Errorf("User with role %s has unexpected behavior on knative services. Error thrown: %v, error expected: %t", test.userContext.Name, err, (test.wantErrStr != ""))
-			}
-			if err != nil && !strings.Contains(err.Error(), test.wantErrStr) {
-				t.Errorf("Unexpected error for user with role %s: %v", test.userContext.Name, err)
+			for gvr, allowed := range test.allowed {
+				client := test.userContext.Clients.Dynamic.Resource(gvr).Namespace(testNamespace)
+
+				obj := objects[gvr].DeepCopy()
+				obj.SetName("test-" + gvr.Resource)
+
+				_, err := client.Create(context.Background(), obj, metav1.CreateOptions{})
+				if (allowed.create && err != nil) || (!allowed.create && !apierrs.IsForbidden(err)) {
+					t.Errorf("Unexpected error creating %s, allowed = %v, err = %v", gvr.String(), allowed.create, err)
+				}
+
+				err = client.Delete(context.Background(), obj.GetName(), metav1.DeleteOptions{})
+				if (allowed.delete && err != nil) || (!allowed.delete && !apierrs.IsForbidden(err)) {
+					t.Errorf("Unexpected error deleting %s, allowed = %v, err = %v", gvr.String(), allowed.delete, err)
+				}
+
+				_, err = client.Get(context.Background(), obj.GetName(), metav1.GetOptions{})
+				// Ignore IsNotFound errors as "Forbidden" would overrule it anyway.
+				if (allowed.get && err != nil && !apierrs.IsNotFound(err)) || (!allowed.get && !apierrs.IsForbidden(err)) {
+					t.Errorf("Unexpected error getting %s, allowed = %v, err = %v", gvr.String(), allowed.get, err)
+				}
+
+				_, err = client.List(context.Background(), metav1.ListOptions{})
+				if (allowed.list && err != nil) || (!allowed.list && !apierrs.IsForbidden(err)) {
+					t.Errorf("Unexpected error listing %s, allowed = %v, err = %v", gvr.String(), allowed.list, err)
+				}
 			}
 		})
 	}
