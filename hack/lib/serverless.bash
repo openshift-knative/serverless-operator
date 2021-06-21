e7d10146483c88484a1bcec88de04889da5cb325
Markus Thömmes <markusthoemmes@me.com>
2021-06-21 05:53:31 +0200

    Drop unnecessary version check (#1035)

--- hack/lib/serverless.bash
+++ hack/lib/serverless.bash
@@ -31,11 +31,6 @@ function install_serverless_previous {
 
   deploy_serverless_operator "$PREVIOUS_CSV"
 
-  # TODO: Remove this when the previous release has readiness checks available (since 1.12).
-  if versions.le "$(metadata.get olm.replaces)" 1.11.0; then
-    timeout 120 "[[ \$(oc get namespace ${SERVING_NAMESPACE} ${EVENTING_NAMESPACE} --no-headers | wc -l) != 2 ]]"
-  fi
-
   if [[ $INSTALL_SERVING == "true" ]]; then
     deploy_knativeserving_cr
   fi
