9414cae2e12047557f8610c9d464f59948fffa7a
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2021-06-22 13:49:17 +0900

    Do not use oc wait

--- test/lib.bash
+++ test/lib.bash
@@ -82,7 +82,7 @@ function serverless_operator_e2e_tests {
     "$@"
 
   # make sure knative-serving-ingress namespace is deleted.
-  oc wait --for=delete namespace "${SERVING_NAMESPACE}-ingress" --timeout=300s
+  timeout 180 "[[ \$(oc get ns "${SERVING_NAMESPACE}"-ingress --no-headers | wc -l) == 1 ]]"
 }
 
 function serverless_operator_kafka_e2e_tests {
