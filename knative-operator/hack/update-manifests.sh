3dc029ce528ef0777dd8ef5365b9576fed1f80df
Matthias Wessendorf <mwessend@redhat.com>
2021-07-01 07:50:02 +0200

    Adding patch (like for eventing core) to remove the HPA
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- knative-operator/hack/update-manifests.sh
+++ knative-operator/hack/update-manifests.sh
@@ -37,6 +37,9 @@ function download_kafka {
 
 download_kafka knativekafka "$KNATIVE_EVENTING_KAFKA_VERSION" "${kafka_files[@]}"
 
+# SRVKE-884: for 1.16 we remove the HPA
+git apply "$root/knative-operator/hack/001-eventing-kafka-hpa.patch"
+
 # Change the minavailable pdb for kafka-webhook to 1
 git apply "$root/knative-operator/hack/007-eventing-kafka-pdb.patch"
 git apply "$root/knative-operator/hack/008-eventing-kafka-webhook-resources.patch"
