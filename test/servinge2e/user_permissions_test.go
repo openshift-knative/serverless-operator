1ff4a956f9e89bcd9a311c90628cd3aa6799b9a0
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2021-07-14 12:17:46 +0900

    Fix lint

--- test/servinge2e/user_permissions_test.go
+++ test/servinge2e/user_permissions_test.go
@@ -14,13 +14,7 @@ import (
 )
 
 const (
-	testNamespace         = "serverless-tests"
-	testNamespace2        = "serverless-tests2"
-	image                 = "gcr.io/knative-samples/helloworld-go"
-	helloworldService     = "helloworld-go"
-	helloworldService2    = "helloworld-go2"
-	kubeHelloworldService = "kube-helloworld-go"
-	helloworldText        = "Hello World!"
+	testNamespace = "serverless-tests"
 )
 
 func init() {
