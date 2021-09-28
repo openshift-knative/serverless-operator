c0fd4db734a3a163efcb97df9218e9e06ea243b6
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2021-09-28 18:26:37 +0900

    Fix HTTPS test for Kourier

--- test/servinge2e/kourier/verify_http_and_https_test.go
+++ test/servinge2e/kourier/verify_http_and_https_test.go
@@ -1,10 +1,6 @@
 package kourier
 
 import (
-	"crypto/tls"
-	"crypto/x509"
-	"io/ioutil"
-	"net/http"
 	"testing"
 
 	"github.com/openshift-knative/serverless-operator/test"
@@ -22,46 +18,13 @@ func TestKnativeServiceHTTPS(t *testing.T) {
 		t.Fatal("Knative Service not ready", err)
 	}
 
-	// Implicitly checks that HTTP works.
+	// Checks that HTTPS works.
 	servinge2e.WaitForRouteServingText(t, caCtx, ksvc.Status.URL.URL(), helloworldText)
 
-	// Now check that HTTPS works.
+	// Now check that HTTP works.
 	httpsURL := ksvc.Status.URL.DeepCopy()
-	httpsURL.Scheme = "https"
 
-	// First, download the cert from the host so we can trust it later.
-	conn, err := tls.Dial("tcp", httpsURL.Host+":443", &tls.Config{
-		InsecureSkipVerify: true,
-	})
-	if err != nil {
-		t.Fatal("Failed to connect to download certificate", err)
-	}
-	defer conn.Close()
-
-	// Add the cert to our cert pool, so it's trusted.
-	certPool, err := x509.SystemCertPool()
-	if err != nil {
-		t.Fatal("Failed to load system cert pool", err)
-	}
-	for _, cert := range conn.ConnectionState().PeerCertificates {
-		certPool.AddCert(cert)
-	}
-
-	client := &http.Client{Transport: &http.Transport{
-		TLSClientConfig: &tls.Config{
-			RootCAs: certPool,
-		},
-	}}
-	t.Log("Requesting", httpsURL.String())
-	resp, err := client.Get(httpsURL.String())
-	if err != nil {
-		t.Fatalf("Request to %v failed, err: %v", httpsURL, err)
-	}
-	if resp.StatusCode != http.StatusOK {
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			t.Error("Failed to read body", err)
-		}
-		t.Fatalf("Response failed, status %v, body %v", resp.StatusCode, string(body))
-	}
+	httpsURL.Scheme = "http"
+	// Implicitly checks that HTTP works.
+	servinge2e.WaitForRouteServingText(t, caCtx, ksvc.Status.URL.URL(), helloworldText)
 }
