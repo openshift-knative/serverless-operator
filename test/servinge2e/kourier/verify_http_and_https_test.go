52ee22c4deb7123bd6551079a39c80f24d9ea3d8
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2021-09-29 20:18:44 +0900

    Add e2e for redirect

--- test/servinge2e/kourier/verify_http_and_https_test.go
+++ test/servinge2e/kourier/verify_http_and_https_test.go
@@ -2,6 +2,7 @@ package kourier
 
 import (
 	"context"
+	"net/http"
 	"testing"
 
 	"github.com/openshift-knative/serverless-operator/test"
@@ -39,4 +40,39 @@ func TestKnativeServiceHTTPS(t *testing.T) {
 		t.Fatalf("The Route at domain %s didn't serve the expected text %q: %v", httpURL, helloworldText, err)
 	}
 
+}
+
+func TestKnativeServiceHTTPRedirect(t *testing.T) {
+
+	caCtx := test.SetupClusterAdmin(t)
+	test.CleanupOnInterrupt(t, func() { test.CleanupAll(t, caCtx) })
+	defer test.CleanupAll(t, caCtx)
+
+	ksvc, err := test.WithServiceReady(caCtx, "https-service", testNamespace, image)
+	if err != nil {
+		t.Fatal("Knative Service not ready", err)
+	}
+
+	// Implicitly checks that HTTPS works.
+	servinge2e.WaitForRouteServingText(t, caCtx, ksvc.Status.URL.URL(), helloworldText)
+
+	// Now check HTTP request.
+	httpURL := ksvc.Status.URL.DeepCopy()
+	httpURL.Scheme = "http"
+
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			// Do not follow redirect.
+			return http.ErrUseLastResponse
+		},
+	}
+
+	t.Log("Requesting", httpURL.String())
+	resp, err := client.Get(httpURL.String())
+	if err != nil {
+		t.Fatalf("Request to %v failed, err: %v", httpURL, err)
+	}
+	if resp.StatusCode != http.StatusMovedPermanently {
+		t.Fatalf("The Route at domain %s didn't serve the expected status code got=%v, want=%v", httpURL, resp.StatusCode, http.StatusMovedPermanently)
+	}
 }
