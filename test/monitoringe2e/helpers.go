47608db04d4cca4d66d17b61f78e1338c554df6a
Martin Gencur <mgencur@redhat.com>
2021-10-11 15:15:13 +0200

    Log prometheus API errors but not fail
    Account for Prometheus startup time.

--- test/monitoringe2e/helpers.go
+++ test/monitoringe2e/helpers.go
@@ -112,7 +112,8 @@ func VerifyHealthStatusMetric(caCtx *test.Context, label string, expectedValue s
 	if err := wait.PollImmediate(test.Interval, prometheusTargetTimeout, func() (bool, error) {
 		value, _, err := pc.Query(context.Background(), fmt.Sprintf(`knative_up{type="%s"}`, label), time.Time{})
 		if err != nil {
-			return false, err
+			caCtx.T.Log("Error:", err)
+			return false, nil
 		}
 
 		vec, ok := value.(prommodel.Vector)
