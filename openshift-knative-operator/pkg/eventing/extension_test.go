f2d22859dc054c0c80584c7fb4af0a592ff5f357
Markus Thömmes <markusthoemmes@me.com>
2021-11-03 21:10:48 +0100

    Update golangci-lint, fix issues and add bidichk (#1277)

--- openshift-knative-operator/pkg/eventing/extension_test.go
+++ openshift-knative-operator/pkg/eventing/extension_test.go
@@ -246,7 +246,7 @@ func TestMonitoring(t *testing.T) {
 			shouldEnableMonitoring, err := c.setupMonitoringToggle()
 
 			if err != nil {
-				t.Errorf("Failed to setup the monitoring toggle %w", err)
+				t.Errorf("Failed to setup the monitoring toggle %v", err)
 			}
 			ext.Reconcile(context.Background(), ke)
 
