c34cbf5105ca35ed5a6933405076a6ff84a47a2c
Matthias Wessendorf <mwessend@redhat.com>
2021-11-16 14:08:07 +0100

    Reading from folders, scripts part
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- knative-operator/hack/update-manifests.sh
+++ knative-operator/hack/update-manifests.sh
@@ -8,11 +8,14 @@ root="$(dirname "${BASH_SOURCE[0]}")/../.."
 # shellcheck disable=SC1091,SC1090
 source "$root/hack/lib/__sources__.bash"
 
-kafka_files=(channel-consolidated source)
+kafka_channel_files=(channel-consolidated)
+kafka_source_files=(source)
 
 function download_kafka {
   component=$1
-  version=$2
+  subdir=$2
+  version=$3
+  shift
   shift
   shift
 
@@ -25,7 +28,7 @@ function download_kafka {
   do
     index=$(( i+1 ))
     file="${files[$i]}.yaml"
-    target_file="$target_dir/$index-$file"
+    target_file="$target_dir/$subdir/$index-$file"
     url="https://github.com/knative-sandbox/$component/releases/download/$version/$file"
 
     wget --no-check-certificate "$url" -O "$target_file"
@@ -35,7 +38,8 @@ function download_kafka {
   done
 }
 
-download_kafka eventing-kafka "$KNATIVE_EVENTING_KAFKA_VERSION" "${kafka_files[@]}"
+download_kafka eventing-kafka channel "$KNATIVE_EVENTING_KAFKA_VERSION" "${kafka_channel_files[@]}"
+download_kafka eventing-kafka source "$KNATIVE_EVENTING_KAFKA_VERSION" "${kafka_source_files[@]}"
 
 # For 1.17 we still skip HPA
 git apply "$root/knative-operator/hack/001-eventing-kafka-remove_hpa.patch"
