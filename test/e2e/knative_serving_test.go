3a2b052f5dd6fa244c957d6bbcec749ba2f539c4
Martin Gencur <mgencur@redhat.com>
2021-12-01 10:55:16 +0100

    Wait for the correct shape of all deployments

--- test/e2e/knative_serving_test.go
+++ test/e2e/knative_serving_test.go
@@ -49,18 +49,24 @@ func TestKnativeServing(t *testing.T) {
 			}
 		}
 		// Check the status of deployments in the knative serving namespace
-		for _, deployment := range []string{"autoscaler", "webhook"} {
+		for _, deployment := range []string{"activator", "autoscaler", "autoscaler-hpa", "controller", "webhook"} {
 			if _, err := test.WithDeploymentReady(caCtx, deployment, servingNamespace); err != nil {
 				t.Fatalf("Deployment %s is not ready: %v", deployment, err)
 			}
 		}
-		// Check the status of deployments in the ingress namespace.
-		for _, deployment := range []string{"net-kourier-controller", "3scale-kourier-gateway"} {
+		// Check the desired scale of deployments in the ingress namespace.
+		ingressDeployments := []string{"net-kourier-controller", "3scale-kourier-gateway"}
+		for _, deployment := range ingressDeployments {
 			if err := test.CheckDeploymentScale(caCtx, servingNamespace+"-ingress", deployment, haReplicas); err != nil {
 				t.Fatalf("Failed to verify default HA settings: %v", err)
 			}
 		}
-
+		// Check the status of deployments in the ingress namespace.
+		for _, deployment := range ingressDeployments {
+			if _, err := test.WithDeploymentReady(caCtx, deployment, servingNamespace+"-ingress"); err != nil {
+				t.Fatalf("Deployment %s is not ready: %v", deployment, err)
+			}
+		}
 	})
 
 	t.Run("make sure no gcr.io references are there", func(t *testing.T) {
