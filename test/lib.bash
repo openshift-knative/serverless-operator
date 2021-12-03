60771fc4cba6a29daeaba90a957c9a5872dd5622
Martin Gencur <mgencur@redhat.com>
2021-12-03 13:02:47 +0100

    Wait for KServices to be deleted in foreground (#1325)
    The KServices are being deleted by default with
    propagationPolicy=Background which can be seen in garbage collector logs
    in kube-controller-manager pod.
    The logs also show that some resources are still in process of deletion
    when the 120 second timeout expires.

--- test/lib.bash
+++ test/lib.bash
@@ -222,8 +222,7 @@ function run_rolling_upgrade_tests {
     --https
 
   # Delete the leftover services.
-  oc delete ksvc --all -n serving-tests
-  timeout 120 "[[ \$(oc get all --no-headers -n serving-tests | wc -l) != 0 ]]"
+  oc delete ksvc --all --cascade='foreground' -n serving-tests
 
   logger.success 'Upgrade tests passed'
 }
