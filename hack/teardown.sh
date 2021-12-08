c33a46482243ee51cc0881ac88e5ecfc069d259f
Martin Gencur <mgencur@redhat.com>
2021-12-08 17:22:23 +0100

    teardown.sh should only delete system namespaces

--- hack/teardown.sh
+++ hack/teardown.sh
@@ -10,4 +10,4 @@ debugging.setup
 teardown_serverless
 teardown_tracing
 delete_catalog_source
-delete_namespaces "${SYSTEM_NAMESPACES[@]}" "${TEST_NAMESPACES[@]}"
+delete_namespaces "${SYSTEM_NAMESPACES[@]}"
