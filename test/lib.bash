79cdecf0844d0cd7e2cbe4829670d0c44f48b599
Martin Gencur <mgencur@redhat.com>
2021-12-15 10:43:29 +0100

    Disable shellcheck for SC2153 in lib.bash

--- test/lib.bash
+++ test/lib.bash
@@ -243,6 +243,7 @@ function teardown {
   logger.warn "Teardown 💀"
   teardown_serverless
   teardown_tracing
+  # shellcheck disable=SC2153
   delete_namespaces "${SYSTEM_NAMESPACES[@]}" "${TEST_NAMESPACES[@]}"
   delete_catalog_source
   delete_users
