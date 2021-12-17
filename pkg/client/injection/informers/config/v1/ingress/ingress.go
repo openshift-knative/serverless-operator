2cb873d6de3f2398274d5ef8f572b5726ab568b3
Stavros Kontopoulos <skontopo@redhat.com>
2021-12-17 12:35:11 +0200

    make generated-files

--- pkg/client/injection/informers/config/v1/ingress/ingress.go
+++ pkg/client/injection/informers/config/v1/ingress/ingress.go
@@ -34,7 +34,7 @@ func withInformer(ctx context.Context) (context.Context, controller.Informer) {
 }
 
 func withDynamicInformer(ctx context.Context) context.Context {
-	inf := &wrapper{client: client.Get(ctx)}
+	inf := &wrapper{client: client.Get(ctx), resourceVersion: injection.GetResourceVersion(ctx)}
 	return context.WithValue(ctx, Key{}, inf)
 }
 
@@ -50,6 +50,8 @@ func Get(ctx context.Context) v1.IngressInformer {
 
 type wrapper struct {
 	client versioned.Interface
+
+	resourceVersion string
 }
 
 var _ v1.IngressInformer = (*wrapper)(nil)
@@ -63,10 +65,18 @@ func (w *wrapper) Lister() configv1.IngressLister {
 	return w
 }
 
+// SetResourceVersion allows consumers to adjust the minimum resourceVersion
+// used by the underlying client.  It is not accessible via the standard
+// lister interface, but can be accessed through a user-defined interface and
+// an implementation check e.g. rvs, ok := foo.(ResourceVersionSetter)
+func (w *wrapper) SetResourceVersion(resourceVersion string) {
+	w.resourceVersion = resourceVersion
+}
+
 func (w *wrapper) List(selector labels.Selector) (ret []*apiconfigv1.Ingress, err error) {
 	lo, err := w.client.ConfigV1().Ingresses().List(context.TODO(), metav1.ListOptions{
-		LabelSelector: selector.String(),
-		// TODO(mattmoor): Incorporate resourceVersion bounds based on staleness criteria.
+		LabelSelector:   selector.String(),
+		ResourceVersion: w.resourceVersion,
 	})
 	if err != nil {
 		return nil, err
@@ -79,6 +89,6 @@ func (w *wrapper) List(selector labels.Selector) (ret []*apiconfigv1.Ingress, er
 
 func (w *wrapper) Get(name string) (*apiconfigv1.Ingress, error) {
 	return w.client.ConfigV1().Ingresses().Get(context.TODO(), name, metav1.GetOptions{
-		// TODO(mattmoor): Incorporate resourceVersion bounds based on staleness criteria.
+		ResourceVersion: w.resourceVersion,
 	})
 }
