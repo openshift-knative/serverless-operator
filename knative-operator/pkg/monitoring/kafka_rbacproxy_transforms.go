a3bb261fa53cc8f56c4df01a6907db7d084e850d
Pierangelo Di Pilato <pierdipi@redhat.com>
2022-02-28 17:59:35 +0100

    [SRVKE-1154] Deploy RBAC proxy for kafka broker and sink components (#1409)
    * [SRVKE-1154] Deploy RBAC proxy for kafka broker and sink components
    
    For scraping metrics over TLS in OpenShift we need to deploy Service
    monitors and the RBAC proxy for:
    
    - kafka-controller
    - kafka-broker-receiver and kafka-broker-dispatcher
    - kafka-sink-receiver
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    * Decouple component names from ServiceAccounts
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    * Add E2E tests
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    * Add tests for montoring resources
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    * Reuse components to create svcs and sm lists
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- knative-operator/pkg/monitoring/kafka_rbacproxy_transforms.go
+++ knative-operator/pkg/monitoring/kafka_rbacproxy_transforms.go
@@ -5,29 +5,81 @@ import (
 	"errors"
 
 	mf "github.com/manifestival/manifestival"
-	serverlessoperatorv1alpha1 "github.com/openshift-knative/serverless-operator/knative-operator/pkg/apis/operator/v1alpha1"
-	"github.com/openshift-knative/serverless-operator/openshift-knative-operator/pkg/monitoring"
 	"k8s.io/apimachinery/pkg/util/sets"
 	operatorv1alpha1 "knative.dev/operator/pkg/apis/operator/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	serverlessoperatorv1alpha1 "github.com/openshift-knative/serverless-operator/knative-operator/pkg/apis/operator/v1alpha1"
+	"github.com/openshift-knative/serverless-operator/openshift-knative-operator/pkg/monitoring"
 )
 
 var (
-	KafkaChannelComponents = []string{"kafka-ch-controller", "kafka-webhook"}
-	KafkaSourceComponents  = []string{"kafka-controller-manager"}
+	KafkaController = Component{
+		Name:               "kafka-controller",
+		ServiceAccountName: "kafka-controller",
+	}
+	KafkaWebhook = Component{
+		Name:               "kafka-webhook-eventing",
+		ServiceAccountName: "kafka-webhook-eventing",
+	}
+
+	KafkaBrokerReceiver = Component{
+		Name:               "kafka-broker-receiver",
+		ServiceAccountName: "knative-kafka-broker-data-plane",
+	}
+	KafkaBrokerDispatcher = Component{
+		Name:               "kafka-broker-dispatcher",
+		ServiceAccountName: "knative-kafka-broker-data-plane",
+	}
+
+	KafkaSinkReceiver = Component{
+		Name:               "kafka-sink-receiver",
+		ServiceAccountName: "knative-kafka-sink-data-plane",
+	}
+
+	KafkaChannelController = Component{
+		Name:               "kafka-ch-controller",
+		ServiceAccountName: "kafka-ch-controller",
+	}
+	KafkaChannelWebhook = Component{
+		Name:               "kafka-webhook",
+		ServiceAccountName: "kafka-webhook",
+	}
+
+	KafkaSourceController = Component{
+		Name:               "kafka-controller-manager",
+		ServiceAccountName: "kafka-controller-manager",
+	}
+
+	deployments = []string{
+		KafkaController.Name,
+		KafkaWebhook.Name,
+		KafkaBrokerReceiver.Name,
+		KafkaBrokerDispatcher.Name,
+		KafkaSinkReceiver.Name,
+		KafkaChannelController.Name,
+		KafkaChannelWebhook.Name,
+		KafkaSourceController.Name,
+	}
 )
 
-func AddRBACProxySupportToManifest(instance *serverlessoperatorv1alpha1.KnativeKafka, components []string) (*mf.Manifest, error) {
+// Component is a target for scraping metrics.
+type Component struct {
+	Name               string
+	ServiceAccountName string
+}
+
+func AddRBACProxyToManifest(instance *serverlessoperatorv1alpha1.KnativeKafka, components ...Component) (*mf.Manifest, error) {
 	proxyManifest := mf.Manifest{}
 	// Only create the roles needed for the deployment service accounts as Prometheus has already
 	// the rights needed due to eventing that is assumed to be installed.
 	for _, c := range components {
-		crbM, err := monitoring.CreateClusterRoleBindingManifest(c, instance.GetNamespace())
+		crbM, err := monitoring.CreateClusterRoleBindingManifest(c.ServiceAccountName, instance.GetNamespace())
 		if err != nil {
 			return nil, err
 		}
 		proxyManifest = proxyManifest.Append(*crbM)
-		if err = monitoring.AppendManifestsForComponent(c, instance.GetNamespace(), &proxyManifest); err != nil {
+		if err = monitoring.AppendManifestsForComponent(c.Name, instance.GetNamespace(), &proxyManifest); err != nil {
 			return nil, err
 		}
 	}
@@ -44,7 +96,7 @@ func GetRBACProxyInjectTransformer(apiClient client.Client) (mf.Transformer, err
 		return nil, errors.New("eventing instance not found")
 	}
 	if monitoring.ShouldEnableMonitoring(eventingList.Items[0].GetSpec().GetConfig()) {
-		return monitoring.InjectRbacProxyContainerToDeployments(sets.NewString(append(KafkaChannelComponents, KafkaSourceComponents...)...)), nil
+		return monitoring.InjectRbacProxyContainerToDeployments(sets.NewString(deployments...)), nil
 	}
 	return nil, nil
 }
