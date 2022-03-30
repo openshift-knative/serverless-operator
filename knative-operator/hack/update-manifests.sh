bc6eceb245390a1140a04ce33fe7e1d9fa8b634f
Pierangelo Di Pilato <pierdipi@redhat.com>
2022-03-30 15:19:39 +0200

    [SRVKE-1184] KafkaSource lease name remapping (#1441)
    * [SRVKE-1184] KafkaSource lease name remapping
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    * Run make generated-files
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- knative-operator/hack/update-manifests.sh
+++ knative-operator/hack/update-manifests.sh
@@ -79,3 +79,6 @@ git apply "$root/knative-operator/hack/011-eventing-kafkachannel-dead-letter-sin
 git apply "$root/knative-operator/hack/012-eventing-kafkachannel-addressable-resolver-binding.patch"
 
 git apply "$root/knative-operator/hack/013-eventing-generate-name-jobs.patch"
+
+# SRVKE-1184 Migration to Broker components
+git apply "$root/knative-operator/hack/014-eventing-kafka-lease-name-remapping.patch"
