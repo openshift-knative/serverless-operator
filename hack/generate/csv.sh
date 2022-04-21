73fb88046003fa65a4fb51fe8167c85e02814013
Pierangelo Di Pilato <pierdipi@redhat.com>
2022-04-21 19:06:27 +0200

    Add kafka-channel-receiver and kafka-channel-dispatcher image override
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- hack/generate/csv.sh
+++ hack/generate/csv.sh
@@ -70,18 +70,16 @@ image "pingsource-mt-adapter__dispatcher"           "${eventing}-mtping"
 image "APISERVER_RA_IMAGE"   "${eventing}-apiserver-receive-adapter"
 image "DISPATCHER_IMAGE"     "${eventing}-channel-dispatcher"
 
-kafka_image "kafka-broker-receiver__kafka-broker-receiver"      "${eventing_kafka_broker}-broker-receiver"
-kafka_image "kafka-broker-dispatcher__kafka-broker-dispatcher"  "${eventing_kafka_broker}-broker-dispatcher"
-kafka_image "kafka-controller__controller"                      "${eventing_kafka_broker}-broker-kafka-controller"
-kafka_image "kafka-sink-receiver__kafka-sink-receiver"          "${eventing_kafka_broker}-broker-receiver"
-kafka_image "kafka-source-dispatcher__kafka-source-dispatcher"  "${eventing_kafka_broker}-broker-dispatcher"
-kafka_image "kafka-webhook-eventing__kafka-webhook-eventing"    "${eventing_kafka_broker}-broker-webhook-kafka"
-kafka_image "kafka-controller-post-install__post-install"      "${eventing_kafka_broker}-broker-post-install"
-kafka_image "knative-kafka-storage-version-migrator__migrate"   "${eventing}-storage-version-migration" # Use eventing core image
-
-## Todo: For the source we currently reuse one one from eventing core.
-## We might want to rethink this. for direct build on the "broker" repo.
-kafka_image "knative-kafka-storage-version-migrator-__migrate"    "${eventing}-storage-version-migration"
+kafka_image "kafka-broker-receiver__kafka-broker-receiver"       "${eventing_kafka_broker}-broker-receiver"
+kafka_image "kafka-broker-dispatcher__kafka-broker-dispatcher"   "${eventing_kafka_broker}-broker-dispatcher"
+kafka_image "kafka-channel-receiver__kafka-channel-receiver"     "${eventing_kafka_broker}-broker-receiver"
+kafka_image "kafka-channel-dispatcher__kafka-channel-dispatcher" "${eventing_kafka_broker}-broker-dispatcher"
+kafka_image "kafka-controller__controller"                       "${eventing_kafka_broker}-broker-kafka-controller"
+kafka_image "kafka-sink-receiver__kafka-sink-receiver"           "${eventing_kafka_broker}-broker-receiver"
+kafka_image "kafka-source-dispatcher__kafka-source-dispatcher"   "${eventing_kafka_broker}-broker-dispatcher"
+kafka_image "kafka-webhook-eventing__kafka-webhook-eventing"     "${eventing_kafka_broker}-broker-webhook-kafka"
+kafka_image "kafka-controller-post-install__post-install"        "${eventing_kafka_broker}-broker-post-install"
+kafka_image "knative-kafka-storage-version-migrator__migrate"    "${eventing}-storage-version-migration" # Use eventing core image
 
 image 'KUBE_RBAC_PROXY'          "${rbac_proxy}"
 image 'KN_PLUGIN_EVENT_SENDER'   "${kn_event}-sender"
