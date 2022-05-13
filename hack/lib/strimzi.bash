d454d109923a3be086b63b7496328396f59a8feb
Matthias Wessendorf <mwessend@redhat.com>
2022-05-13 16:28:13 +0200

    Being explicit about authorization for our test kafkausers
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- hack/lib/strimzi.bash
+++ hack/lib/strimzi.bash
@@ -43,6 +43,8 @@ function install_strimzi_cluster {
             tls: true
             authentication:
               type: scram-sha-512
+        authorization:
+          type: simple
         config:
           offsets.topic.replication.factor: 3
           transaction.state.log.replication.factor: 3
