e0b372b549a9220984863505c9afd78b363125e2
Martin Gencur <mgencur@redhat.com>
2022-05-18 11:49:30 +0200

    Wait for image pull secret to be associated with SA

--- hack/lib/catalogsource.bash
+++ hack/lib/catalogsource.bash
@@ -84,6 +84,7 @@ EOF
   timeout 120 "[[ \$(oc -n $OLM_NAMESPACE get pods -l olm.catalogSource=serverless-operator --no-headers | wc -l) != 1 ]]"
   index_pod=$(oc -n "$OLM_NAMESPACE" get pods -l olm.catalogSource=serverless-operator -oname)
   if ! oc -n "$OLM_NAMESPACE" get "$index_pod" -ojsonpath='{.spec.imagePullSecrets}' | grep dockercfg &>/dev/null; then
+    timeout 120 "[[ \$(oc -n $OLM_NAMESPACE get sa serverless-operator -ojsonpath='{.imagePullSecrets}' | grep -c dockercfg) == 0 ]]"
     oc -n "$OLM_NAMESPACE" delete pods -l olm.catalogSource=serverless-operator
   fi
 
