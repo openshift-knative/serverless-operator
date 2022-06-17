acf5ab10e25fd156436960335965ed77f6d87a07
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2022-06-17 17:33:39 +0900

    Inject `SERVING_NAMESPACE` env value to kourier controller (#1623)
    Since https://github.com/knative-sandbox/net-kourier/pull/855, kourier
    controller supports SERVING_NAMESPACE env value to specify the serving
    namespace.
    
    The feature is required when internal encryption is supported.

--- openshift-knative-operator/pkg/serving/extension.go
+++ openshift-knative-operator/pkg/serving/extension.go
@@ -71,7 +71,7 @@ func (e *extension) Transformers(ks operatorv1alpha1.KComponent) []mf.Transforme
 			corev1.EnvVar{Name: "NO_PROXY", Value: os.Getenv("NO_PROXY")},
 		),
 		overrideKourierNamespace(ks),
-		addHTTPOptionDisabledEnvValue(),
+		addKourierEnvValues(),
 		enableSecretInformerFiltering(ks),
 	}, monitoring.GetServingTransformers(ks)...)
 }
