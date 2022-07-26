2ac1c4a70742f72cdb873be4af566d9244de01be
Stavros Kontopoulos <skontopo@redhat.com>
2022-07-26 06:17:43 +0300

    Remove istio, kourier patches (#1642)
    * remove istio, kourier patches
    
    * image injection
    
    * remove patches
    
    * revert branches

--- openshift-knative-operator/hack/update-manifests.sh
+++ openshift-knative-operator/hack/update-manifests.sh
@@ -15,7 +15,9 @@ eventing_files=(eventing-crds.yaml eventing-core.yaml in-memory-channel.yaml mt-
 
 # This excludes the gateways and peerauthentication settings as we want customers to do
 # manipulate those.
-istio_files=(200-clusterrole 400-config-istio 500-controller 500-webhook-deployment 500-webhook-secret 500-webhook-service 600-mutating-webhook 600-validating-webhook)
+istio_files=(networkpolicy-mesh 200-clusterrole 400-config-istio 500-controller 500-webhook-deployment 500-webhook-secret 500-webhook-service 600-mutating-webhook 600-validating-webhook)
+
+kourier_files=(kourier config-network)
 
 export KNATIVE_EVENTING_MANIFESTS_DIR=${KNATIVE_EVENTING_MANIFESTS_DIR:-""}
 export KNATIVE_SERVING_MANIFESTS_DIR=${KNATIVE_SERVING_MANIFESTS_DIR:-""}
@@ -28,6 +30,7 @@ function download_serving {
   shift
 
   files=("$@")
+  echo "Files: ${files[*]}"
 
   component_dir="$root/openshift-knative-operator/cmd/operator/kodata/knative-${component}"
   target_dir="${component_dir}/${version:1}"
@@ -66,12 +69,13 @@ function download_eventing {
   rm -r "$component_dir"
   mkdir -p "$target_dir"
 
+  branch=$(metadata.get dependencies.eventing_artifacts_branch)
   for ((i = 0; i < ${#files[@]}; i++)); do
     index=$(( i+1 ))
     file="${files[$i]}"
     target_file="$target_dir/$index-$file"
     if [[ ${KNATIVE_EVENTING_MANIFESTS_DIR} = "" ]]; then
-      branch=$(metadata.get dependencies.eventing_artifacts_branch)
+
       url="https://raw.githubusercontent.com/openshift/knative-eventing/${branch}/openshift/release/artifacts/$file"
       echo "Downloading file from ${url}"
       wget --no-check-certificate "$url" -O "$target_file"
@@ -91,22 +95,29 @@ function download_ingress {
   shift
 
   files=("$@")
+  echo "Files: ${files[*]}"
 
   ingress_dir="$root/openshift-knative-operator/cmd/operator/kodata/ingress/$(versions.major_minor "${KNATIVE_SERVING_VERSION}")"
-  rm -r "$ingress_dir"
+  SKIP_TARGET_DELETION=${SKIP_TARGET_DELETION:-false}
+  if [[ "${SKIP_TARGET_DELETION}" == "false" ]]; then
+    rm -rf "$ingress_dir"
+  fi
   mkdir -p "$ingress_dir"
 
+  branch=$(metadata.get "dependencies.${component/-/_}_artifacts_branch")
+  index=0
   for (( i=0; i<${#files[@]}; i++ ));
   do
-    index=$(( i+1 ))
+
     file="${files[$i]}.yaml"
     ingress_target_file="$ingress_dir/$index-$file"
-    url="https://raw.githubusercontent.com/knative-sandbox/${component}/knative-${version}/config/${file}"
+    url="https://raw.githubusercontent.com/openshift-knative/${component}/${branch}/openshift/release/artifacts/$index-$file"
 
     wget --no-check-certificate "$url" -O "$ingress_target_file"
 
     # Break all image references so we know our overrides work correctly.
     yaml.break_image_references "$ingress_target_file"
+    index=$(( i+1 ))
   done
 }
 
@@ -129,34 +140,10 @@ fi
 
 download_ingress net-istio "v$(metadata.get dependencies.net_istio)" "${istio_files[@]}"
 
-url="https://github.com/knative-sandbox/net-kourier/releases/download/knative-v$(metadata.get dependencies.kourier)/kourier.yaml"
-kourier_dir="$root/openshift-knative-operator/cmd/operator/kodata/ingress/$(versions.major_minor "${KNATIVE_SERVING_VERSION}")"
-kourier_file="$kourier_dir/0-kourier.yaml"
-wget --no-check-certificate "$url" -O "$kourier_file"
-# TODO: [SRVKS-610] These values should be replaced by operator instead of sed.
-sed -i -e 's/net-kourier-controller.knative-serving/net-kourier-controller/g' "$kourier_file"
-# Break all image references so we know our overrides work correctly.
-yaml.break_image_references "$kourier_file"
-# Download config-network.yaml for Kourier. This is necessary as kourier uses different namespace (knative-serving-ingress).
-config_network_url="https://raw.githubusercontent.com/knative/networking/release-$(versions.major_minor "${KNATIVE_SERVING_VERSION}")/config/config-network.yaml"
-config_network="$kourier_dir/1-config-network.yaml"
-wget --no-check-certificate "$config_network_url" -O "$config_network"
-sed -i -e '/labels:$/a \    app.kubernetes.io\/component: kourier' "$config_network"
-sed -i -e '/labels:$/a \    networking.knative.dev\/ingress-provider: kourier' "$config_network"
-
-# Add networkpolicy for webhook when net-istio is enabled.
-git apply "$root/openshift-knative-operator/hack/007-networkpolicy-mesh.patch"
-
-# Make Kourier rollout in a more defensive way so no requests get dropped.
-# TODO: Can probably be removed in 1.21 and/or be sent upstream.
-git apply "$root/openshift-knative-operator/hack/008-kourier-rollout.patch"
+SKIP_TARGET_DELETION=true download_ingress net-kourier "v$(metadata.get dependencies.net_kourier)" "${kourier_files[@]}"
 
 #
 # DOWNLOAD EVENTING
 #
 download_eventing eventing "$KNATIVE_EVENTING_VERSION" "${eventing_files[@]}"
 
-# __Note__
-# artifacts are downloaded from midstream openshift/release/artifacts directory.
-# Before adding patches to this file consider sending a patch to midstream and then by just running
-# `make generated-files` the patch will appear in the final bundled artifacts.
