f4d36a1f29c8d4565aa83250f8f2a67bdef764ea
Pierangelo Di Pilato <pierdipi@redhat.com>
2022-08-08 11:15:50 +0200

    Fix linter error
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- knative-operator/pkg/controller/knativeeventing/knativeeventing_controller_test.go
+++ knative-operator/pkg/controller/knativeeventing/knativeeventing_controller_test.go
@@ -3,37 +3,10 @@ package knativeeventing
 import (
 	"os"
 
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
-	operatorv1alpha1 "knative.dev/operator/pkg/apis/operator/v1alpha1"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	"github.com/openshift-knative/serverless-operator/knative-operator/pkg/apis"
 	"github.com/openshift-knative/serverless-operator/knative-operator/pkg/monitoring/dashboards"
-)
-
-var (
-	ke = &operatorv1alpha1.KnativeEventing{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "knative-eventing",
-			Namespace: "knative-eventing",
-		},
-	}
-	req = reconcile.Request{
-		NamespacedName: types.NamespacedName{Namespace: ke.Namespace, Name: ke.Name},
-	}
-	dashboardNamespace = &corev1.Namespace{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: dashboards.ConfigManagedNamespace,
-		},
-	}
-	eventingNamespace = &corev1.Namespace{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "knative-eventing",
-		},
-	}
 )
 
 func init() {
