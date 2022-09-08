e4936b0f0de313303a64ebe414bd76f7efd7a399
Martin Gencur <mgencur@redhat.com>
2022-09-08 11:44:29 +0200

    Remove TestKnativeServiceHTTPS when internal-encryption is enabled
    * The Route is passthrough with insecureEdgeTerminationPolicy:Redirect.
    The kourier gateway servers only HTTPS and it requires certificates.

--- test/servinge2e/kourier/verify_http_and_https_test.go
+++ test/servinge2e/kourier/verify_http_and_https_test.go
@@ -1,47 +1,12 @@
 package kourier
 
 import (
-	"context"
 	"net/http"
 	"testing"
 
 	"github.com/openshift-knative/serverless-operator/test"
 	"github.com/openshift-knative/serverless-operator/test/servinge2e"
-	pkgTest "knative.dev/pkg/test"
-	"knative.dev/pkg/test/spoof"
 )
-
-func TestKnativeServiceHTTPS(t *testing.T) {
-
-	caCtx := test.SetupClusterAdmin(t)
-	test.CleanupOnInterrupt(t, func() { test.CleanupAll(t, caCtx) })
-	defer test.CleanupAll(t, caCtx)
-
-	ksvc, err := test.WithServiceReady(caCtx, "https-service", test.Namespace, image)
-	if err != nil {
-		t.Fatal("Knative Service not ready", err)
-	}
-
-	// Implicitly checks that HTTPS works.
-	servinge2e.WaitForRouteServingText(t, caCtx, ksvc.Status.URL.URL(), helloworldText)
-
-	// Now check that HTTP works.
-	httpURL := ksvc.Status.URL.DeepCopy()
-
-	httpURL.Scheme = "http"
-	if _, err := pkgTest.CheckEndpointState(
-		context.Background(),
-		caCtx.Clients.Kube,
-		t.Logf,
-		httpURL.URL(),
-		spoof.MatchesBody(helloworldText),
-		"WaitForRouteToServeText",
-		true,
-	); err != nil {
-		t.Fatalf("The Route at domain %s didn't serve the expected text %q: %v", httpURL, helloworldText, err)
-	}
-
-}
 
 func TestKnativeServiceHTTPRedirect(t *testing.T) {
 
