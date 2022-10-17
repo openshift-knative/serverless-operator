26e8788e4bbf28ac9bd38287d405cb1867fc288b
Martin Gencur <mgencur@redhat.com>
2022-10-17 11:58:18 +0200

    Commit images.go

---
+++ test/images.go
@@ -0,0 +1,9 @@
+package test
+
+// The list of images used by serverless-operator test suite.
+const (
+	HelloworldGoImg   = "helloworld-go"
+	HelloOpenshiftImg = "hello-openshift"
+	// registry.ci.openshift.org/openshift/knative-v0.17.3:knative-serving-test-httpproxy
+	HttpProxyImg = "knative-serving-test-httpproxy"
+)
