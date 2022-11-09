271211131d1fdcbf25c3e73e0824e03ed789fd4b
Pierangelo Di Pilato <pierdipi@redhat.com>
2022-11-09 08:49:22 +0100

    Fix eventing test images tags in image template
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- test/lib.bash
+++ test/lib.bash
@@ -198,7 +198,7 @@ function run_rolling_upgrade_tests {
   local base serving_image_version eventing_image_version eventing_kafka_image_version eventing_kafka_broker_image_version image_template channels common_opts
 
   serving_image_version=$(versions.major_minor "${KNATIVE_SERVING_VERSION}")
-  eventing_image_version=$(versions.major_minor "${KNATIVE_EVENTING_VERSION}")
+  eventing_image_version="${KNATIVE_EVENTING_VERSION}"
   eventing_kafka_image_version=$(versions.major_minor "${KNATIVE_EVENTING_KAFKA_VERSION}")
   eventing_kafka_broker_image_version=$(versions.major_minor "${KNATIVE_EVENTING_KAFKA_BROKER_VERSION}")
 
@@ -209,19 +209,19 @@ function run_rolling_upgrade_tests {
 $base{{- with .Name }}
 {{- if eq .      "wathola-kafka-sender"}}{{.}}:v$eventing_kafka_image_version
 {{- else if eq . "kafka-consumer"      }}knative-eventing-kafka-broker-test-kafka-consumer:knative-v$eventing_kafka_broker_image_version
-{{- else if eq . "event-flaker"        }}knative-eventing-test-event-flaker:knative-v$eventing_image_version
-{{- else if eq . "event-library"       }}knative-eventing-test-event-library:knative-v$eventing_image_version
-{{- else if eq . "event-sender"        }}knative-eventing-test-event-sender:knative-v$eventing_image_version
-{{- else if eq . "eventshub"           }}knative-eventing-test-eventshub:knative-v$eventing_image_version
-{{- else if eq . "heartbeats"          }}knative-eventing-test-heartbeats:knative-v$eventing_image_version
-{{- else if eq . "performance"         }}knative-eventing-test-performance:knative-v$eventing_image_version
-{{- else if eq . "print"               }}knative-eventing-test-print:knative-v$eventing_image_version
-{{- else if eq . "recordevents"        }}knative-eventing-test-recordevents:knative-v$eventing_image_version
-{{- else if eq . "request-sender"      }}knative-eventing-test-request-sender:knative-v$eventing_image_version
-{{- else if eq . "wathola-fetcher"     }}knative-eventing-test-wathola-fetcher:knative-v$eventing_image_version
-{{- else if eq . "wathola-forwarder"   }}knative-eventing-test-wathola-forwarder:knative-v$eventing_image_version
-{{- else if eq . "wathola-receiver"    }}knative-eventing-test-wathola-receiver:knative-v$eventing_image_version
-{{- else if eq . "wathola-sender"      }}knative-eventing-test-wathola-sender:knative-v$eventing_image_version
+{{- else if eq . "event-flaker"        }}knative-eventing-test-event-flaker:$eventing_image_version
+{{- else if eq . "event-library"       }}knative-eventing-test-event-library:$eventing_image_version
+{{- else if eq . "event-sender"        }}knative-eventing-test-event-sender:$eventing_image_version
+{{- else if eq . "eventshub"           }}knative-eventing-test-eventshub:$eventing_image_version
+{{- else if eq . "heartbeats"          }}knative-eventing-test-heartbeats:$eventing_image_version
+{{- else if eq . "performance"         }}knative-eventing-test-performance:$eventing_image_version
+{{- else if eq . "print"               }}knative-eventing-test-print:$eventing_image_version
+{{- else if eq . "recordevents"        }}knative-eventing-test-recordevents:$eventing_image_version
+{{- else if eq . "request-sender"      }}knative-eventing-test-request-sender:$eventing_image_version
+{{- else if eq . "wathola-fetcher"     }}knative-eventing-test-wathola-fetcher:$eventing_image_version
+{{- else if eq . "wathola-forwarder"   }}knative-eventing-test-wathola-forwarder:$eventing_image_version
+{{- else if eq . "wathola-receiver"    }}knative-eventing-test-wathola-receiver:$eventing_image_version
+{{- else if eq . "wathola-sender"      }}knative-eventing-test-wathola-sender:$eventing_image_version
 {{- else                               }}{{.}}:v$serving_image_version{{end -}}
 {{end -}}
 EOF
