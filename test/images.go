ec309357dc3cf7898703fa0959316a334e82ec67
Marek Schmidt <maschmid@redhat.com>
2023-01-09 14:58:42 +0100

    make eventinge2e verify the events were received (#1867)
    * make eventinge2e verify the events were received
    
    * lint
    
    * goimports
    
    * add SYSTEM_NAMESPACE for tests using eventing/test/lib
    
    * use a json message instead of plain text in kafka_source_to_ksvc_test
    
    * export PingSourceData
    
    * use plain bootstrap for the cron sender, as it lacks the secret
    
    * cleanup
    
    * delete the broker in source_native_kafka_broker_ksvc_test
    
    * nits
    
    * SCALE_UP=4 on test-e2e-with-kafka

--- test/images.go
+++ test/images.go
@@ -2,7 +2,9 @@ package test
 
 // The list of images used by serverless-operator test suite.
 const (
-	HelloworldGoImg   = "helloworld-go"
-	HelloOpenshiftImg = "hello-openshift"
-	HTTPProxyImg      = "httpproxy"
+	HelloworldGoImg     = "helloworld-go"
+	HelloOpenshiftImg   = "hello-openshift"
+	HTTPProxyImg        = "httpproxy"
+	RecordEventsImg     = "recordevents"
+	WatholaForwarderImg = "wathola-forwarder"
 )
