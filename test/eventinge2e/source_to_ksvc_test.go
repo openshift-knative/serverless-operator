9afa67f05136bab692bf02e4de18a3f0c2262a74
Marek Schmidt <maschmid@redhat.com>
2023-01-12 18:00:08 +0100

    release 1.27 e2e eventing fix backport (#1926)
    * make eventinge2e verify the events were received (#1867)
    
    * make eventinge2e verify the events were received
    
    * lint
    
    * goimports
    
    * add SYSTEM_NAMESPACE for tests using eventing/test/lib
    
    * use a json message instead of plain text in kafka_source_to_ksvc_test
    
    * export PingSourceData
    
    * use plain bootstrap for the cron sender, as it lacks the secret
    
    * cleanup
    
    * delete the broker in source_native_kafka_broker_ksvc_test
    
    * nits
    
    * SCALE_UP=4 on test-e2e-with-kafka
    
    * increase timeout in downstream eventing+kafka tests

--- test/eventinge2e/source_to_ksvc_test.go
+++ test/eventinge2e/source_to_ksvc_test.go
@@ -5,20 +5,9 @@ import (
 	"testing"
 
 	"github.com/openshift-knative/serverless-operator/test"
-	"github.com/openshift-knative/serverless-operator/test/servinge2e"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	pkgTest "knative.dev/pkg/test"
-
 	sourcesv1 "knative.dev/eventing/pkg/apis/sources/v1"
 	duckv1 "knative.dev/pkg/apis/duck/v1"
-)
-
-const (
-	pingSourceName    = "smoke-test-ping"
-	helloWorldService = "helloworld-go"
-	helloWorldText    = "Hello World!"
-	ksvcAPIVersion    = "serving.knative.dev/v1"
-	ksvcKind          = "Service"
 )
 
 func TestKnativeSourceToKnativeService(t *testing.T) {
@@ -30,11 +19,7 @@ func TestKnativeSourceToKnativeService(t *testing.T) {
 	test.CleanupOnInterrupt(t, cleanup)
 	defer cleanup()
 
-	// Setup a knative service
-	ksvc, err := test.WithServiceReady(client, helloWorldService, test.Namespace, pkgTest.ImagePath(test.HelloworldGoImg))
-	if err != nil {
-		t.Fatal("Knative Service not ready", err)
-	}
+	eventStore, ksvc := DeployKsvcWithEventInfoStoreOrFail(client, t, test.Namespace, helloWorldService)
 
 	ps := &sourcesv1.PingSource{
 		ObjectMeta: metav1.ObjectMeta{
@@ -42,7 +27,7 @@ func TestKnativeSourceToKnativeService(t *testing.T) {
 			Namespace: test.Namespace,
 		},
 		Spec: sourcesv1.PingSourceSpec{
-			Data: helloWorldText,
+			Data: PingSourceData,
 			SourceSpec: duckv1.SourceSpec{
 				Sink: duckv1.Destination{
 					Ref: &duckv1.KReference{
@@ -54,9 +39,10 @@ func TestKnativeSourceToKnativeService(t *testing.T) {
 			},
 		},
 	}
-	_, err = client.Clients.Eventing.SourcesV1().PingSources(test.Namespace).Create(context.Background(), ps, metav1.CreateOptions{})
+	_, err := client.Clients.Eventing.SourcesV1().PingSources(test.Namespace).Create(context.Background(), ps, metav1.CreateOptions{})
 	if err != nil {
 		t.Fatal("Knative PingSource not created: %+V", err)
 	}
-	servinge2e.WaitForRouteServingText(t, client, ksvc.Status.URL.URL(), helloWorldText)
+
+	AssertPingSourceDataReceivedAtLeastOnce(eventStore)
 }
