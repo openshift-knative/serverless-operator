030d03662129f1caf752695d983533ea02387dc5
Martin Gencur <mgencur@redhat.com>
2023-02-09 15:50:17 +0100

    Restructure Broker kitchensink tests to separate definition and execution

--- test/kitchensinke2e/broker_test.go
+++ test/kitchensinke2e/broker_test.go
@@ -9,9 +9,53 @@ import (
 	"knative.dev/reconciler-test/pkg/feature"
 )
 
-func BrokerReadiness(testLabel string, broker component, brokerDls component, triggers []component, triggerDls component) *feature.Feature {
+var brokers = []component{
+	inMemoryChannelMtBroker,
+	kafkaChannelMtBroker,
+	kafkaBroker,
+}
+
+var deadLetterSinks = []component{
+	kafkaChannel,
+	inMemoryChannel,
+	genericChannelWithKafkaChannelTemplate,
+	genericChannelWithInMemoryChannelTemplate,
+	inMemoryChannelMtBroker,
+	kafkaChannelMtBroker,
+	kafkaBroker,
+	inMemoryChannelSequence,
+	kafkaChannelSequence,
+	inMemoryChannelParallel,
+	kafkaChannelParallel,
+	ksvc,
+}
+
+var triggers = []component{
+	kafkaChannel,
+	inMemoryChannel,
+	genericChannelWithKafkaChannelTemplate,
+	genericChannelWithInMemoryChannelTemplate,
+	inMemoryChannelMtBroker,
+	kafkaChannelMtBroker,
+	kafkaBroker,
+	inMemoryChannelSequence,
+	kafkaChannelSequence,
+	inMemoryChannelParallel,
+	kafkaChannelParallel,
+	ksvc,
+}
+
+func BrokerReadiness(broker component, brokerDls component, triggers []component, triggerDls component) *feature.Feature {
+	testLabel := shortLabel(broker)
+	if brokerDls != nil {
+		testLabel = testLabel + "b" + shortLabel(brokerDls)
+	}
+	if triggerDls != nil {
+		testLabel = testLabel + "t" + shortLabel(triggerDls)
+	}
+
 	brokerName := testLabel
-	dlsName := testLabel + "-dls"
+	dlsName := testLabel + "-bdls"
 	triggerDlsName := testLabel + "-tdls"
 	f := feature.NewFeatureNamed(fmt.Sprintf("%s with %s Broker dls and %s Trigger dls", label(broker), label(brokerDls), label(triggerDls)))
 
@@ -54,78 +98,51 @@ func BrokerReadiness(testLabel string, broker component, brokerDls component, tr
 	return f
 }
 
-func TestBrokerReadiness(t *testing.T) {
-	brokers := []component{
-		inMemoryChannelMtBroker,
-		kafkaChannelMtBroker,
-		kafkaBroker,
-	}
-
-	deadLetterSinks := []component{
-		kafkaChannel,
-		inMemoryChannel,
-		genericChannelWithKafkaChannelTemplate,
-		genericChannelWithInMemoryChannelTemplate,
-		inMemoryChannelMtBroker,
-		kafkaChannelMtBroker,
-		kafkaBroker,
-		inMemoryChannelSequence,
-		kafkaChannelSequence,
-		inMemoryChannelParallel,
-		kafkaChannelParallel,
-		ksvc,
-	}
-
-	triggers := []component{
-		kafkaChannel,
-		inMemoryChannel,
-		genericChannelWithKafkaChannelTemplate,
-		genericChannelWithInMemoryChannelTemplate,
-		inMemoryChannelMtBroker,
-		kafkaChannelMtBroker,
-		kafkaBroker,
-		inMemoryChannelSequence,
-		kafkaChannelSequence,
-		inMemoryChannelParallel,
-		kafkaChannelParallel,
-		ksvc,
-	}
-
+func BrokerFeatureSetWithBrokerDLS() feature.FeatureSet {
+	var features []*feature.Feature
 	// Test all combinations of Broker X DeadLetterSinks, each broker with all possible Triggers
 	// with the DeadLetterSink set on the Broker
 	for _, broker := range brokers {
 		for _, deadLetterSink := range deadLetterSinks {
 			broker := broker
 			deadLetterSink := deadLetterSink
-
-			testLabel := shortLabel(broker) + shortLabel(deadLetterSink)
-
-			t.Run(testLabel, func(t *testing.T) {
-				t.Parallel()
-
-				ctx, env := defaultContext(t)
-
-				env.Test(ctx, t, BrokerReadiness(testLabel, broker, deadLetterSink, triggers, nil))
-			})
+			features = append(features, BrokerReadiness(broker, deadLetterSink, triggers, nil))
 		}
 	}
+	return feature.FeatureSet{
+		Name:     "BrokerDLS",
+		Features: features,
+	}
+}
 
+func BrokerFeatureSetWithTriggerDLS() feature.FeatureSet {
+	var features []*feature.Feature
 	// Test all combinations of Broker X DeadLetterSinks, each broker with all possible Triggers
 	// with the DeadLetterSink set on the Trigger
 	for _, broker := range brokers {
 		for _, deadLetterSink := range deadLetterSinks {
 			broker := broker
 			deadLetterSink := deadLetterSink
+			features = append(features, BrokerReadiness(broker, nil, triggers, deadLetterSink))
+		}
+	}
+	return feature.FeatureSet{
+		Name:     "TriggerDLS",
+		Features: features,
+	}
+}
 
-			// Just to distinguish the label from the ones above, we add "t" for a "T"rigger deadLetterSink
-			testLabel := shortLabel(broker) + "t" + shortLabel(deadLetterSink)
-
-			t.Run(testLabel, func(t *testing.T) {
+func TestBrokerReadiness(t *testing.T) {
+	featureSets := []feature.FeatureSet{
+		BrokerFeatureSetWithBrokerDLS(),
+		BrokerFeatureSetWithTriggerDLS(),
+	}
+	for _, fs := range featureSets {
+		for _, f := range fs.Features {
+			t.Run(fs.Name, func(t *testing.T) {
 				t.Parallel()
-
 				ctx, env := defaultContext(t)
-
-				env.Test(ctx, t, BrokerReadiness(testLabel, broker, nil, triggers, deadLetterSink))
+				env.Test(ctx, t, f)
 			})
 		}
 	}
