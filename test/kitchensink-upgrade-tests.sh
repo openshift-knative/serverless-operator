04734bf3f3e9a072febd337bfc805d5978f29cdb
Martin Gencur <mgencur@redhat.com>
2023-02-14 10:09:13 +0100

    Commit kitchensink-upgrade-tests.sh

---
+++ test/kitchensink-upgrade-tests.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC1091,SC1090
+source "$(dirname "${BASH_SOURCE[0]}")/lib.bash"
+
+set -Eeuo pipefail
+
+# Enable extra verbosity if running in CI.
+if [ -n "$OPENSHIFT_CI" ]; then
+  env
+fi
+debugging.setup # both install and test
+dump_state.setup # test
+
+logger.success '🚀 Cluster prepared for testing.'
+
+kitchensink_upgrade_tests
+
+success
