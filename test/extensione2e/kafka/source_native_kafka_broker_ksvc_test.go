b2d929a0b6f5e79a9d6467e63f681846f5c28f72
Marek Schmidt <maschmid@redhat.com>
2023-02-16 11:02:08 +0100

    skip test for SRVKE-1408

--- test/extensione2e/kafka/source_native_kafka_broker_ksvc_test.go
+++ test/extensione2e/kafka/source_native_kafka_broker_ksvc_test.go
@@ -42,6 +42,7 @@ func TestSourceToNativeKafkaBrokerToKnativeService(t *testing.T) {
 func TestSourceToNamespacedKafkaBrokerToKnativeService(t *testing.T) {
 	eventinge2e.KnativeSourceBrokerTriggerKnativeService(t, createBrokerFunc(t, kafka.NamespacedBrokerClass), func(ctx *test.Context) {
 		t.Run("verify namespaced Kafka Broker data plane metrics work correctly", func(t *testing.T) {
+			t.Skip("SRVKE-1408")
 			if err := monitoringe2e.VerifyMetrics(ctx, monitoringe2e.NamespacedKafkaBrokerDataPlaneQueries(test.Namespace)); err != nil {
 				t.Fatal("Failed to verify that namespaced Kafka Broker data plane metrics work correctly", err)
 			}
