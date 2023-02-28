b88958c138149facfdd256d5f86741cc8c4f594e
Martin Gencur <mgencur@redhat.com>
2023-02-28 12:17:57 +0100

    Run UI tests conditionally

--- test/ui-e2e-tests.sh
+++ test/ui-e2e-tests.sh
@@ -48,7 +48,16 @@ check_node
 archive_cypress_artifacts
 logger.success '🚀 Cluster prepared for testing.'
 
-pushd "$(dirname "${BASH_SOURCE[0]}")/ui" >/dev/null
-npm install
-npm run install
-npm run "${NPM_TARGET}"
+function ui_tests {
+  should_run "${FUNCNAME[0]}" || return
+
+  logger.info "Running UI tests"
+
+  pushd "$(dirname "${BASH_SOURCE[0]}")/ui" >/dev/null
+  npm install
+  npm run install
+  npm run "${NPM_TARGET}"
+}
+
+ui_tests
+
