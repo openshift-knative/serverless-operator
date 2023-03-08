b080ecdfff186ccd899db28912cc627e2179ab00
Martin Gencur <mgencur@redhat.com>
2023-03-08 13:49:24 +0100

    Revert "UI tests support test selection"
    This reverts commit 77a04448fa10d27bc78b0f8d178ba1e50e8d5c40.

--- test/ui-e2e-tests.sh
+++ test/ui-e2e-tests.sh
@@ -29,33 +29,26 @@ function archive_cypress_artifacts {
   popd >/dev/null
 }
 
-function ui_tests {
-  should_run "${FUNCNAME[0]}" || return 0
+OCP_VERSION="$(oc get clusterversion version -o jsonpath='{.status.desired.version}')"
+OCP_USERNAME="${OCP_USERNAME:-uitesting}"
+OCP_PASSWORD="${OCP_PASSWORD:-$(echo "$OCP_USERNAME" | sha1sum - | awk '{print $1}')}"
+OCP_LOGIN_PROVIDER="${OCP_LOGIN_PROVIDER:-my_htpasswd_provider}"
+CYPRESS_BASE_URL="https://$(oc get route console -n openshift-console -o jsonpath='{.status.ingress[].host}')"
+# use dev to run test development UI
+NPM_TARGET="${NPM_TARGET:-test}"
+if [ -n "${BUILD_ID:-}" ]; then
+  export CYPRESS_NUM_TESTS_KEPT_IN_MEMORY=0
+fi
+export OCP_VERSION OCP_USERNAME OCP_PASSWORD OCP_LOGIN_PROVIDER CYPRESS_BASE_URL
 
-  OCP_VERSION="$(oc get clusterversion version -o jsonpath='{.status.desired.version}')"
-  OCP_USERNAME="${OCP_USERNAME:-uitesting}"
-  OCP_PASSWORD="${OCP_PASSWORD:-$(echo "$OCP_USERNAME" | sha1sum - | awk '{print $1}')}"
-  OCP_LOGIN_PROVIDER="${OCP_LOGIN_PROVIDER:-my_htpasswd_provider}"
-  CYPRESS_BASE_URL="https://$(oc get route console -n openshift-console -o jsonpath='{.status.ingress[].host}')"
-  # use dev to run test development UI
-  NPM_TARGET="${NPM_TARGET:-test}"
-  if [ -n "${BUILD_ID:-}" ]; then
-    export CYPRESS_NUM_TESTS_KEPT_IN_MEMORY=0
-  fi
-  export OCP_VERSION OCP_USERNAME OCP_PASSWORD OCP_LOGIN_PROVIDER CYPRESS_BASE_URL
+create_namespaces "${TEST_NAMESPACES[@]}"
+add_user "$OCP_USERNAME" "$OCP_PASSWORD"
+oc adm policy add-role-to-user edit "$OCP_USERNAME" -n "$TEST_NAMESPACE"
+check_node
+archive_cypress_artifacts
+logger.success '🚀 Cluster prepared for testing.'
 
-  create_namespaces "${TEST_NAMESPACES[@]}"
-  add_user "$OCP_USERNAME" "$OCP_PASSWORD"
-  oc adm policy add-role-to-user edit "$OCP_USERNAME" -n "$TEST_NAMESPACE"
-  check_node
-  archive_cypress_artifacts
-  logger.success '🚀 Cluster prepared for testing.'
-
-  pushd "$(dirname "${BASH_SOURCE[0]}")/ui" >/dev/null
-  npm install
-  npm run install
-  npm run "${NPM_TARGET}"
-}
-
-ui_tests
-
+pushd "$(dirname "${BASH_SOURCE[0]}")/ui" >/dev/null
+npm install
+npm run install
+npm run "${NPM_TARGET}"
