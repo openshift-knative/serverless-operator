7d2fef5da73a878c57f66156eae5b6df4a51a3c6
Martin Gencur <mgencur@redhat.com>
2023-03-16 13:55:15 +0100

    testselect clones repo into a temp dir

--- hack/lib/testselect.bash
+++ hack/lib/testselect.bash
@@ -4,15 +4,20 @@ function run_testselect {
   if [[ -n "${ARTIFACT_DIR:-}" ]]; then
     GO111MODULE=off go get github.com/openshift-knative/hack/cmd/testselect
 
+    local clonedir rootdir
+    clonedir=$(mktemp -d)
+
     # CLONEREFS_OPTIONS var is set in CI
     echo "${CLONEREFS_OPTIONS}" > "${ARTIFACT_DIR}/clonerefs.json"
 
     cat "${ARTIFACT_DIR}/clonerefs.json"
 
-    local rootdir
     rootdir="$(dirname "$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")")"
 
+    # The testselect clones a repository. Make sure it's cloned into a temp dir.
+    pushd "$clonedir" || return $?
     "${GOPATH}/bin/testselect" --testsuites="${rootdir}/test/testsuites.yaml" --clonerefs="${ARTIFACT_DIR}/clonerefs.json" --output="${ARTIFACT_DIR}/tests.txt"
+    popd "$clonedir" || return $?
 
     logger.info 'Tests to be run:'
     cat "${ARTIFACT_DIR}/tests.txt"
