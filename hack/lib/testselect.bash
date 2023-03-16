3b9b46f8f286362f0ced1bf22650b647599ee7d7
Martin Gencur <mgencur@redhat.com>
2023-03-16 14:00:17 +0100

    Fix popd

--- hack/lib/testselect.bash
+++ hack/lib/testselect.bash
@@ -15,9 +15,9 @@ function run_testselect {
     rootdir="$(dirname "$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")")"
 
     # The testselect clones a repository. Make sure it's cloned into a temp dir.
-    pushd "$clonedir" || return $?
+    pushd "$clonedir"
     "${GOPATH}/bin/testselect" --testsuites="${rootdir}/test/testsuites.yaml" --clonerefs="${ARTIFACT_DIR}/clonerefs.json" --output="${ARTIFACT_DIR}/tests.txt"
-    popd "$clonedir" || return $?
+    popd
 
     logger.info 'Tests to be run:'
     cat "${ARTIFACT_DIR}/tests.txt"
