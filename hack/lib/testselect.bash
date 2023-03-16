fc1359f958d1d0a81ea48ad6d6e2968ae9dc96d2
Martin Gencur <mgencur@redhat.com>
2023-03-16 11:15:32 +0100

    Use GOPATH for running testselect (#2003)
    Do not rely on PATH having the binary because other repositories might
    set GOPATH to a different directory and then "go get" will install it t
    here. The directory might not be on path.

--- hack/lib/testselect.bash
+++ hack/lib/testselect.bash
@@ -10,7 +10,7 @@ if [[ -n "${ARTIFACT_DIR:-}" ]]; then
 
   rootdir="$(dirname "$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")")"
 
-  testselect --testsuites="${rootdir}/test/testsuites.yaml" --clonerefs="${ARTIFACT_DIR}/clonerefs.json" --output="${ARTIFACT_DIR}/tests.txt"
+  "${GOPATH}/bin/testselect" --testsuites="${rootdir}/test/testsuites.yaml" --clonerefs="${ARTIFACT_DIR}/clonerefs.json" --output="${ARTIFACT_DIR}/tests.txt"
 
   logger.info 'Tests to be run:'
   cat "${ARTIFACT_DIR}/tests.txt"
