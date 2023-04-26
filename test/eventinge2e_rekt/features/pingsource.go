edc7fdc1c50abebbd6519d9221f81b9e9a34ba46
Martin Gencur <mgencur@redhat.com>
2023-04-26 16:33:16 +0200

    Use KReference in test

--- test/eventinge2e_rekt/features/pingsource.go
+++ test/eventinge2e_rekt/features/pingsource.go
@@ -1,17 +1,16 @@
 package features
 
 import (
-	"context"
-
 	"github.com/cloudevents/sdk-go/v2/test"
 	"github.com/openshift-knative/serverless-operator/test/eventinge2e_rekt/resources/brokerconfig"
 	"knative.dev/eventing/test/rekt/resources/broker"
 	"knative.dev/eventing/test/rekt/resources/pingsource"
 	"knative.dev/eventing/test/rekt/resources/trigger"
+	duckv1 "knative.dev/pkg/apis/duck/v1"
 	"knative.dev/reconciler-test/pkg/eventshub"
 	"knative.dev/reconciler-test/pkg/eventshub/assert"
 	"knative.dev/reconciler-test/pkg/feature"
-	"knative.dev/reconciler-test/resources/svc"
+	"knative.dev/reconciler-test/pkg/resources/knativeservice"
 )
 
 func SendsEventsWithSinkRef() *feature.Feature {
@@ -30,20 +29,24 @@ func SendsEventsWithSinkRef() *feature.Feature {
 	f.Requirement("broker is ready", broker.IsReady(br))
 	f.Requirement("broker is addressable", broker.IsAddressable(br))
 
-	f.Setup("install sink", eventshub.Install(sink, eventshub.StartReceiver)) //eventshub.AsKnativeService
+	f.Setup("install sink", eventshub.Install(sink,
+		eventshub.StartReceiver,
+		eventshub.AsKnativeService))
 
 	f.Setup("install trigger", trigger.Install(trig, br,
-		trigger.WithSubscriber(svc.AsKReference(sink), "")))
+		trigger.WithSubscriber(&duckv1.KReference{
+			APIVersion: knativeservice.GVR().GroupVersion().String(),
+			Kind:       "Service", //broker.GVR().Resource
+			Name:       sink,
+		}, "")))
 	f.Setup("trigger goes ready", trigger.IsReady(trig))
 
-	f.Setup("install pingsource", func(ctx context.Context, t feature.T) {
-		// TODO: Couldn't get address ???
-		brokeruri, err := broker.Address(ctx, br)
-		if err != nil {
-			t.Error("Failed to get address of broker", err)
-		}
-		pingsource.Install(source, pingsource.WithSink(nil, brokeruri.String()))(ctx, t)
-	})
+	f.Setup("install pingsource", pingsource.Install(source, pingsource.WithSink(
+		&duckv1.KReference{
+			APIVersion: broker.GVR().GroupVersion().String(),
+			Kind:       "Broker", //broker.GVR().Resource
+			Name:       br,
+		}, "")))
 	f.Requirement("pingsource goes ready", pingsource.IsReady(source))
 
 	f.Stable("pingsource as event source").
