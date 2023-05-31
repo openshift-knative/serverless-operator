0b145d744f2878d4936831ddd34ccd42c0741ed4
Martin Gencur <mgencur@redhat.com>
2023-05-31 09:00:27 +0200

    Verify encrypted traffic for KafkaSource scenario

---
+++ test/extensione2erekt/features/encryption.go
@@ -0,0 +1,45 @@
+package features
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"time"
+
+	eventingfeatures "github.com/openshift-knative/serverless-operator/test/eventinge2erekt/features"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"knative.dev/reconciler-test/pkg/environment"
+	"knative.dev/reconciler-test/pkg/feature"
+)
+
+func VerifyEncryptedTrafficToKafkaSink(sinkName string, since time.Time) *feature.Feature {
+	f := feature.NewFeature()
+
+	f.Stable("path to kafka sink").
+		Must("has encrypted traffic", verifyEncryptedTrafficToKafkaSink(sinkName, since))
+
+	return f
+}
+
+func verifyEncryptedTrafficToKafkaSink(sinkName string, since time.Time) feature.StepFn {
+	return func(ctx context.Context, t feature.T) {
+		// source -> activator
+		// When running within Mesh a mesh-specific VirtualService is used which
+		// gets istio-ingressgateway out of the path.
+		sinkPath := fmt.Sprintf("/%s/%s", environment.FromContext(ctx).Namespace(), sinkName)
+		logFilter := eventingfeatures.LogFilter{
+			PodNamespace:  "knative-eventing",
+			PodSelector:   metav1.ListOptions{LabelSelector: "app=kafka-sink-receiver"},
+			PodLogOptions: &corev1.PodLogOptions{Container: "istio-proxy", SinceTime: &metav1.Time{Time: since}},
+			JSONLogFilter: func(m map[string]interface{}) bool {
+				return eventingfeatures.GetMapValueAsString(m, "path") == sinkPath &&
+					eventingfeatures.GetMapValueAsString(m, "authority") == "kafka-sink-ingress.knative-eventing.svc.cluster.local"
+			}}
+
+		err := eventingfeatures.VerifyPodLogsEncryptedRequestToHost(ctx, logFilter)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
--- test/extensione2erekt/kafkasource_ksvc_test.go
+++ test/extensione2erekt/kafkasource_ksvc_test.go
@@ -2,8 +2,12 @@ package extensione2erekt
 
 import (
 	"testing"
+	"time"
 
+	eventingfeatures "github.com/openshift-knative/serverless-operator/test/eventinge2erekt/features"
+	kafkafeatures "github.com/openshift-knative/serverless-operator/test/extensione2erekt/features"
 	"knative.dev/eventing-kafka-broker/test/rekt/features"
+	"knative.dev/reconciler-test/pkg/environment"
 	"knative.dev/reconciler-test/pkg/feature"
 )
 
@@ -25,7 +29,14 @@ func TestKafkaSourceTLS(t *testing.T) {
 	kafkaSink := feature.MakeRandomK8sName("kafkaSink")
 	topic := feature.MakeRandomK8sName("topic")
 
+	since := time.Now()
+
 	env.Test(ctx, t, features.KafkaSourceTLS(kafkaSource, kafkaSink, topic))
+
+	if ic := environment.GetIstioConfig(ctx); ic.Enabled {
+		env.Test(ctx, t, eventingfeatures.VerifyEncryptedTrafficToActivatorToApp(env.References(), since))
+		env.Test(ctx, t, kafkafeatures.VerifyEncryptedTrafficToKafkaSink(kafkaSink, since))
+	}
 }
 
 func TestKafkaSourceSASL(t *testing.T) {
