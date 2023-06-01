303606bd29f2087d18d35a9a5d83902b437fd962
Martin Gencur <mgencur@redhat.com>
2023-06-01 06:09:31 +0200

    Disable channel-based test for now with Istio

--- test/extensione2erekt/containersource_kafkachannel_ksvc_test.go
+++ test/extensione2erekt/containersource_kafkachannel_ksvc_test.go
@@ -6,6 +6,7 @@ import (
 	"knative.dev/eventing/test/rekt/features/channel"
 	"knative.dev/eventing/test/rekt/resources/subscription"
 	duckv1 "knative.dev/pkg/apis/duck/v1"
+	"knative.dev/reconciler-test/pkg/environment"
 	"knative.dev/reconciler-test/pkg/manifest"
 )
 
@@ -14,6 +15,10 @@ func TestContainerSourceKafkaChannelKsvc(t *testing.T) {
 	t.Parallel()
 
 	ctx, env := defaultEnvironment(t)
+
+	if ic := environment.GetIstioConfig(ctx); ic.Enabled {
+		t.Skip("Channel-based tests cannot run in service mesh mode for now")
+	}
 
 	createSubscriberFn := func(ref *duckv1.KReference, uri string) manifest.CfgFn {
 		return subscription.WithSubscriber(ref, uri)
