fda81c94a67d60d3f60f118a1b0360b2f915a51c
Martin Gencur <mgencur@redhat.com>
2023-06-12 11:10:51 +0200

    Const for hold application

--- serving/metadata-webhook/pkg/defaults/ksvc_defaults.go
+++ serving/metadata-webhook/pkg/defaults/ksvc_defaults.go
@@ -10,9 +10,10 @@ import (
 const (
 	openshiftPassthrough = "serving.knative.openshift.io/enablePassthrough"
 
-	sidecarInject                = "sidecar.istio.io/inject"
-	sidecarrewriteAppHTTPProbers = "sidecar.istio.io/rewriteAppHTTPProbers"
-	proxyIstioConfig             = "proxy.istio.io/config"
+	sidecarInject                   = "sidecar.istio.io/inject"
+	sidecarrewriteAppHTTPProbers    = "sidecar.istio.io/rewriteAppHTTPProbers"
+	proxyIstioConfig                = "proxy.istio.io/config"
+	holdApplicationUntilProxyStarts = `{ "holdApplicationUntilProxyStarts": true }`
 
 	maistraProxyEnv          = "sidecar.maistra.io/proxyEnv"
 	terminationDrainDuration = `{ "TERMINATION_DRAIN_DURATION_SECONDS": "20" }`
@@ -46,7 +47,7 @@ func (r *TargetKService) SetDefaults(ctx context.Context) {
 	r.Spec.Template.Annotations[sidecarInject] = "true"
 	r.Spec.Template.Annotations[sidecarrewriteAppHTTPProbers] = "true"
 	r.Spec.Template.Annotations[maistraProxyEnv] = terminationDrainDuration
-	r.Spec.Template.Annotations[proxyIstioConfig] = `{ "holdApplicationUntilProxyStarts": true }`
+	r.Spec.Template.Annotations[proxyIstioConfig] = holdApplicationUntilProxyStarts
 }
 
 // Validate returns nil due to no need for validation
