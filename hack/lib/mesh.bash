388cca87ad5ddce888c240b65ba95155fea3fc9d
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2023-06-15 11:07:24 +0900

    Drop apply

--- hack/lib/mesh.bash
+++ hack/lib/mesh.bash
@@ -118,7 +118,7 @@ function deploy_gateways {
 
   oc apply -f "${resources_dir}"/namespace.yaml || return $?
   oc apply -f "${resources_dir}"/gateway.yaml || return $?
-  oc apply -f "${resources_dir}"/peerauthentication.yaml || return $?
+#  oc apply -f "${resources_dir}"/peerauthentication.yaml || return $?
 
   oc create ns "${EVENTING_NAMESPACE}" --dry-run=client -oyaml | kubectl apply -f -
   oc apply -n "${EVENTING_NAMESPACE}" -f "${resources_dir}"/kafka-service-entry.yaml || return $?
@@ -129,7 +129,7 @@ function deploy_gateways {
 }
 
 function undeploy_gateways {
-  oc delete -f "${resources_dir}"/peerauthentication.yaml --ignore-not-found || return $?
+#  oc delete -f "${resources_dir}"/peerauthentication.yaml --ignore-not-found || return $?
   oc delete -f "${resources_dir}"/gateway.yaml --ignore-not-found || return $?
   oc delete -f "${resources_dir}"/smmr.yaml --ignore-not-found || return $?
   oc delete -n cert-manager secret ca-key-pair  --ignore-not-found || return $?
