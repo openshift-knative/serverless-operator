f690bd7b191d089888b7a0e22d9348e7cd144060
Pierangelo Di Pilato <pierdipi@redhat.com>
2023-07-26 12:34:59 +0200

    Introduce default environment for eventinge2erekt tests (#2195)
    Co-authored-by: Martin Gencur <mgencur@redhat.com>

--- test/eventinge2erekt/main_test.go
+++ test/eventinge2erekt/main_test.go
@@ -1,11 +1,17 @@
 package eventinge2erekt
 
 import (
+	"context"
 	"os"
 	"testing"
+	"time"
 
 	"knative.dev/eventing/test/rekt/resources/broker"
+	"knative.dev/pkg/system"
 	"knative.dev/reconciler-test/pkg/environment"
+	"knative.dev/reconciler-test/pkg/eventshub"
+	"knative.dev/reconciler-test/pkg/k8s"
+	"knative.dev/reconciler-test/pkg/knative"
 )
 
 var global environment.GlobalEnvironment
@@ -18,4 +24,17 @@ func TestMain(m *testing.M) {
 
 	// Run the tests.
 	os.Exit(m.Run())
+}
+
+func defaultEnvironment(t *testing.T) (context.Context, environment.Environment) {
+	return global.Environment(
+		knative.WithKnativeNamespace(system.Namespace()),
+		knative.WithLoggingConfig,
+		knative.WithTracingConfig,
+		k8s.WithEventListener,
+		// Enables KnativeService in the scenario.
+		eventshub.WithKnativeServiceForwarder,
+		environment.WithPollTimings(5*time.Second, 4*time.Minute),
+		environment.Managed(t),
+	)
 }
