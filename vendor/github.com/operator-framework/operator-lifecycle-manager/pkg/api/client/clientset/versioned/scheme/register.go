131171b9361438f3c0190409c21ae9c8a02b9354
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2023-08-02 17:05:47 +0900

    Bump github.com/operator-framework/operator-lifecycle-manager (#2193)
    * Bump github.com/operator-framework/operator-lifecycle-manager
    
    Bumps [github.com/operator-framework/operator-lifecycle-manager](https://github.com/operator-framework/operator-lifecycle-manager) from 0.17.1-0.20210607005641-f05ea078ab46 to 3.11.0+incompatible.
    - [Release notes](https://github.com/operator-framework/operator-lifecycle-manager/releases)
    - [Changelog](https://github.com/operator-framework/operator-lifecycle-manager/blob/master/.goreleaser.yml)
    - [Commits](https://github.com/operator-framework/operator-lifecycle-manager/commits)
    
    ---
    updated-dependencies:
    - dependency-name: github.com/operator-framework/operator-lifecycle-manager
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Run ./hack/update-deps.sh
    
    * Bump github.com/operator-framework/operator-lifecycle-manager
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- vendor/github.com/operator-framework/operator-lifecycle-manager/pkg/api/client/clientset/versioned/scheme/register.go
+++ vendor/github.com/operator-framework/operator-lifecycle-manager/pkg/api/client/clientset/versioned/scheme/register.go
@@ -22,6 +22,7 @@ import (
 	operatorsv1 "github.com/operator-framework/api/pkg/operators/v1"
 	operatorsv1alpha1 "github.com/operator-framework/api/pkg/operators/v1alpha1"
 	operatorsv1alpha2 "github.com/operator-framework/api/pkg/operators/v1alpha2"
+	operatorsv2 "github.com/operator-framework/api/pkg/operators/v2"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 	schema "k8s.io/apimachinery/pkg/runtime/schema"
@@ -36,19 +37,20 @@ var localSchemeBuilder = runtime.SchemeBuilder{
 	operatorsv1alpha1.AddToScheme,
 	operatorsv1alpha2.AddToScheme,
 	operatorsv1.AddToScheme,
+	operatorsv2.AddToScheme,
 }
 
 // AddToScheme adds all types of this clientset into the given scheme. This allows composition
 // of clientsets, like in:
 //
-//   import (
-//     "k8s.io/client-go/kubernetes"
-//     clientsetscheme "k8s.io/client-go/kubernetes/scheme"
-//     aggregatorclientsetscheme "k8s.io/kube-aggregator/pkg/client/clientset_generated/clientset/scheme"
-//   )
+//	import (
+//	  "k8s.io/client-go/kubernetes"
+//	  clientsetscheme "k8s.io/client-go/kubernetes/scheme"
+//	  aggregatorclientsetscheme "k8s.io/kube-aggregator/pkg/client/clientset_generated/clientset/scheme"
+//	)
 //
-//   kclientset, _ := kubernetes.NewForConfig(c)
-//   _ = aggregatorclientsetscheme.AddToScheme(clientsetscheme.Scheme)
+//	kclientset, _ := kubernetes.NewForConfig(c)
+//	_ = aggregatorclientsetscheme.AddToScheme(clientsetscheme.Scheme)
 //
 // After this, RawExtensions in Kubernetes types will serialize kube-aggregator types
 // correctly.
