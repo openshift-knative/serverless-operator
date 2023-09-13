375304b1e2fef2caf8c7461cd4c0f975218638d1
Martin Gencur <mgencur@redhat.com>
2023-09-13 14:01:40 +0200

    Do not compare memory consumption
    It's not reliable through this metric

--- test/upgrade/kitchensink/kitchensink_test.go
+++ test/upgrade/kitchensink/kitchensink_test.go
@@ -21,7 +21,6 @@ package kitchensink
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"math/rand"
 	"os"
@@ -33,12 +32,7 @@ import (
 	"github.com/openshift-knative/serverless-operator/test/kitchensinke2e/features"
 	"github.com/openshift-knative/serverless-operator/test/upgrade"
 	"github.com/openshift-knative/serverless-operator/test/upgrade/installation"
-	"github.com/prometheus/common/model"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"knative.dev/pkg/client/injection/kube/client"
-	"knative.dev/pkg/injection/clients/dynamicclient"
-	"knative.dev/pkg/logging"
-	logtesting "knative.dev/pkg/logging/testing"
 	_ "knative.dev/pkg/system/testing"
 	pkgupgrade "knative.dev/pkg/test/upgrade"
 	"knative.dev/reconciler-test/pkg/environment"
@@ -196,7 +190,6 @@ func TestUpgradeStress(t *testing.T) {
 		}
 	}
 
-	systemPodsMemory := make(map[string]float64)
 	suite := pkgupgrade.Suite{
 		Tests: pkgupgrade.Tests{
 			PreUpgrade: featureGroup.PreUpgradeTests(),
@@ -214,15 +207,11 @@ func TestUpgradeStress(t *testing.T) {
 			PostDowngrade: featureGroup.PostDowngradeTests(),
 		},
 		Installations: pkgupgrade.Installations{
-			UpgradeWith: append([]pkgupgrade.Operation{
-				// Ensure memory usage is recorded after PreUpgrade tests as those can use t.Parallel.
-				RecordMemoryUsage(ctx, systemPodsMemory),
-			}, upgrade.ServerlessUpgradeOperations(ctx)...),
+			UpgradeWith: upgrade.ServerlessUpgradeOperations(ctx),
 			DowngradeWith: []pkgupgrade.Operation{
 				// Skip actual downgrade but run additional checks here. They are ensured to
 				// run after PostUpgrade tests.
 				VerifyPodRestarts(ctx),
-				VerifyMemoryUsage(ctx, systemPodsMemory),
 			},
 		},
 	}
@@ -258,69 +247,4 @@ func VerifyPodRestarts(ctx *test.Context) pkgupgrade.Operation {
 			c.T.Fatalf("Container restart detected for Pods: %v", podsRestarted)
 		}
 	})
-}
-
-func RecordMemoryUsage(ctx *test.Context, systemPodsMemory map[string]float64) pkgupgrade.Operation {
-	return pkgupgrade.NewOperation("RecordMemoryUsage", func(c pkgupgrade.Context) {
-		recordMemoryUsage(c.T, ctx, systemPodsMemory)
-	})
-}
-
-func VerifyMemoryUsage(ctx *test.Context, systemPodsMemory map[string]float64) pkgupgrade.Operation {
-	return pkgupgrade.NewOperation("VerifyMemoryUsage", func(c pkgupgrade.Context) {
-
-		newSystemPodsMemory := make(map[string]float64)
-
-		recordMemoryUsage(c.T, ctx, newSystemPodsMemory)
-
-		for pod, mem := range newSystemPodsMemory {
-			origMem, ok := systemPodsMemory[pod]
-			if !ok {
-				continue
-			}
-			if mem > (origMem * memoryIncreaseTolerance) {
-				c.T.Errorf("Memory consumption for %s higher than (%.1f * original). Original: %.1f, new: %.1f ",
-					pod, memoryIncreaseTolerance, origMem, mem)
-			}
-		}
-	})
-}
-
-func recordMemoryUsage(t *testing.T, ctx *test.Context, systemPodsMemory map[string]float64) {
-	// Give time to settle.
-	time.Sleep(30 * time.Second)
-
-	prometheusCtx := context.WithValue(context.Background(), client.Key{}, ctx.Clients.Kube)
-	prometheusCtx = context.WithValue(prometheusCtx, dynamicclient.Key{}, ctx.Clients.Dynamic)
-	prometheusCtx = logging.WithLogger(prometheusCtx, logtesting.TestLogger(t))
-	prometheus, err := test.NewPrometheusClient(prometheusCtx)
-	if err != nil {
-		t.Fatalf("Unable to get Prometheus client: %v", err)
-	}
-
-	namespaces := []string{test.ServingNamespace,
-		test.EventingNamespace, test.IngressNamespace, test.OperatorsNamespace}
-
-	for _, ns := range namespaces {
-		value, warnings, err := prometheus.Query(context.Background(),
-			fmt.Sprintf(memoryWorkingSetQuery, ns),
-			time.Now())
-		if err != nil {
-			t.Fatalf("Unable to query metrics: %v", err)
-		}
-		for _, w := range warnings {
-			t.Logf("Prometheus warning: %v", w)
-		}
-
-		vector := value.(model.Vector)
-		for _, sample := range vector {
-			pod := string(sample.Metric["pod"])
-			split := strings.Split(pod, "-")
-			// Remove the last two parts split by dash, containing random chars.
-			// Example: imc-controller-644bd94dff-7nhkx -> imc-controller
-			component := ns + "/" + strings.Join(split[0:len(split)-2], "-")
-			// There might be more Pods for each component due to HA. Compute the sum of values.
-			systemPodsMemory[component] = systemPodsMemory[component] + float64(sample.Value)
-		}
-	}
 }
