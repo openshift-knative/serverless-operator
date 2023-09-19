5c80a03269663398f1b836332da6db06843dcedf
Martin Gencur <mgencur@redhat.com>
2023-09-19 14:53:55 +0200

    Test with Reply and DLS - not working

--- test/extensione2erekt/servicemesh/multitenant_test.go
+++ test/extensione2erekt/servicemesh/multitenant_test.go
@@ -3,6 +3,7 @@ package servicemesh
 import (
 	"context"
 	"testing"
+	"time"
 
 	cetest "github.com/cloudevents/sdk-go/v2/test"
 	"github.com/google/uuid"
@@ -11,6 +12,7 @@ import (
 	"knative.dev/eventing-kafka-broker/control-plane/pkg/kafka"
 	"knative.dev/eventing-kafka-broker/test/rekt/resources/configmap"
 	duckv1 "knative.dev/eventing/pkg/apis/duck/v1"
+	"knative.dev/eventing/test/rekt/features"
 	"knative.dev/eventing/test/rekt/resources/broker"
 	"knative.dev/eventing/test/rekt/resources/channel_impl"
 	"knative.dev/eventing/test/rekt/resources/containersource"
@@ -22,6 +24,7 @@ import (
 	"knative.dev/reconciler-test/pkg/eventshub/assert"
 	"knative.dev/reconciler-test/pkg/feature"
 	"knative.dev/reconciler-test/pkg/k8s"
+	"knative.dev/reconciler-test/pkg/knative"
 	"knative.dev/reconciler-test/pkg/manifest"
 	"knative.dev/reconciler-test/pkg/resources/service"
 )
@@ -107,6 +110,107 @@ func verifyContainerSourceToChannelBlocked(channel, sink string, channelCtx cont
 	//	"start_time": "2023-09-19T07:43:17.249Z", "upstream_cluster": "inbound|8080||", "upstream_host": "-",
 	//	"upstream_local_address": "-", "upstream_service_time": -, "upstream_transport_failure_reason": "-",
 	//	"user_agent": "Go-http-client/1.1", "x_forwarded_for": "-" }
+
+	return f
+}
+
+// ContainerSource -> KafkaChannel -> Subscription -> Sink (tenant-1)
+//
+//	-> with reply to Sink (tenant-2)
+//	-> with deadLetterSink to Sink (tenant-1)
+//
+// The sink in "reply" should not receive any event because it's in a different tenant.
+// The original sink and dead letter sink should receive an event.
+func TestContainerSourceKafkaChannelKsvcWithReplyAndDLSCrossTenant(t *testing.T) {
+	t.Parallel()
+
+	// Do not use Knative Service Forwarder as it doesn't work correctly with replies.
+	ctxTenant1, envTenant1 := global.Environment(
+		environment.WithNamespace("tenant-1"),
+		knative.WithKnativeNamespace(system.Namespace()),
+		knative.WithLoggingConfig,
+		knative.WithTracingConfig,
+		k8s.WithEventListener,
+		environment.WithPollTimings(5*time.Second, 4*time.Minute),
+		environment.Managed(t),
+	)
+	ctxTenant2, envTenant2 := global.Environment(
+		environment.WithNamespace("tenant-2"),
+		knative.WithKnativeNamespace(system.Namespace()),
+		knative.WithLoggingConfig,
+		knative.WithTracingConfig,
+		k8s.WithEventListener,
+		environment.WithPollTimings(5*time.Second, 4*time.Minute),
+		environment.Managed(t),
+	)
+
+	replySink := feature.MakeRandomK8sName("sink")
+
+	// Deploy reply-sink in tenant-1.
+	envTenant1.Test(ctxTenant1, t, replySinkKsvc(replySink))
+	// Check cross-tenant event.
+	envTenant2.Test(ctxTenant2, t, verifyContainerSourceToChannelWithReplyAndDLS(replySink, ctxTenant1))
+}
+
+func replySinkKsvc(sink string) *feature.Feature {
+	f := feature.NewFeature()
+
+	f.Setup("install sink", eventshub.Install(sink, eventshub.StartReceiver))
+
+	return f
+}
+
+func verifyContainerSourceToChannelWithReplyAndDLS(replySink string, replySinkCtx context.Context) *feature.Feature {
+	f := feature.NewFeature()
+
+	channel := feature.MakeRandomK8sName("channel")
+	sink := feature.MakeRandomK8sName("sink")
+	dls := feature.MakeRandomK8sName("dls")
+	cs := feature.MakeRandomK8sName("containersource")
+
+	f.Setup("install channel", channel_impl.Install(channel))
+	f.Setup("channel is ready", channel_impl.IsReady(channel))
+
+	f.Setup("install sink", eventshub.Install(sink, eventshub.StartReceiver))
+	f.Setup("install dls", eventshub.Install(dls, eventshub.StartReceiver))
+
+	replySinkRef := service.AsKReference(replySink)
+	replySinkRef.Namespace = environment.FromContext(replySinkCtx).Namespace()
+
+	sub := feature.MakeRandomK8sName("subscription")
+	f.Setup("install sink subscription", subscription.Install(sub,
+		subscription.WithChannel(channel_impl.AsRef(channel)),
+		subscription.WithSubscriber(service.AsKReference(sink), ""),
+		subscription.WithReply(replySinkRef, ""),
+		subscription.WithDeadLetterSink(service.AsKReference(dls), ""),
+	))
+
+	f.Setup("subscription is ready", subscription.IsReady(sub))
+
+	f.Requirement("install containersource", containersource.Install(cs,
+		containersource.WithSink(channel_impl.AsRef(channel), "")))
+	f.Requirement("containersource goes ready", containersource.IsReady(cs))
+
+	f.Assert("sink receives events", func(ctx context.Context, t feature.T) {
+		assert.OnStore(sink).
+			Match(features.HasKnNamespaceHeader(environment.FromContext(ctx).Namespace())).
+			MatchEvent(cetest.HasType("dev.knative.eventing.samples.heartbeat")).
+			AtLeast(1)(ctx, t)
+	})
+
+	f.Assert("deadlettersink receives events", func(ctx context.Context, t feature.T) {
+		assert.OnStore(dls).
+			Match(features.HasKnNamespaceHeader(environment.FromContext(ctx).Namespace())).
+			MatchEvent(cetest.HasType("dev.knative.eventing.samples.heartbeat")).
+			AtLeast(1)(ctx, t)
+	})
+
+	f.Assert("reply sink does not receive events", func(ctx context.Context, t feature.T) {
+		assert.OnStore(replySink).
+			Match(features.HasKnNamespaceHeader(environment.FromContext(ctx).Namespace())).
+			MatchEvent(cetest.HasType("dev.knative.eventing.samples.heartbeat")).
+			Not()(replySinkCtx, t)
+	})
 
 	return f
 }
