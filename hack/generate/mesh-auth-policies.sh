1c974f63ba7da9933aba9fc08e27f15f5011db27
Reto Lehmann <retocode@icloud.com>
2023-10-05 14:58:08 +0200

    Get helm chart from Github instead of OCI

--- hack/generate/mesh-auth-policies.sh
+++ hack/generate/mesh-auth-policies.sh
@@ -18,10 +18,15 @@ helm > /dev/null || exit 127
 source "$(dirname "${BASH_SOURCE[0]}")/../lib/metadata.bash"
 
 policies_path="$(dirname "${BASH_SOURCE[0]}")/../lib/mesh_resources/authorization-policies/helm"
-if [ -z "${ISTIO_CHART_VERSION:-}" ]; then
-  chart_version="$(metadata.get project.version)"
-else
-  chart_version="${ISTIO_CHART_VERSION}"
+chart_version="$(metadata.get project.version | grep -Eo '[0-9]+\.[0-9]+')" # grep removes the patch version in semver
+
+# Pull image template from Github
+template_cache=$(mktemp -d)
+
+if ! git clone -b "release-${chart_version}" --depth 1 https://github.com/openshift-knative/knative-istio-authz-chart.git "$template_cache"; then
+   # branch might not yet be there, then we fallback to using `main`
+   echo "Failed to clone knative-istio-authz-chart with branch release-${chart_version}. Falling back to using main."
+   git clone --depth 1 https://github.com/openshift-knative/knative-istio-authz-chart.git "$template_cache"
 fi
 
 echo "Cleaning up old resources in $policies_path"
@@ -32,7 +37,7 @@ mkdir -p "$policies_path"
 for tenant in ${tenants//,/ }; do
   echo "Generating AuthorizationPolicies for tenant $tenant"
 
-  helm template oci://quay.io/openshift-knative/knative-istio-authz-onboarding --version "$chart_version" --set "name=$tenant" --set "namespaces={$tenant}" > "$policies_path/$tenant.yaml"
+  helm template "$template_cache" --version "$chart_version" --set "name=$tenant" --set "namespaces={$tenant}" > "$policies_path/$tenant.yaml"
 done
 
 echo "Istio AuthorizationPolicies successfully updated for version $chart_version"
