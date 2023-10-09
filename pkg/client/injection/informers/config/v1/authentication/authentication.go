29a2d95d279e189eb89f980fd432105fc621f513
Stavros Kontopoulos <st.kontopoulos@gmail.com>
2023-10-09 15:11:37 +0300

    make generated-files

--- pkg/client/injection/informers/config/v1/authentication/authentication.go
+++ pkg/client/injection/informers/config/v1/authentication/authentication.go
@@ -5,15 +5,8 @@ package authentication
 import (
 	context "context"
 
-	versioned "github.com/openshift-knative/serverless-operator/pkg/client/clientset/versioned"
 	v1 "github.com/openshift-knative/serverless-operator/pkg/client/informers/externalversions/config/v1"
-	client "github.com/openshift-knative/serverless-operator/pkg/client/injection/client"
 	factory "github.com/openshift-knative/serverless-operator/pkg/client/injection/informers/factory"
-	configv1 "github.com/openshift-knative/serverless-operator/pkg/client/listers/config/v1"
-	apiconfigv1 "github.com/openshift/api/config/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	labels "k8s.io/apimachinery/pkg/labels"
-	cache "k8s.io/client-go/tools/cache"
 	controller "knative.dev/pkg/controller"
 	injection "knative.dev/pkg/injection"
 	logging "knative.dev/pkg/logging"
@@ -21,7 +14,6 @@ import (
 
 func init() {
 	injection.Default.RegisterInformer(withInformer)
-	injection.Dynamic.RegisterDynamicInformer(withDynamicInformer)
 }
 
 // Key is used for associating the Informer inside the context.Context.
@@ -33,11 +25,6 @@ func withInformer(ctx context.Context) (context.Context, controller.Informer) {
 	return context.WithValue(ctx, Key{}, inf), inf.Informer()
 }
 
-func withDynamicInformer(ctx context.Context) context.Context {
-	inf := &wrapper{client: client.Get(ctx), resourceVersion: injection.GetResourceVersion(ctx)}
-	return context.WithValue(ctx, Key{}, inf)
-}
-
 // Get extracts the typed informer from the context.
 func Get(ctx context.Context) v1.AuthenticationInformer {
 	untyped := ctx.Value(Key{})
@@ -46,49 +33,4 @@ func Get(ctx context.Context) v1.AuthenticationInformer {
 			"Unable to fetch github.com/openshift-knative/serverless-operator/pkg/client/informers/externalversions/config/v1.AuthenticationInformer from context.")
 	}
 	return untyped.(v1.AuthenticationInformer)
-}
-
-type wrapper struct {
-	client versioned.Interface
-
-	resourceVersion string
-}
-
-var _ v1.AuthenticationInformer = (*wrapper)(nil)
-var _ configv1.AuthenticationLister = (*wrapper)(nil)
-
-func (w *wrapper) Informer() cache.SharedIndexInformer {
-	return cache.NewSharedIndexInformer(nil, &apiconfigv1.Authentication{}, 0, nil)
-}
-
-func (w *wrapper) Lister() configv1.AuthenticationLister {
-	return w
-}
-
-// SetResourceVersion allows consumers to adjust the minimum resourceVersion
-// used by the underlying client.  It is not accessible via the standard
-// lister interface, but can be accessed through a user-defined interface and
-// an implementation check e.g. rvs, ok := foo.(ResourceVersionSetter)
-func (w *wrapper) SetResourceVersion(resourceVersion string) {
-	w.resourceVersion = resourceVersion
-}
-
-func (w *wrapper) List(selector labels.Selector) (ret []*apiconfigv1.Authentication, err error) {
-	lo, err := w.client.ConfigV1().Authentications().List(context.TODO(), metav1.ListOptions{
-		LabelSelector:   selector.String(),
-		ResourceVersion: w.resourceVersion,
-	})
-	if err != nil {
-		return nil, err
-	}
-	for idx := range lo.Items {
-		ret = append(ret, &lo.Items[idx])
-	}
-	return ret, nil
-}
-
-func (w *wrapper) Get(name string) (*apiconfigv1.Authentication, error) {
-	return w.client.ConfigV1().Authentications().Get(context.TODO(), name, metav1.GetOptions{
-		ResourceVersion: w.resourceVersion,
-	})
 }
