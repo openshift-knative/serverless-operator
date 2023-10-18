98048416b02a5645bde6525846853ad7bd8ccb70
Pierangelo Di Pilato <pierdipi@redhat.com>
2023-10-18 12:28:05 +0200

    SKIP_GENERATE_RELEASE in EKB tests
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- test/eventing-kafka.bash
+++ test/eventing-kafka.bash
@@ -30,6 +30,8 @@ function upstream_knative_eventing_kafka_broker_e2e {
 
   export TEST_IMAGE_TEMPLATE="registry.ci.openshift.org/openshift/knative-eventing-kafka-broker-test-{{.Name}}:${KNATIVE_EVENTING_KAFKA_BROKER_VERSION}"
 
+  export SKIP_GENERATE_RELEASE=true
+
   # || true -> Suppress errors due to readonly or undefined variables
   # shellcheck disable=SC1091,SC1090
   source "${KNATIVE_EVENTING_KAFKA_BROKER_HOME}/openshift/e2e-common.sh" || true
