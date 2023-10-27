59065fe3e8439d533818324748d42d0c5dbc332e
Martin Gencur <mgencur@redhat.com>
2023-10-27 15:44:08 +0200

    Reduce running tests to Eventing REKT

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -24,29 +24,29 @@ logger.success '🚀 Cluster prepared for testing.'
 # Run serverless-operator specific tests.
 create_namespaces "${TEST_NAMESPACES[@]}"
 link_global_pullsecret_to_namespaces "${TEST_NAMESPACES[@]}"
-if [[ "$USER_MANAGEMENT_ALLOWED" == "true" ]]; then
-  create_htpasswd_users && add_roles
-fi
+#if [[ "$USER_MANAGEMENT_ALLOWED" == "true" ]]; then
+#  create_htpasswd_users && add_roles
+#fi
 
 run_testselect
-
-serverless_operator_e2e_tests
-if [[ $TEST_KNATIVE_KAFKA == true ]]; then
-  serverless_operator_kafka_e2e_tests
-fi
+#
+#serverless_operator_e2e_tests
+#if [[ $TEST_KNATIVE_KAFKA == true ]]; then
+#  serverless_operator_kafka_e2e_tests
+#fi
 
 [ -n "$OPENSHIFT_CI" ] && setup_quick_api_deprecation_alerts
 
 # Run Knative Serving & Eventing downstream E2E tests.
-downstream_serving_e2e_tests
-downstream_eventing_e2e_tests
+#downstream_serving_e2e_tests
+#downstream_eventing_e2e_tests
 downstream_eventing_e2e_rekt_tests
-downstream_monitoring_e2e_tests
-if [[ $TEST_KNATIVE_KAFKA == true ]]; then
-  downstream_knative_kafka_e2e_tests
-  downstream_knative_kafka_e2e_rekt_tests
-fi
-
-[ -n "$OPENSHIFT_CI" ] && check_serverless_alerts
-
-success
+#downstream_monitoring_e2e_tests
+#if [[ $TEST_KNATIVE_KAFKA == true ]]; then
+#  downstream_knative_kafka_e2e_tests
+#  downstream_knative_kafka_e2e_rekt_tests
+#fi
+#
+#[ -n "$OPENSHIFT_CI" ] && check_serverless_alerts
+#
+#success
