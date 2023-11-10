f523358a421f3a7a0890c38fdce00963820d6498
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2023-11-10 11:19:00 +0900

    [SRVKS-1154] Do not configure default domain if anything is set (#2305)
    * Use ConfigureIfUnset for config-domain
    
    * Fix
    
    * Fix
    
    * Fix
    
    * update
    
    * Add unit test
    
    * Add multiple entries test
    
    * Fix review comments

--- openshift-knative-operator/pkg/common/config.go
+++ openshift-knative-operator/pkg/common/config.go
@@ -34,3 +34,22 @@ func ConfigureIfUnset(s *base.CommonSpec, cm, key, value string) {
 	}
 	s.Config[cm][key] = value
 }
+
+// ConfigureIfConfigmapUnset sets a value in the given ConifgMap if any configuration is not already set.
+// For example, the config-domain can take an arbitrary domain as a key, so it should be used here.
+func ConfigureIfConfigmapUnset(s *base.CommonSpec, cm, key, value string) {
+	if s.Config == nil {
+		s.Config = make(map[string]map[string]string, 1)
+	}
+
+	if s.Config[cm] == nil {
+		s.Config[cm] = make(map[string]string, 1)
+	}
+
+	if len(s.Config[cm]) != 0 {
+		// Already set, nothing to do here.
+		return
+	}
+
+	s.Config[cm][key] = value
+}
--- openshift-knative-operator/pkg/common/config_test.go
+++ openshift-knative-operator/pkg/common/config_test.go
@@ -74,3 +74,83 @@ func TestConfigure(t *testing.T) {
 		})
 	}
 }
+
+func TestConfigureIfConfigmapUnset(t *testing.T) {
+	cases := []struct {
+		name     string
+		in       base.ConfigMapData
+		expected base.ConfigMapData
+	}{{
+		name: "all nil",
+		expected: base.ConfigMapData{
+			"foo": map[string]string{
+				"bar": "baz",
+			},
+		},
+	}, {
+		name: "first level already set but empty configuration",
+		in: base.ConfigMapData{
+			"foo": map[string]string{},
+		},
+		expected: base.ConfigMapData{
+			"foo": map[string]string{
+				"bar": "baz",
+			},
+		},
+	}, {
+		name: "override",
+		in: base.ConfigMapData{
+			"foo": map[string]string{
+				"bar": "nope",
+			},
+		},
+		expected: base.ConfigMapData{
+			"foo": map[string]string{
+				"bar": "nope",
+			},
+		},
+	}, {
+		name: "override multiple entries",
+		in: base.ConfigMapData{
+			"foo": map[string]string{
+				"bar": "nope",
+				"qux": "baz",
+			},
+		},
+		expected: base.ConfigMapData{
+			"foo": map[string]string{
+				"bar": "nope",
+				"qux": "baz",
+			},
+		},
+	}, {
+		name: "unrelated values",
+		in: base.ConfigMapData{
+			"foo": map[string]string{
+				"bar2": "baz2",
+			},
+			"foo2": map[string]string{
+				"bar": "baz",
+			},
+		},
+		expected: base.ConfigMapData{
+			"foo": map[string]string{
+				"bar2": "baz2",
+			},
+			"foo2": map[string]string{
+				"bar": "baz",
+			},
+		},
+	}}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			s := &base.CommonSpec{Config: c.in}
+			ConfigureIfConfigmapUnset(s, "foo", "bar", "baz")
+
+			if !cmp.Equal(s.Config, c.expected) {
+				t.Errorf("Got = %v, want: %v, diff:\n%s", s.Config, c.expected, cmp.Diff(s.Config, c.expected))
+			}
+		})
+	}
+}
