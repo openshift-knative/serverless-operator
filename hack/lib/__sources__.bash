fc85d17f33574204a2cc666df5204c9d204685dd
David Simansky <dsimansk@redhat.com>
2023-11-15 11:45:35 +0100

    Inject new yq

--- hack/lib/__sources__.bash
+++ hack/lib/__sources__.bash
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# Temp replace until build-image is updated
+if [ -n "$OPENSHIFT_CI" ]; then
+  export YQ_TEMP
+  YQ_TEMP=$(mktemp -d)
+  export PATH="$YQ_TEMP:$PATH:$YQ_TEMP"
+
+  wget https://github.com/mikefarah/yq/releases/download/v4.35.2/yq_linux_amd64 -O "$YQ_TEMP/yq"
+  chmod +x "$YQ_TEMP/yq"
+
+  yq --version
+fi
+
 declare -a __sources=(metadata vars common ui scaleup namespaces catalogsource serverless tracing mesh certmanager strimzi keda tracing clusterlogging testselect)
 
 for source in "${__sources[@]}"; do
