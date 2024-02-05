39b04d7b81a126c258a3365e98ffa926c743992c
Pierangelo Di Pilato <pierdipi@redhat.com>
2024-02-05 11:52:24 +0100

    Improve wait certmanager ready
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- hack/lib/certmanager.bash
+++ hack/lib/certmanager.bash
@@ -32,8 +32,11 @@ function deploy_certmanager_operator {
   fi
 
   logger.info "Waiting until cert manager operator is available"
+
   timeout 600 "[[ \$(oc get deploy -n ${deployment_namespace} cert-manager --no-headers | wc -l) != 1 ]]" || return 1
   timeout 600 "[[ \$(oc get deploy -n ${deployment_namespace} cert-manager-webhook --no-headers | wc -l) != 1 ]]" || return 1
+  oc wait deployments -n ${deployment_namespace} cert-manager-webhook --for condition=ready --timeout=600s
+  oc wait deployments -n ${deployment_namespace} cert-manager --for condition=ready --timeout=600s
 
   oc apply -f "${certmanager_resources_dir}"/selfsigned-issuer.yaml || return $?
   oc apply -f "${certmanager_resources_dir}"/eventing-ca-issuer.yaml || return $?
