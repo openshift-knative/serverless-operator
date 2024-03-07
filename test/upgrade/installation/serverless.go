8136b0df3597875b5062deb4d2c619e2a9c195c0
Calum Murray <cmurray@redhat.com>
2024-03-07 15:55:53 -0500

    move order of when to run teardown script
    Signed-off-by: Calum Murray <cmurray@redhat.com>

--- test/upgrade/installation/serverless.go
+++ test/upgrade/installation/serverless.go
@@ -193,16 +193,16 @@ func DowngradeServerless(ctx *test.Context) error {
 		return fmt.Errorf("eventing downgrade failed: %w", err)
 	}
 
+	if err := downgradeKafkaContractsWithScript(); err != nil {
+		return fmt.Errorf("failed to downgrade contracts: %w", err)
+	}
+
 	if _, err := v1alpha1.WaitForKnativeKafkaState(ctx,
 		"knative-kafka",
 		knativeEventing,
 		v1alpha1.IsKnativeKafkaWithVersionReady(strings.TrimPrefix(test.Flags.KafkaVersionPrevious, "v")),
 	); err != nil {
 		return fmt.Errorf("knative kafka downgrade failed: %w", err)
-	}
-
-	if err := downgradeKafkaContractsWithScript(); err != nil {
-		return fmt.Errorf("failed to downgrade contracts: %w", err)
 	}
 
 	return nil
