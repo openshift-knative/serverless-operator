ca3472c69e61a924113addedd1e7b80317b54c9b
Christoph Stäbler <cstabler@redhat.com>
2024-03-08 08:12:53 +0100

    Fix shellcheck issues

--- test/upgrade/installation/downgrade.sh
+++ test/upgrade/installation/downgrade.sh
@@ -2,8 +2,8 @@
 
 for cm_name in "$@"; do
   cmdata=$(kubectl get cm "$cm_name" -n knative-eventing -ojson)
-  if [ ! -z "$cmdata" ]; then
-    new_data=$(echo $cmdata | jq -r .binaryData.data | base64 --decode | jq 'del(.trustBundles)' -c | base64 -w 0)
-    echo $cmdata | jq --arg new_data $new_data '.binaryData.data = $new_data' | kubectl apply -f -
+  if [ -n "$cmdata" ]; then
+    new_data=$(echo "$cmdata" | jq -r .binaryData.data | base64 --decode | jq 'del(.trustBundles)' -c | base64 -w 0)
+    echo "$cmdata" | jq --arg new_data "$new_data" '.binaryData.data = $new_data' | kubectl apply -f -
   fi
 done
