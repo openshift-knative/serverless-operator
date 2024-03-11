22560000435126810faaf6699fea5e0d637ee43d
Christoph Stäbler <cstabler@redhat.com>
2024-03-11 11:58:24 +0100

    Unset ingress.enableAutoCreateEventTypes in downgrade.sh too

--- test/upgrade/installation/downgrade.sh
+++ test/upgrade/installation/downgrade.sh
@@ -21,7 +21,7 @@ done
 for cm_name in "${contract_configmaps[@]}"; do
   cmdata=$(kubectl get cm "$cm_name" -n "$target_namespace" -ojson || true)
   if [ -n "$cmdata" ]; then
-    new_data=$(echo "$cmdata" | jq -r .binaryData.data | base64 --decode | jq 'del(.trustBundles)' -c | base64 -w 0)
+    new_data=$(echo "$cmdata" | jq -r .binaryData.data | base64 --decode | jq 'del(.trustBundles)' | jq 'del(.resources[].ingress.enableAutoCreateEventTypes)' -c | base64 -w 0)
     echo "$cmdata" | jq --arg new_data "$new_data" '.binaryData.data = $new_data' | kubectl apply -f -
   fi
 done
