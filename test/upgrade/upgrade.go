c579c65e2433e4ba10dd8075cef2ef37d8ced9d7
Martin Gencur <mgencur@redhat.com>
2024-06-28 17:57:25 +0200

    SRVKE-1626 Run cleanup uperations for EKB during downgrades (#2736)
    * Remove workaround for webhook strategy in downgrades
    
    The minimum supported version if now OCP 4.12
    
    * Run clean up operations from EKB during downgrades
    
    * Use knative-eventing namespace when deleting dispatcher
    
    We use knative-serving as system namespace for upgrade tests.

--- test/upgrade/upgrade.go
+++ test/upgrade/upgrade.go
@@ -4,6 +4,7 @@ import (
 	"github.com/openshift-knative/serverless-operator/test"
 	"github.com/openshift-knative/serverless-operator/test/upgrade/installation"
 	pkgupgrade "knative.dev/pkg/test/upgrade"
+	"knative.dev/reconciler-test/pkg/environment"
 )
 
 func ServerlessUpgradeOperations(ctx *test.Context) []pkgupgrade.Operation {
@@ -16,12 +17,14 @@ func ServerlessUpgradeOperations(ctx *test.Context) []pkgupgrade.Operation {
 	}
 }
 
-func ServerlessDowngradeOperations(ctx *test.Context) []pkgupgrade.Operation {
+func ServerlessDowngradeOperations(ctx *test.Context, glob environment.GlobalEnvironment) []pkgupgrade.Operation {
 	return []pkgupgrade.Operation{
 		pkgupgrade.NewOperation("DowngradeServerless", func(c pkgupgrade.Context) {
 			if err := installation.DowngradeServerless(ctx); err != nil {
 				c.T.Error("Serverless downgrade failed:", err)
 			}
+			installation.CleanupTriggerv2ConsumerGroups(c, glob)
+			installation.CleanupTriggerv2Deployments(c, glob)
 		}),
 	}
 }
