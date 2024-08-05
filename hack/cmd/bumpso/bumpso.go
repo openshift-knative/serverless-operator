448b3f604801f7a1e8fa5d2b6669bd6de636abc9
Martin Gencur <mgencur@redhat.com>
2024-08-05 18:35:45 +0200

    Generate index metadata for default channel from array of channels

--- hack/cmd/bumpso/bumpso.go
+++ hack/cmd/bumpso/bumpso.go
@@ -93,7 +93,6 @@ func run() error {
 	_ = common.SetNestedField(&node, currentVersion.String(), "olm", "replaces")
 	_ = common.SetNestedField(&node, previousVersion.String(), "olm", "previous", "replaces")
 	_ = common.SetNestedField(&node, skipRange(currentVersion, newVersion), "olm", "skipRange")
-	_ = common.SetNestedField(&node, skipRange(previousVersion, currentVersion), "olm", "previous", "skipRange")
 	_ = common.SetNestedField(&node, channelsList, "olm", "channels", "list")
 	_ = common.SetNestedField(&node, serving, "dependencies", "previous", "serving")
 	_ = common.SetNestedField(&node, eventing, "dependencies", "previous", "eventing")
--- hack/generate/index.sh
+++ hack/generate/index.sh
@@ -14,12 +14,52 @@ values[VERSION]="$(metadata.get project.version)"
 values[PREVIOUS_VERSION]="$(metadata.get olm.replaces)"
 values[PREVIOUS_REPLACES]="$(metadata.get olm.previous.replaces)"
 values[DEFAULT_CHANNEL]="$(metadata.get olm.channels.default)"
-values[LATEST_VERSIONED_CHANNEL]="$(metadata.get 'olm.channels.list[*]' | head -n 2 | tail -n 1)"
-values[PREVIOUS_CHANNEL]="$(metadata.get 'olm.channels.list[*]' | head -n 3 | tail -n 1)"
-values[PREVIOUS_REPLACES_CHANNEL]="$(metadata.get 'olm.channels.list[*]' | head -n 4 | tail -n 1)"
+values[LATEST_VERSIONED_CHANNEL]="$(metadata.get 'olm.channels.list[1]')"
+values[PREVIOUS_CHANNEL]="$(metadata.get 'olm.channels.list[2]')"
+values[PREVIOUS_REPLACES_CHANNEL]="$(metadata.get 'olm.channels.list[3]')"
 
 values[PREVIOUS_CHANNEL_HEAD]="${values[PREVIOUS_CHANNEL]#stable-}.0"
 values[PREVIOUS_REPLACES_CHANNEL_HEAD]="${values[PREVIOUS_REPLACES_CHANNEL]#stable-}.0"
+
+# Default channel includes more versions back to allow testing upgrades from older versions.
+function add_default_channel_entries {
+  local default_entry_yaml default_channel
+  default_entry_yaml="$(mktemp -t default-entry-XXXXX.yaml)"
+  default_channel=$(metadata.get olm.channels.default)
+  cat > "${default_entry_yaml}" <<EOF
+schema: olm.channel
+name: ${default_channel}
+package: serverless-operator
+entries:
+EOF
+
+  declare -a channels
+  channels=($(metadata.get 'olm.channels.list[*]'))
+  # Delete first element as this is the default channel
+  unset 'channels[0]'
+  for i in "${!channels[@]}"; do
+    current_version=${channels[$i]#stable-}.0
+    previous_i=$(( i+1 ))
+    # If this is the last item only enter name, without "replaces".
+    if [[ $i -eq ${#channels[@]} ]]; then
+      yq write --inplace "$default_entry_yaml" 'entries[+].name' "serverless-operator-v${current_version}"
+      break
+    fi
+    previous_version=${channels[$previous_i]#stable-}.0
+
+  cat << EOF | yq write --inplace --script - "$default_entry_yaml"
+- command: update
+  path: entries[+]
+  value:
+    name: "serverless-operator-v${current_version}"
+    replaces: "serverless-operator-v${previous_version}"
+    skipRange: ">=${previous_version} <${current_version}"
+EOF
+  done
+
+  echo "---" >> ${1}
+  cat "${default_entry_yaml}" >> ${1}
+}
 
 # Start fresh
 cp "$template" "$target"
@@ -28,3 +68,5 @@ for before in "${!values[@]}"; do
   echo "Value: ${before} -> ${values[$before]}"
   sed --in-place "s/__${before}__/${values[${before}]}/" "$target"
 done
+
+add_default_channel_entries "${target}"
