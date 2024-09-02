caec6c0c2e06b0d245e77f9b3fae083a762e6144
Pierangelo Di Pilato <pierdipi@redhat.com>
2024-09-02 17:30:50 +0200

    Generate CSV with Konflux images (serving and eventing)
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- hack/generate/images-rekt.sh
+++ hack/generate/images-rekt.sh
@@ -9,7 +9,6 @@ target="${2:?Provide a target file as arg[2]}"
 source "$(dirname "${BASH_SOURCE[0]}")/../lib/images.bash"
 
 default_knative_eventing_images
-default_knative_eventing_test_images
 default_knative_eventing_istio_images
 default_knative_eventing_kafka_broker_images
 default_knative_backstage_plugins_images
