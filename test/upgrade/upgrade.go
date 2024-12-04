3ea9010b1d7dae88aa9537c6bc75da9079324de2
Martin Gencur <mgencur@redhat.com>
2024-12-04 12:44:31 +0100

    Fix lint

--- test/upgrade/upgrade.go
+++ test/upgrade/upgrade.go
@@ -4,7 +4,6 @@ import (
 	"github.com/openshift-knative/serverless-operator/test"
 	"github.com/openshift-knative/serverless-operator/test/upgrade/installation"
 	pkgupgrade "knative.dev/pkg/test/upgrade"
-	"knative.dev/reconciler-test/pkg/environment"
 )
 
 func ServerlessUpgradeOperations(ctx *test.Context) []pkgupgrade.Operation {
@@ -17,15 +16,12 @@ func ServerlessUpgradeOperations(ctx *test.Context) []pkgupgrade.Operation {
 	}
 }
 
-func ServerlessDowngradeOperations(ctx *test.Context, glob environment.GlobalEnvironment) []pkgupgrade.Operation {
+func ServerlessDowngradeOperations(ctx *test.Context) []pkgupgrade.Operation {
 	return []pkgupgrade.Operation{
 		pkgupgrade.NewOperation("DowngradeServerless", func(c pkgupgrade.Context) {
 			if err := installation.DowngradeServerless(ctx); err != nil {
 				c.T.Error("Serverless downgrade failed:", err)
 			}
-			// Tests are disabled on z-stream releases. It was relevant for 1.33 -> 1.34 upgrade/downgrade scenario
-			//installation.CleanupTriggerv2ConsumerGroups(c, glob)
-			//installation.CleanupTriggerv2Deployments(c, glob)
 		}),
 	}
 }
