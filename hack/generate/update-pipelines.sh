34223e5f6a3d1820e46668047b674fba5c2b4593
Pierangelo Di Pilato <pierdipi@redhat.com>
2024-12-09 10:04:59 +0100

    Fix update-pipelines tag index
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- hack/generate/update-pipelines.sh
+++ hack/generate/update-pipelines.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 readonly version=$(yq read olm-catalog/serverless-operator/project.yaml project.version)
 # shellcheck disable=SC2038
-find .tekton/ \( -name "*-push.yaml" \) | xargs -I{} yq write --inplace "{}" "spec.params(name==additional-tags).value[0]" "${version}"
+find .tekton/ \( -name "*-push.yaml" \) | xargs -I{} yq write --inplace "{}" "spec.params(name==additional-tags).value[1]" "${version}"
