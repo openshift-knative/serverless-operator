50463792c9bb37f05b9f37ff91809f64b9a25c38
Pierangelo Di Pilato <pierdipi@redhat.com>
2025-01-30 13:41:24 +0100

    Bump Eventing to v1.16  (#3364)
    * Bump Eventing to v1.16
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    * Run make generated-files
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    * Install certmanager for Kakfa tests
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    ---------
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- test/upgrade/upgrade.go
+++ test/upgrade/upgrade.go
@@ -2,7 +2,6 @@ package upgrade
 
 import (
 	pkgupgrade "knative.dev/pkg/test/upgrade"
-	"knative.dev/reconciler-test/pkg/environment"
 
 	"github.com/openshift-knative/serverless-operator/test"
 	"github.com/openshift-knative/serverless-operator/test/upgrade/installation"
@@ -18,7 +17,7 @@ func ServerlessUpgradeOperations(ctx *test.Context) []pkgupgrade.Operation {
 	}
 }
 
-func ServerlessDowngradeOperations(ctx *test.Context, glob environment.GlobalEnvironment) []pkgupgrade.Operation {
+func ServerlessDowngradeOperations(ctx *test.Context) []pkgupgrade.Operation {
 	return []pkgupgrade.Operation{
 		pkgupgrade.NewOperation("DowngradeServerless", func(c pkgupgrade.Context) {
 			if err := installation.DowngradeServerless(ctx); err != nil {
