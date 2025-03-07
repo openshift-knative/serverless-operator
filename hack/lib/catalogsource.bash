426ba210ea7c77bc47c5ad5839dfbbffc51ec9ec
Christoph Stäbler <cstabler@redhat.com>
2025-03-07 09:17:09 +0100

    Add mirrors under correct field in ImageDigestMirrorSet YAML (#3506)

--- hack/lib/catalogsource.bash
+++ hack/lib/catalogsource.bash
@@ -131,11 +131,11 @@ function create_image_content_source_policy {
   index="${1:?Pass index image as arg[1]}"
   registry_source="${2:?Pass source registry arg[2]}"
   registry_target="${3:?Pass target registry arg[3]}"
-  output_file="${4:?Pass output file arg[4]}"
-  digest_mirror_output_file="${5:?Pass digest_mirror_output_file arg[5]}"
+  image_content_source_policy_output_file="${4:?Pass output file arg[4]}"
+  image_digest_mirror_output_file="${5:?Pass image_digest_mirror_output_file arg[5]}"
 
   logger.info "Install ImageContentSourcePolicy"
-  cat > "$output_file" <<EOF
+  cat > "$image_content_source_policy_output_file" <<EOF
 apiVersion: operator.openshift.io/v1alpha1
 kind: ImageContentSourcePolicy
 metadata:
@@ -146,7 +146,7 @@ spec:
   repositoryDigestMirrors:
 EOF
 
-  cat > "$digest_mirror_output_file" <<EOF
+  cat > "$image_digest_mirror_output_file" <<EOF
 apiVersion: operator.openshift.io/v1alpha1
 kind: ImageDigestMirrorSet
 metadata:
@@ -182,8 +182,8 @@ EOF
           target_img=${img%-rhel*}
         fi
 
-        add_repository_digest_mirrors "$output_file" "${registry_source}/${img}" "${registry_target}/${target_img}"
-        add_repository_digest_mirrors "$digest_mirror_output_file" "${registry_source}/${img}" "${registry_target}/${target_img}"
+        add_repository_digest_mirrors "$image_content_source_policy_output_file" "${registry_source}/${img}" "${registry_target}/${target_img}"
+        add_image_digest_mirrors "$image_digest_mirror_output_file" "${registry_source}/${img}" "${registry_target}/${target_img}"
       fi
     done <<< "$mirrors"
 }
@@ -193,6 +193,17 @@ function add_repository_digest_mirrors {
   cat << EOF | yq write --inplace --script - "$1"
 - command: update
   path: spec.repositoryDigestMirrors[+]
+  value:
+    mirrors: [ "${3}" ]
+    source: "${2}"
+EOF
+}
+
+function add_image_digest_mirrors {
+  echo "Add mirror image to '${1}' - $2 = $3"
+  cat << EOF | yq write --inplace --script - "$1"
+- command: update
+  path: spec.imageDigestMirrors[+]
   value:
     mirrors: [ "${3}" ]
     source: "${2}"
