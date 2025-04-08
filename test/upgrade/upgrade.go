a454bfe6c42a5d0e2cfa4fb67515a50ad42154da
Marek Schmidt <maschmid@redhat.com>
2025-04-08 15:24:10 +0200

    [release-1.35] remove obsolete downgrade kafkachannel cleanup (#3585)
    * remove obsolete downgrade kafkachannel cleanup
    
    * cleanup

--- test/upgrade/upgrade.go
+++ test/upgrade/upgrade.go
@@ -1,11 +1,9 @@
 package upgrade
 
 import (
-	pkgupgrade "knative.dev/pkg/test/upgrade"
-	"knative.dev/reconciler-test/pkg/environment"
-
 	"github.com/openshift-knative/serverless-operator/test"
 	"github.com/openshift-knative/serverless-operator/test/upgrade/installation"
+	pkgupgrade "knative.dev/pkg/test/upgrade"
 )
 
 func ServerlessUpgradeOperations(ctx *test.Context) []pkgupgrade.Operation {
@@ -18,14 +16,12 @@ func ServerlessUpgradeOperations(ctx *test.Context) []pkgupgrade.Operation {
 	}
 }
 
-func ServerlessDowngradeOperations(ctx *test.Context, glob environment.GlobalEnvironment) []pkgupgrade.Operation {
+func ServerlessDowngradeOperations(ctx *test.Context) []pkgupgrade.Operation {
 	return []pkgupgrade.Operation{
 		pkgupgrade.NewOperation("DowngradeServerless", func(c pkgupgrade.Context) {
 			if err := installation.DowngradeServerless(ctx); err != nil {
 				c.T.Error("Serverless downgrade failed:", err)
 			}
-			installation.CleanupChannelv2ConsumerGroups(c, glob)
-			installation.CleanupChannelv2Deployments(c, glob)
 		}),
 	}
 }
