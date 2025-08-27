02b45384e3aceab95e16d1cd455196d5ceb985f0
Marek Schmidt <maschmid@redhat.com>
2025-08-27 14:46:37 +0200

    fixes bundle generation for .micro >=.2 versions

--- hack/generate/annotations.sh
+++ hack/generate/annotations.sh
@@ -9,7 +9,7 @@ target="${2:?Provide a target annotations file as arg[2]}"
 source "$(dirname "${BASH_SOURCE[0]}")/../lib/metadata.bash"
 
 declare -A values
-values[operators.operatorframework.io.bundle.channel.default.v1]="$(metadata.get olm.channels.default)"
+values[operators.operatorframework.io.bundle.channel.default.v1]="$(metadata.get olm.channels.operator_default)"
 values[operators.operatorframework.io.bundle.channels.v1]="$(metadata.get olm.channels.default),$(metadata.get 'olm.channels.list[*]' | head -n 2 | tail -n 1)"
 values[operators.operatorframework.io.bundle.package.v1]="$(metadata.get project.name)"
 
