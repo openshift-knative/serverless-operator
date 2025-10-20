832b3b0f1914b6e5f889ae9aa2d911116a2b15eb
Marek Schmidt <maschmid@redhat.com>
2025-10-20 12:54:44 +0200

    Make 1.37.0 replace 1.36.1, update index-generating scripts

--- hack/generate/index.sh
+++ hack/generate/index.sh
@@ -8,7 +8,7 @@ target="${1:?Provide a target index yaml file as arg[1]}"
 source "$(dirname "${BASH_SOURCE[0]}")/../lib/__sources__.bash"
 
 function add_channel_entries {
-  local channel_entry_yaml channel target current_version replaces_version base_version
+  local channel_entry_yaml channel target version replaces_version skip_range previous_bundles_len
   channel=${1:?Provide channel name}
   target=${2:?Provide target file name}
   channel_entry_yaml="$(mktemp -t default-entry-XXXXX.yaml)"
@@ -21,40 +21,35 @@ package: serverless-operator
 entries:
 EOF
 
-  current_version=$(metadata.get 'project.version')
-  major=$(versions.major "$current_version")
-  minor=$(versions.minor "$current_version")
-  micro=$(versions.micro "$current_version")
+  version=$(metadata.get 'project.version')
+  replaces_version=$(metadata.get 'olm.replaces')
+  skip_range=$(metadata.get 'olm.skipRange')
 
-  for i in $(seq "$INDEX_IMAGE_NUM_CSVS"); do
-    current_major=$((major))
-    current_minor=$((minor))
-    current_micro=$((micro - i + 1))
+  cat << EOF | yq write --inplace --script - "$channel_entry_yaml"
+  - command: update
+    path: entries[+]
+    value:
+      name: "serverless-operator.v${version}"
+      replaces: "serverless-operator.v${replaces_version}"
+      skipRange: "${skip_range}"
+EOF
 
-    if [[ "$current_micro" -le 0 ]]; then
-      current_minor=$((minor + current_micro))
-      current_micro=0
-    fi
+  previous_bundles_len=$(metadata.get 'olm.previousBundles' | yq read - -l '')
+  for i in $(seq ${previous_bundles_len}); do
+    version=$(metadata.get "olm.previousBundles | yq read - '[$i]['version']")
+    replaces_version=$(metadata.get "olm.previousBundles | yq read - '[$i]['replaces']")
+    skip_range=$(metadata.get "olm.previousBundles | yq read - '[$i]['skipRange']")
 
-    current_version="${current_major}.${current_minor}.${current_micro}"
-    if [[ "$current_micro" == "0" ]]; then
-      base_version="${current_major}.$(( current_minor-1 )).0"
-      replaces_version="${current_major}.$(( current_minor-1 )).0"
-    else
-      base_version="${current_major}.${current_minor}.0"
-      replaces_version="${current_major}.${current_minor}.$(( current_micro-1 ))"
-    fi
-
-    if [[ $i -eq "$INDEX_IMAGE_NUM_CSVS" ]]; then
-      yq write --inplace "$channel_entry_yaml" 'entries[+].name' "serverless-operator.v${current_version}"
+    if [[ $i -eq $previous_bundles_len ]]; then
+      yq write --inplace "$channel_entry_yaml" 'entries[+].name' "serverless-operator.v${version}"
     else
       cat << EOF | yq write --inplace --script - "$channel_entry_yaml"
   - command: update
     path: entries[+]
     value:
-      name: "serverless-operator.v${current_version}"
+      name: "serverless-operator.v${version}"
       replaces: "serverless-operator.v${replaces_version}"
-      skipRange: ">=${base_version} <${current_version}"
+      skipRange: "${skip_range}"
 EOF
     fi
   done
