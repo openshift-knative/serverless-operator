3473d4435298d411c4b5d8b34c263bf1d7d31cdb
David Simansky <dsimansk@redhat.com>
2026-04-01 14:20:04 +0200

    Try native kube proxy injection

--- vendor/knative.dev/reconciler-test/pkg/k8s/wait.go
+++ vendor/knative.dev/reconciler-test/pkg/k8s/wait.go
@@ -348,8 +348,7 @@ func WaitForServiceReady(ctx context.Context, t feature.T, name string, readines
 
 	if cfg := environment.GetIstioConfig(ctx); cfg.Enabled {
 		job.Spec.Template.Annotations = map[string]string{
-			"sidecar.istio.io/inject":                "true",
-			"sidecar.istio.io/exitOnMainTermination": "true",
+			"sidecar.istio.io/nativeSidecar": "true",
 			"sidecar.istio.io/rewriteAppHTTPProbers": "true",
 		}
 	}
